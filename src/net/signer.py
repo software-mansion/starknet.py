6262a8ba1e303e3ceacf5d3c0a85c00dde292704
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2021-11-26 16:42:32 +0100

    fmt

--- src/net/signer.py
+++ src/net/signer.py
@@ -5,7 +5,7 @@ from starkware.cairo.common.hash_state import compute_hash_on_elements
 from starkware.starknet.definitions.transaction_type import TransactionType
 from starkware.starknet.public.abi import get_selector_from_name
 from starkware.starknet.services.api.gateway.transaction import InvokeFunction
-from starkware.crypto.signature.signature import (private_to_stark_key, sign)
+from starkware.crypto.signature.signature import private_to_stark_key, sign
 from src.net import Client
 from src.types import AddressRepresentation, parse_address
 
@@ -16,24 +16,32 @@ class KeyPair:
     public_key: int
 
 
-def hash_message(account: int, to: int, selector: int, calldata: List[int], nonce: int) -> int:
-    return compute_hash_on_elements([
-        account,
-        to,
-        selector,
-        compute_hash_on_elements(calldata),
-        nonce,
-    ])
+def hash_message(
+    account: int, to: int, selector: int, calldata: List[int], nonce: int
+) -> int:
+    return compute_hash_on_elements(
+        [
+            account,
+            to,
+            selector,
+            compute_hash_on_elements(calldata),
+            nonce,
+        ]
+    )
 
 
 class Signer(Client):
-    def __init__(self, address: AddressRepresentation, key_pair: KeyPair, *args, **kwargs):
+    def __init__(
+        self, address: AddressRepresentation, key_pair: KeyPair, *args, **kwargs
+    ):
         super().__init__(*args, **kwargs)
         self.address = parse_address(address)
         self.key_pair = key_pair
 
     async def add_transaction(
-        self, tx: InvokeFunction, token: Optional[str] = None,
+        self,
+        tx: InvokeFunction,
+        token: Optional[str] = None,
     ) -> Dict[str, int]:
         if tx.tx_type == TransactionType.DEPLOY:
             return await super().add_transaction(tx, token)
@@ -41,12 +49,12 @@ class Signer(Client):
         result = await super().call_contract(
             InvokeFunction(
                 contract_address=self.address,
-                entry_point_selector=get_selector_from_name('get_nonce'),
+                entry_point_selector=get_selector_from_name("get_nonce"),
                 calldata=[],
                 signature=[],
             )
         )
-        nonce = int(result['result'][0], 16)
+        nonce = int(result["result"][0], 16)
 
         msg_hash = hash_message(
             account=self.address,
@@ -61,7 +69,7 @@ class Signer(Client):
 
         return await super().add_transaction(
             InvokeFunction(
-                entry_point_selector=get_selector_from_name('execute'),
+                entry_point_selector=get_selector_from_name("execute"),
                 calldata=[
                     tx.contract_address,
                     tx.entry_point_selector,
@@ -70,6 +78,6 @@ class Signer(Client):
                     nonce,
                 ],
                 contract_address=self.address,
-                signature=[r, s]
+                signature=[r, s],
             )
         )
