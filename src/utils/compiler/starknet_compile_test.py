f0599b7d602931341f04d61abcac9502c072ea59
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2021-12-02 15:09:20 +0100

    Check if abi is not an empty array

--- src/utils/compiler/starknet_compile_test.py
+++ src/utils/compiler/starknet_compile_test.py
@@ -4,6 +4,7 @@ import os
 from src.utils.compiler.starknet_compile import starknet_compile
 
 directory = os.path.dirname(__file__)
+
 map_filename = os.path.join(directory, "map.cairo")
 test_file_content = open(map_filename).read()
 
@@ -11,4 +12,5 @@ test_file_content = open(map_filename).read()
 def test_starknet_compilation():
     output_file_str = starknet_compile({map_filename: test_file_content})
     output_json = json.loads(output_file_str)
-    assert output_json.get("abi") is not None
+
+    assert output_json.get("abi") != []
