6d591022152cfa625362bed76668e1a14b9653ef
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-03 18:31:11 +0100

    define cmake cxx compiler in command opts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 cd crypto-cpp
 mkdir -p build/Release
-(cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release ../..)
+(cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="$CMAKE_CXX_COMPILER" ../..)
 make -C build/Release
 cp build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
