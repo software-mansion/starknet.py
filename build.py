a85d27c79f70b8030b9c34697b4919f51f3c3293
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-04 10:27:20 +0100

    Try using poetry-core's WheelBuilder

--- build.py
+++ build.py
@@ -4,11 +4,11 @@ import os
 def build(setup_args):
     os.system(
         """
-        cd crypto-cpp
-        mkdir -p build/Release
-        sed -i'.original' 's/${CMAKE_CXX_FLAGS} -std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC/-std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC ${CMAKE_CXX_FLAGS}/' CMakeLists.txt
-        (cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-Wno-type-limits -Wno-range-loop-analysis" ../..)
-        make -C build/Release
-        cp build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
+    cd crypto-cpp
+    mkdir -p build/Release
+    sed -i'.original' 's/${CMAKE_CXX_FLAGS} -std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC/-std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC ${CMAKE_CXX_FLAGS}/' CMakeLists.txt
+    (cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-Wno-type-limits -Wno-range-loop-analysis" ../..)
+    make -C build/Release
+    cp build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
     """
     )
