5548b6bcc7d185272ceb8ae45d51a1f57ae5f3a2
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-05 13:52:36 +0100

    disable unused parameter inspection

--- build_extension.sh
+++ build_extension.sh
@@ -2,7 +2,7 @@
 cd crypto-cpp
 mkdir -p build/Release
 sed -i'.original' 's/${CMAKE_CXX_FLAGS} -std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC/-std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC ${CMAKE_CXX_FLAGS}/' CMakeLists.txt
-(cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-Wno-type-limits -Wno-range-loop-analysis" ../..)
+(cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-Wno-type-limits -Wno-range-loop-analysis -Wno-unused-parameter" ../..)
 make -C build/Release
 if [ $? -ne 0 ]; then
   exit 1
