af6de9d4b76ab3751163669b839226680077944b
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-05 12:52:12 +0100

    Try removing the flags overall

--- builder.py
+++ builder.py
@@ -16,7 +16,6 @@ class BuildCrypto(build_ext):
             """
             cd crypto-cpp
             mkdir -p build/Release
-            export CXXFLAGS="-Wno-type-limits -Wno-range-loop-analysis"
             (cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" ../..)
             make -C build/Release
             cp build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
