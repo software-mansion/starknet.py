0e9fd372a8062c5da13dafb7b3784f5569d8e9ca
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-05 13:19:24 +0100

    Fix build procedure

--- builder.py
+++ builder.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
@@ -12,8 +13,9 @@ class CryptoExtension(Extension):
 class BuildCrypto(build_ext):
     def build_extension(self, ext):
         # TODO: Check if windows and then execute corresponding script
+        subprocess.run("chmod +x ./build_extension.sh", shell=True, check=True)
         subprocess.run(
-            "build_extension.sh",
+            "./build_extension.sh",
             shell=True,
             check=True,
         )
