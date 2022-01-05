bbd23fa489624e4bf8eb1dec9b165aed2e204379
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-05 19:25:45 +0100

    Convert if to powershell style

--- build_extension.ps1
+++ build_extension.ps1
@@ -3,8 +3,11 @@ mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '${CMAKE_CXX_FLAGS} -std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '-std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
 cd build\Release
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-Wno-type-limits -Wno-range-loop-analysis -Wno-unused-parameter" ..\..
-make -C build\Release
-if errorlevel neq 0 (
-   exit /b %errorlevel%
-)
+$Result = make -C build\Release
+if ($Result.ExitCode -ne 0)
+  {
+    Write-Output "Build returned a non-zero code"
+    Exit $Result.ExitCode
+  }
+
 copy build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
