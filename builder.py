97843a7a8917788940f413d9eba4e9c3bbb8d888
Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
2022-01-12 21:01:53 +0100

    Try fixing builder & pyproject converter (#70)

--- builder.py
+++ builder.py
@@ -3,6 +3,8 @@ import os
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
 
+from setuptools.command.build_py import build_py
+
 
 class CryptoExtension(Extension):
     def __init__(self):
@@ -10,8 +12,20 @@ class CryptoExtension(Extension):
         Extension.__init__(self, self.name, sources=[])
 
 
+class BuildPy(build_py):
+    def run(self):
+        self.run_command("build_ext")
+        return super().run()
+
+
 class BuildCrypto(build_ext):
+    already_built = False
+
     def build_extension(self, ext):
+        if self.already_built:
+            print("was already built, skipping...")
+            return
+
         if os.name != "nt":
             subprocess.run("chmod a+x ./build_extension.sh", shell=True, check=True)
             subprocess.run(
@@ -26,3 +40,5 @@ class BuildCrypto(build_ext):
                 process.wait()
                 if process.returncode != 0:
                     raise Exception("Build returned a non-zero code")
+
+        self.already_built = True
