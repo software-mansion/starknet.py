cce2bc79e5494bfe742bc787fc195bc4bc93df46
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-13 15:31:22 +0100

    Try going back to build root

--- build_extension.ps1
+++ build_extension.ps1
@@ -3,7 +3,8 @@ mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/std:c++17 /Werror /Wall /Wextra ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
 cd build\Release
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" ..\..
-$Result = make -C .
+cd ..\..
+$Result = make -C build/Release
 if ($Result.ExitCode -ne 0)
   {
     Write-Output "Build returned a non-zero code"
