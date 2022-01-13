2aa9a15bbb17b472d99b2cd938427f1c3556e81f
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-13 12:22:50 +0100

    Remove extra flags for g++

--- build_extension.ps1
+++ build_extension.ps1
@@ -2,7 +2,7 @@ cd crypto-cpp
 mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/std:c++17 /Werror /Wall /Wextra ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
 cd build\Release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="/Wno-type-limits /Wno-range-loop-analysis /Wno-unused-parameter" ..\..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" ..\..
 $Result = make -C build\Release
 if ($Result.ExitCode -ne 0)
   {
