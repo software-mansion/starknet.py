dc47ee5d153c9283d6c0fde20881ae9a0a09e8dc
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-13 16:07:11 +0100

    Try something else

--- build_extension.ps1
+++ build_extension.ps1
@@ -3,7 +3,6 @@ mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/std:c++17 /Werror /Wall /Wextra ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
 cd build\Release
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" ..\..
-Get-Location
-dir
-make -C build\Release
+cd ..\..
+make -C build/Release
 copy build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
