4007c9d10cc996b9234656f6dd5ec87d4cd5a9d7
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-13 15:51:29 +0100

    Output some debug information

--- build_extension.ps1
+++ build_extension.ps1
@@ -3,11 +3,7 @@ mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/std:c++17 /Werror /Wall /Wextra ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
 cd build\Release
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" ..\..
-$Result = make .
-if ($Result.ExitCode -ne 0)
-  {
-    Write-Output "Build returned a non-zero code"
-    Exit $Result.ExitCode
-  }
-
+Get-Location
+dir
+make -C build\Release
 copy build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
