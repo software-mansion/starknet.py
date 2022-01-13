78be3ac13acaf645949de07eb276d0ac98f24c1b
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-13 19:33:03 +0100

    Try lowering std as well

--- build_extension.ps1
+++ build_extension.ps1
@@ -1,7 +1,7 @@
 cd crypto-cpp
 mkdir build\Release
-(Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/WX /Wall ${CMAKE_CXX_FLAGS} /std:c++20'} | Set-Content CMakeLists.txt
-(Get-content CMakeLists.txt) | Foreach-Object {$_ -replace 'CMAKE_CXX_STANDARD 17', 'CMAKE_CXX_STANDARD 20'} | Set-Content CMakeLists.txt
+(Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/WX /Wall ${CMAKE_CXX_FLAGS} /std:c++14'} | Set-Content CMakeLists.txt
+(Get-content CMakeLists.txt) | Foreach-Object {$_ -replace 'CMAKE_CXX_STANDARD 17', 'CMAKE_CXX_STANDARD 14'} | Set-Content CMakeLists.txt
 Get-Content CMakeLists.txt
 cd build\Release
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" ..\..
