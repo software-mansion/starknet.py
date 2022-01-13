c589887eab1aab8a8dbeea1943d3a34133ec0e1d
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-13 16:37:48 +0100

    Try using cmake directly for building vcxproj files

--- build_extension.ps1
+++ build_extension.ps1
@@ -3,8 +3,5 @@ mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/std:c++17 /Werror /Wall /Wextra ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
 cd build\Release
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" ..\..
-cd ..\..
-dir
-dir build\Release
-make
-copy build/Release/src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
+cmake --build .
+copy ./src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
