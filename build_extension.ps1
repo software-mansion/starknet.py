a597f8ff78969589e929687aaebe0e1a3fbf0cbd
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-14 14:25:36 +0100

    Update action triggers, restore the workflow

--- build_extension.ps1
+++ build_extension.ps1
@@ -1,7 +1,6 @@
 cd crypto-cpp
 mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '-Wextra -Wall ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
-Get-Content CMakeLists.txt
 cd build\Release
 cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="c++" ..\..
 cmake --build .
