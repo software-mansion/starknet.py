40eaeecc70e69341070991ca96caae3167a6ceaa
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-01-14 13:50:09 +0100

    Switch generating makefile output to mingw format

--- build_extension.ps1
+++ build_extension.ps1
@@ -3,6 +3,6 @@ mkdir build\Release
 (Get-content CMakeLists.txt) | Foreach-Object {$_ -replace '\$\{CMAKE_CXX_FLAGS\} -std=c\+\+17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC', '/WX /Wall ${CMAKE_CXX_FLAGS}'} | Set-Content CMakeLists.txt
 Get-Content CMakeLists.txt
 cd build\Release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="c++" ..\..
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="c++" ..\..
 cmake --build .
 copy ./src/starkware/crypto/ffi/libcrypto_c_exports.* ../starknet_py/utils/crypto
