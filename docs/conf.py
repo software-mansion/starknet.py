e2d439a59a39218261017b833563bcb881e82f5c
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-02-03 13:07:25 +0100

    try aligning versions of enum tools

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Software Mansion"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-needs_extensions = {"enum_tools": "0.9.0"}
+needs_extensions = {"enum_tools": "^0.9.0"}
 
 extensions = [
     "sphinx.ext.autodoc",
