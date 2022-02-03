ebce474bc00ab46707bbf49b6401e30f405f875e
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-02-03 11:44:03 +0100

    Specify needs_extensions in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ author = "Software Mansion"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+needs_extensions = {"enum_tools.autoenum": "0.9.0"}
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
