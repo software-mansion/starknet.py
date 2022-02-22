dbc882eafa96eafb1062f0e85ed1ebb5a9f09a26
pweglik <36445788+pweglik@users.noreply.github.com>
2022-02-22 11:39:21 +0100

    Add unit tests (#87)

--- starknet_py/contract_test.py
+++ starknet_py/contract_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from starknet_py.contract import Contract, PreparedFunctionCall, ContractData
 from starknet_py.net import Client
 
@@ -78,4 +80,13 @@ def test_transaction_hash():
     )
     assert (
         call.hash == 0x203BFF8307C3266B0749A0D1DBA143907F32F7E55C84A4A34077690C9C91BAC
+    )
+
+
+def test_no_valid_source():
+    with pytest.raises(ValueError) as v_err:
+        Contract.compute_contract_hash()
+
+    assert "One of compiled_contract or compilation_source is required." in str(
+        v_err.value
     )
--- starknet_py/tests/e2e/account/account_client_test.py
+++ starknet_py/tests/e2e/account/account_client_test.py
@@ -4,7 +4,7 @@ import pytest
 
 from starknet_py.contract import Contract
 from starknet_py.net import AccountClient
-from starknet_py.net.models import StarknetChainId
+from starknet_py.net.models import StarknetChainId, InvokeFunction, TransactionType
 from starknet_py.tests.e2e.utils import DEVNET_ADDRESS
 
 directory = os.path.dirname(__file__)
@@ -25,3 +25,23 @@ async def test_deploy_account_contract_and_sign_tx():
     (resp,) = await map_contract.functions["get"].call(k)
 
     assert resp == v
+
+
+@pytest.mark.asyncio
+async def test_error_when_tx_signed():
+    acc_client = await AccountClient.create_account(
+        net=DEVNET_ADDRESS, chain=StarknetChainId.TESTNET
+    )
+
+    invoke_function = InvokeFunction(
+        contract_address=123,
+        entry_point_selector=123,
+        calldata=[],
+        signature=[123, 321],
+    )
+    with pytest.raises(TypeError) as t_err:
+        await acc_client.add_transaction(tx=invoke_function)
+
+    assert "Adding signatures to a signer tx currently isn't supported" in str(
+        t_err.value
+    )
