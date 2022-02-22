f8877e50003b54f15fd4a6ed3c14b9a7273f9e49
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-02-22 12:29:06 +0100

    check for OS in the script

--- build_extension.sh
+++ build_extension.sh
@@ -4,7 +4,14 @@ cd crypto-cpp
 mkdir -p build/Release
 TARGET_MACHINE=$(gcc -dumpmachine)
 echo "Targeting ${TARGET_MACHINE}"
-sed -i'.original' "s/\${CMAKE_CXX_FLAGS} -std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC/-std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC \${CMAKE_CXX_FLAGS} -target ${TARGET_MACHINE}/" CMakeLists.txt
+
+if [[ $(uname) == "Darwin" ]] :
+  then
+    sed -i'.original' "s/\${CMAKE_CXX_FLAGS} -std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC/-std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC \${CMAKE_CXX_FLAGS} -target ${TARGET_MACHINE}/" CMakeLists.txt
+  else
+    sed -i'.original' "s/\${CMAKE_CXX_FLAGS} -std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC/-std=c++17 -Werror -Wall -Wextra -fno-strict-aliasing -fPIC \${CMAKE_CXX_FLAGS}/" CMakeLists.txt
+fi
+
 cat CMakeLists.txt
 (cd build/Release; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="g++" -DCMAKE_CXX_FLAGS="-Wno-type-limits -Wno-range-loop-analysis -Wno-unused-parameter" ../..)
 make -C build/Release
