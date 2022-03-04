17de316a0afbd71deb1606c9348d4fc725f1351e
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2022-03-04 10:01:08 +0100

    Updated hashing

--- starknet_py/utils/crypto/cpp_bindings_test.py
+++ starknet_py/utils/crypto/cpp_bindings_test.py
@@ -7,14 +7,17 @@ from starknet_py.utils.crypto.cpp_bindings import (
     load_cpp_lib,
 )
 from starknet_py.utils.crypto.facade import (
+    MultiCall,
+    Call,
     use_cpp_variant,
-    hash_message,
+    hash_multicall,
     message_signature,
 )
 
 
 def test_hashing(monkeypatch):
-    args = 1, 2, 3, [4, 5], 6, 0
+    call = Call(to_addr=1, selector=2, calldata=[4, 5])
+    multi_call = MultiCall(account=3, calls=[call], nonce=6)
     times = []
 
     load_cpp_lib()  # Pre-load cpp extension to make the hashing time appropriate
@@ -23,14 +26,14 @@ def test_hashing(monkeypatch):
 
         assert use_cpp_variant()
         start = time.time()
-        hash_1 = hash_message(*args)
+        hash_1 = hash_multicall(multi_call)
         end = time.time()
         time_with_crypto = end - start
 
         monkeypatch.setenv("DISABLE_CRYPTO_C_EXTENSION", "true")
         assert not use_cpp_variant()
         start = time.time()
-        hash_2 = hash_message(*args)
+        hash_2 = hash_multicall(multi_call)
         end = time.time()
         time_without_crypto = end - start
         times.append((time_with_crypto, time_without_crypto))
