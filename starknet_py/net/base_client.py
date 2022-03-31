831f7f28bde9f45fd522dff292c7be4a48cc45a6
Artur Michalek <artur.michalek@swmansion.com>
2022-03-31 15:29:11 +0200

    Initial work on BaseClient abc

---
+++ starknet_py/net/base_client.py
@@ -0,0 +1,143 @@
+from abc import ABC, abstractmethod
+from typing import Union
+
+from starknet_py.net.client_models import *
+from starknet_py.contract import Contract
+
+
+class BaseClient(ABC):
+    @abstractmethod
+    async def get_block(
+        self,
+        block_hash: Optional[Union[int, str]] = None,
+        block_number: Optional[int] = None,
+    ) -> StarknetBlock:
+        """
+        Retrieve the block's data by its number or hash
+
+        :param block_hash: Block's hash
+        :param block_number: Block's number
+        :return: StarknetBlock object representing retrieved block
+        """
+
+    @abstractmethod
+    async def get_state_update(
+        self,
+        block_hash: Optional[Union[int, str]] = None,
+        block_number: Optional[int] = None,
+    ) -> BlockState:
+        """
+        # TODO expand specs of this class, create proper dataclass
+        Get the information about the result of executing the requested block
+
+        :param block_hash: Block's hash
+        :param block_number: Block's number
+        :return: JsonObject object representing retrieved block
+        """
+
+    @abstractmethod
+    async def get_storage_at(
+        self,
+        contract_address: int,
+        key: int,
+        block_hash: Optional[Union[int, str]] = None,
+        block_number: Optional[int] = None,
+    ) -> str:
+        """
+        :param contract_address: Contract's address on Starknet
+        :param key: An address of the storage variable inside the contract.
+        :param block_hash: Fetches the value of the variable at given block hash
+        :param block_number: See above, uses block number instead of hash
+        :return: Storage value of given contract
+        """
+
+    @abstractmethod
+    async def get_transaction(
+        self,
+        tx_hash: Union[int, str],
+        block_hash: Optional[Union[int, str]] = None,
+        block_number: Optional[int] = None,
+        index: Optional[int] = None,
+    ) -> Transaction:
+        """
+        Get the details and status of a submitted transaction
+
+        :param tx_hash: Transaction's hash
+        :param block_hash: Fetches the value of the variable at given block hash
+        :param block_number: See above, uses block number instead of hash
+        :param index: The index in the block to search for the transaction
+        :return: Dictionary representing JSON of the transaction on Starknet
+        """
+
+    @abstractmethod
+    async def get_transaction_receipt(
+        self,
+        tx_hash: Union[int, str],
+    ) -> TransactionReceipt:
+        """
+        Get the transaction receipt
+
+        :param tx_hash: Transaction's hash
+        :return: Dictionary representing JSON of the transaction's receipt on Starknet
+        """
+
+    @abstractmethod
+    async def get_code(
+        self,
+        contract_address: int,
+        block_hash: Optional[Union[int, str]] = None,
+        block_number: Optional[int] = None,
+    ) -> ContractCode:
+        """
+        Get deployed contract's bytecode and abi.
+
+        :raises BadRequest: when contract is not found
+        :param contract_address: Address of the contract on Starknet
+        :param block_hash: Get code at specific block hash
+        :param block_number: Get code at given block number (or "pending" for pending block)
+        :return: JSON representation of compiled: {"bytecode": list, "abi": dict}
+        """
+
+    @abstractmethod
+    async def call_contract(
+        self,
+        invoke_tx: FunctionCall,
+        block_hash: Optional[Union[int, str]] = None,
+        block_number: Optional[int] = None,
+    ) -> List[int]:
+        """
+        Call the contract with given instance of InvokeTransaction
+
+        :param invoke_tx: Invoke transaction
+        :param block_hash: Block hash to execute the contract at specific point of time
+        :param block_number: Block number (or "pending" for pending block) to execute the contract at
+        :return: List of integers representing contract's function output (structured like calldata)
+        """
+
+    @abstractmethod
+    async def add_transaction(
+        self,
+        tx: Transaction,
+    ) -> SentTransaction:
+        """
+        Send a transaction to the network
+
+        :param tx: Transaction object (i.e. InvokeFunction, Deploy).
+        :return: Dictionary with `code`, `transaction_hash`
+        """
+
+    @abstractmethod
+    async def deploy(
+        self,
+        contract: Contract,
+        constructor_calldata: List[int],
+        salt: Optional[int] = None,
+    ) -> SentTransaction:
+        """
+        Deploy a contract to the network
+
+        :param contract: Contract object
+        :param constructor_calldata: Data to call the contract constructor with
+        :param salt: Salt to be used when signing a transaction
+        return: Dict with result of the transaction deployment
+        """
---
+++ starknet_py/net/client_models.py
@@ -0,0 +1,121 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import List, Optional, Any, Dict
+
+
+@dataclass
+class Event:
+    from_address: int
+    keys: List[int]
+    data: List[int]
+
+
+@dataclass
+class FunctionCall:
+    """
+    Dataclass representing a call to a function
+    """
+
+    contract_address: int
+    entry_point_selector: int
+    calldata: List[int]
+
+
+@dataclass
+class L1toL2Message:
+    l1_address: int
+    l2_address: int
+    payload: List[int]
+
+
+@dataclass
+class L2toL1Message:
+    l2_address: int
+    l1_address: int
+    payload: List[int]
+
+
+@dataclass
+class Transaction:
+    """
+    Dataclass representing a transaction
+    """
+
+    hash: int
+    function_call: Optional[FunctionCall]
+
+
+class TransactionStatus(Enum):
+    UNKNOWN = 0
+    RECEIVED = auto()
+    PENDING = auto()
+    ACCEPTED_ON_L2 = auto()
+    ACCEPTED_ON_L1 = auto()
+    REJECTED = auto()
+
+
+@dataclass
+class TransactionReceipt:
+    """
+    Dataclass representing details of sent transaction
+    """
+
+    hash: int
+    status: TransactionStatus
+    events: List[Event]
+    l1_to_l2_consumed_message: L1toL2Message
+    l2_to_l1_messages: List[L2toL1Message]
+
+
+@dataclass
+class SentTransaction:
+    hash: int
+    code: str
+
+
+class BlockStatus(Enum):
+    """
+    Enum representing block status
+    """
+
+    NOT_RECEIVED = 0
+    RECEIVED = auto()
+    PENDING = auto()
+    REJECTED = auto()
+    ACCEPTED_ON_L2 = auto()
+    ACCEPTED_ON_L1 = auto()
+
+
+@dataclass
+class StarknetBlock:
+    """
+    Dataclass representing a transaction on starknet
+    """
+
+    block_hash: int
+    parent_block_hash: int
+    block_number: int
+    status: BlockStatus
+    root: int
+    transactions: List[int]
+    transactions_details: Optional[List[Transaction]]
+    timestamp: int
+
+
+@dataclass
+class BlockState:
+    """
+    Dataclass representing a change in state of a block
+    """
+
+    block_hash: int
+    root: int
+    timestamp: int
+    # TODO add proper diff dataclass
+    diff: Any
+
+
+@dataclass
+class ContractCode:
+    bytecode: List[int]
+    abi: Dict[str, Any]
