420c392a975c7bedc1153964bfdec352487e9fe7
Artur Michalek <artur.michalek@swmansion.com>
2022-04-08 15:01:02 +0200

    Move common schemas to separate package

---
+++ starknet_py/net/common_schemas/common_schemas.py
@@ -0,0 +1,72 @@
+from typing import Any, Union, Mapping
+
+from marshmallow import fields, ValidationError
+
+from starknet_py.net.client_models import TransactionStatus, BlockStatus
+
+
+class Felt(fields.Field):
+    # TODO test that serialization and deserialization is correct
+    """
+    Field that serializes int to felt (hex encoded string)
+    """
+
+    def _serialize(self, value: Any, attr: str, obj: Any, **kwargs):
+        if value is None:
+            return None
+        return str(hex(value))
+
+    def _deserialize(
+        self,
+        value: Any,
+        attr: Union[str, None],
+        data: Union[Mapping[str, Any], None],
+        **kwargs,
+    ):
+        try:
+            assert isinstance(value, str) and value.startswith("0x")
+            return int(value, 16)
+        except (ValueError, AssertionError) as error:
+            raise ValidationError("Invalid felt") from error
+
+
+class StatusField(fields.Field):
+    def _serialize(self, value: Any, attr: str, obj: Any, **kwargs):
+        # TODO should we serialize to string?
+        return value.name if value is not None else ""
+
+    def _deserialize(
+        self,
+        value: Any,
+        attr: Union[str, None],
+        data: Union[Mapping[str, Any], None],
+        **kwargs,
+    ):
+        # TODO maybe simplify
+        enum_values = {v.name: k for k, v in enumerate(TransactionStatus)}
+
+        if value not in enum_values:
+            raise ValidationError("Invalid TransactionStatus enum key")
+
+        return TransactionStatus(enum_values[value])
+
+
+class BlockStatusField(fields.Field):
+    def _serialize(self, value: Any, attr: str, obj: Any, **kwargs):
+        # TODO should we serialize to string?
+        return value.name if value is not None else ""
+
+    def _deserialize(
+        self,
+        value: Any,
+        attr: Union[str, None],
+        data: Union[Mapping[str, Any], None],
+        **kwargs,
+    ):
+        # TODO maybe simplify
+        enum_values = {v.name: k for k, v in enumerate(BlockStatus)}
+
+        if value not in enum_values:
+            raise ValidationError("Invalid BlockStatus enum key")
+
+        return BlockStatus(enum_values[value])
--- starknet_py/net/rpc_schemas/rpc_schemas.py
+++ starknet_py/net/rpc_schemas/rpc_schemas.py
@@ -1,81 +1,16 @@
 import json
-from typing import Any, Mapping, Union
-from marshmallow import Schema, fields, post_load, ValidationError
+from marshmallow import Schema, fields, post_load
 
 from starknet_py.net.client_models import (
     Transaction,
-    TransactionStatus,
     ContractCode,
-    BlockStatus,
     StarknetBlock,
 )
-
-
-class Felt(fields.Field):
-    # TODO test that serialization and deserialization is correct
-    """
-    Field that serializes int to felt (hex encoded string)
-    """
-
-    def _serialize(self, value: Any, attr: str, obj: Any, **kwargs):
-        if value is None:
-            return None
-        return str(hex(value))
-
-    def _deserialize(
-        self,
-        value: Any,
-        attr: Union[str, None],
-        data: Union[Mapping[str, Any], None],
-        **kwargs,
-    ):
-        try:
-            assert isinstance(value, str) and value.startswith("0x")
-            return int(value, 16)
-        except (ValueError, AssertionError) as error:
-            raise ValidationError("Invalid felt") from error
-
-
-class StatusField(fields.Field):
-    def _serialize(self, value: Any, attr: str, obj: Any, **kwargs):
-        # TODO should we serialize to string?
-        return value.name if value is not None else ""
-
-    def _deserialize(
-        self,
-        value: Any,
-        attr: Union[str, None],
-        data: Union[Mapping[str, Any], None],
-        **kwargs,
-    ):
-        # TODO maybe simplify
-        enum_values = {v.name: k for k, v in enumerate(TransactionStatus)}
-
-        if value not in enum_values:
-            raise ValidationError("Invalid TransactionStatus enum key")
-
-        return TransactionStatus(enum_values[value])
-
-
-class BlockStatusField(fields.Field):
-    def _serialize(self, value: Any, attr: str, obj: Any, **kwargs):
-        # TODO should we serialize to string?
-        return value.name if value is not None else ""
-
-    def _deserialize(
-        self,
-        value: Any,
-        attr: Union[str, None],
-        data: Union[Mapping[str, Any], None],
-        **kwargs,
-    ):
-        # TODO maybe simplify
-        enum_values = {v.name: k for k, v in enumerate(BlockStatus)}
-
-        if value not in enum_values:
-            raise ValidationError("Invalid BlockStatus enum key")
-
-        return BlockStatus(enum_values[value])
+from starknet_py.net.common_schemas.common_schemas import (
+    Felt,
+    BlockStatusField,
+    StatusField,
+)
 
 
 class FunctionCallSchema(Schema):
@@ -103,12 +38,14 @@ class EventSchema(Schema):
 
 
 class L1toL2MessageSchema(Schema):
+    # TODO handle missing fields
     l1_address = Felt(data_key="from_address")
     l2_address = Felt()
     payload = fields.List(Felt(), data_key="payload")
 
 
 class L2toL1MessageSchema(Schema):
+    # TODO handle missing fields
     l2_address = Felt()
     l1_address = Felt(data_key="to_address")
     payload = fields.List(Felt(), data_key="payload")
