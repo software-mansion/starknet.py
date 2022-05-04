53066b26e90918713585fc025de23705fd970914
Artur Michalek <artur.michalek@swmansion.com>
2022-05-04 10:19:34 +0200

    Refactor transaction exceptions

--- starknet_py/transaction_exceptions.py
+++ starknet_py/transaction_exceptions.py
@@ -6,14 +6,18 @@ class TransactionFailedError(Exception):
     Base exception for transaction failure
     """
 
-    def __init__(self, code: Optional[str] = None, message: Optional[str] = None):
+    def __init__(
+        self,
+        code: Optional[str] = None,
+        message: Optional[str] = "Unknown starknet error",
+    ):
         self.code = code
-        self.message = message or "Unknown starknet error"
+        self.message = message
         super().__init__(self.message)
 
     def __str__(self):
         return (
-            f"Transaction failed with following error: "
+            f"Transaction failed with following starknet error: "
             f"{self.code + ':' if self.code is not None else ''}{self.message}."
         )
 
@@ -22,9 +26,6 @@ class TransactionRejectedError(TransactionFailedError):
     """
     Exception for transactions rejected by starknet
     """
-
-    def __init__(self, message: str, code: Optional[str] = None):
-        super().__init__(code=code, message=message)
 
     def __str__(self):
         return (
@@ -42,4 +43,4 @@ class TransactionNotReceivedError(TransactionFailedError):
         super().__init__(message="Transaction not received")
 
     def __str__(self):
-        return "Transaction was not received on starknet."
+        return "Transaction was not received on starknet"
