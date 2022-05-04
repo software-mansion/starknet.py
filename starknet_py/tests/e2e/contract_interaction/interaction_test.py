aa18a8cae70c290af8eebd3cbebd4bd54b67a20d
Artur Michalek <artur.michalek@swmansion.com>
2022-05-04 11:22:53 +0200

    Add e2e tests

--- starknet_py/tests/e2e/contract_interaction/interaction_test.py
+++ starknet_py/tests/e2e/contract_interaction/interaction_test.py
@@ -4,8 +4,9 @@ from pathlib import Path
 import pytest
 from starkware.starknet.public.abi import get_selector_from_name
 
+from starknet_py.transaction_exceptions import TransactionRejectedError
 from starknet_py.contract import Contract
-from starknet_py.net.client import BadRequest
+from starknet_py.net.client import BadRequest, Client
 from starknet_py.net.models import InvokeFunction
 from starknet_py.tests.e2e.utils import DevnetClient, DevnetClientWithoutAccount
 from starknet_py.utils.crypto.facade import sign_calldata
@@ -310,3 +311,34 @@ async def test_call_unitinialized_contract():
         )
 
     assert "500" in str(exinfo.value)
+
+
+@pytest.mark.asyncio
+async def test_wait_for_tx_devnet():
+    client = await DevnetClient.make_devnet_client()
+
+    deployment = await Contract.deploy(compilation_source=map_source, client=client)
+    await client.wait_for_tx(deployment.hash)
+
+
+@pytest.mark.asyncio
+async def test_wait_for_tx_testnet():
+    client = Client(net="testnet")
+
+    deployment = await Contract.deploy(compilation_source=map_source, client=client)
+    await client.wait_for_tx(deployment.hash)
+
+
+@pytest.mark.asyncio
+async def test_wait_for_tx_throws_on_transaction_rejected():
+    client = await DevnetClient.make_devnet_client()
+    deploy = await Contract.deploy(compilation_source=map_source, client=client)
+    contract = deploy.deployed_contract
+    invoke = contract.functions["put"].prepare(key=0x1, value=0x1, max_fee=0)
+
+    # modify selector so that transaction will get rejected
+    invoke.selector = 0x0123
+    transaction = await invoke.invoke()
+
+    with pytest.raises(TransactionRejectedError):
+        await client.wait_for_tx(transaction.hash)
