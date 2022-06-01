2c0c28d361dee6a45b7b1f2cc660f9a72d1238c0
Artur Michalek <artur.michalek@swmansion.com>
2022-06-01 15:55:22 +0200

    Remove keys abstractmethods

--- starknet_py/net/signer/base_signer.py
+++ starknet_py/net/signer/base_signer.py
@@ -6,22 +6,6 @@ from starknet_py.net.models.transaction import Transaction
 
 class BaseSigner(ABC):
     @abstractmethod
-    def public_key(self) -> int:
-        """
-        Get public key
-
-        :return: public key
-        """
-
-    @abstractmethod
-    def private_key(self) -> int:
-        """
-        Get private key
-
-        :return: private key
-        """
-
-    @abstractmethod
     def sign_transaction(self, transaction: Transaction) -> Tuple[int, int]:
         """
         Sign a transaction and return signature as two integers 'r' and 's'
