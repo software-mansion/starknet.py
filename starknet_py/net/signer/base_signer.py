d2f9268efab9fdb40195cd697a7f49850a5f4cf7
Artur Michalek <artur.michalek@swmansion.com>
2022-06-01 15:56:57 +0200

    Add docstring to class

--- starknet_py/net/signer/base_signer.py
+++ starknet_py/net/signer/base_signer.py
@@ -5,6 +5,10 @@ from starknet_py.net.models.transaction import Transaction
 
 
 class BaseSigner(ABC):
+    """
+    Base class for transaction signer. Implement methods from this ABC to use a custom signer in AccountClient
+    """
+
     @abstractmethod
     def sign_transaction(self, transaction: Transaction) -> Tuple[int, int]:
         """
