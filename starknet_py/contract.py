65a2d009947945f757a80d608273dc9f70763632
war-in <61014013+war-in@users.noreply.github.com>
2022-06-08 15:58:02 +0200

    Warn if max fee is zero (#189)
    * Warn when max_fee is 0
    
    * Better message

--- starknet_py/contract.py
+++ starknet_py/contract.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import dataclasses
 import sys
+import warnings
 from dataclasses import dataclass
 from functools import lru_cache
 from typing import (
@@ -229,6 +230,11 @@ class PreparedFunctionCall:
 
         if self.max_fee is None:
             raise ValueError("Max_fee must be specified when invoking a transaction")
+
+        if self.max_fee == 0:
+            warnings.warn(
+                "Transaction will fail with max_fee set to 0. Change it to a higher value."
+            )
 
         tx = self._make_invoke_function(signature=signature)
         response = await self._client.add_transaction(tx=tx)
