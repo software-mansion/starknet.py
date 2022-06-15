6917886ccd4cf495b61892139e0ae2da3f77a8bf
war-in <marcin.warchol20@gmail.com>
2022-06-15 13:56:22 +0200

    review changes

--- starknet_py/tests/e2e/client/client_test.py
+++ starknet_py/tests/e2e/client/client_test.py
@@ -12,6 +12,14 @@ directory = os.path.dirname(__file__)
 map_source = Path(directory, "map.cairo").read_text("utf-8")
 
 
+def represents_int(string):
+    try:
+        int(string, 16)
+        return True
+    except ValueError:
+        return False
+
+
 @pytest.mark.asyncio
 async def test_get_class_hash_at(run_devnet):
     client = await DevnetClientFactory(run_devnet).make_devnet_client()
@@ -24,17 +32,17 @@ async def test_get_class_hash_at(run_devnet):
 
     class_hash = await client.get_class_hash_at(contract_address=contract.address)
 
-    assert class_hash is not None
+    assert represents_int(class_hash) is True
 
 
 @pytest.mark.asyncio
 async def test_get_class_hash_at_error_when_contract_not_deployed(run_devnet):
     client = await DevnetClientFactory(run_devnet).make_devnet_client()
 
-    with pytest.raises(BadRequest) as breq:
+    with pytest.raises(BadRequest) as bad_request:
         await client.get_class_hash_at(contract_address="0x1")
 
-    assert "Contract with address 0x1 is not deployed" in str(breq.value)
+    assert "Contract with address 0x1 is not deployed" in str(bad_request.value)
 
 
 @pytest.mark.asyncio
@@ -44,14 +52,14 @@ async def test_get_class_by_hash(run_devnet):
     declare_result = await client.declare(compilation_source=map_source)
     class_hash = declare_result["class_hash"]
 
-    assert await client.get_class_by_hash(class_hash) is not None
+    assert isinstance(await client.get_class_by_hash(class_hash), dict)
 
 
 @pytest.mark.asyncio
 async def test_get_class_by_hash_error_when_contract_not_deployed(run_devnet):
     client = await DevnetClientFactory(run_devnet).make_devnet_client()
 
-    with pytest.raises(BadRequest) as breq:
+    with pytest.raises(BadRequest) as bad_request:
         await client.get_class_by_hash(1)
 
-    assert "Class with hash 0x1 is not declared" in str(breq.value)
+    assert "Class with hash 0x1 is not declared" in str(bad_request.value)
