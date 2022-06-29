5b26c821af40f102705533335c3a3e0fdb19b0b0
Artur Michalek <artur.michalek@swmansion.com>
2022-06-29 14:28:06 +0200

    Update docs

--- starknet_py/tests/e2e/docs/guide/test_handling_client_errors.py
+++ starknet_py/tests/e2e/docs/guide/test_handling_client_errors.py
@@ -1,13 +1,13 @@
 import pytest
 
-from starknet_py.tests.e2e.utils import DevnetClientFactory
+from starknet_py.net.models import StarknetChainId
 
 
 @pytest.mark.asyncio
 async def test_handling_client_errors(run_devnet):
     # pylint: disable=import-outside-toplevel
     # add to docs: start
-    from starknet_py.net.client import GatewayClient
+    from starknet_py.net.gateway_client import GatewayClient
     from starknet_py.net.networks import TESTNET
     from starknet_py.net.client_errors import ContractNotFoundError
     from starknet_py.contract import Contract
@@ -16,7 +16,7 @@ async def test_handling_client_errors(run_devnet):
         contract_address = "1"  # Doesn't exist
         client = GatewayClient(TESTNET)
         # add to docs: end
-        client = await DevnetClientFactory(run_devnet).make_devnet_client()
+        client = GatewayClient(net=run_devnet, chain=StarknetChainId.TESTNET)
         # add to docs: start
         await Contract.from_address(contract_address, client)
     except ContractNotFoundError as error:
