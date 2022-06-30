f6627a264a6c95b4a1edec34402d483b57629077
Arcticae <tomekgsd@gmail.com>
2022-06-30 14:26:16 +0200

    Review comments

--- starknet_py/transactions/deploy.py
+++ starknet_py/transactions/deploy.py
@@ -2,18 +2,31 @@ from typing import Union, List, Optional
 
 from starkware.starknet.definitions.fields import ContractAddressSalt
 
+from starknet_py.common import create_compiled_contract
+from starknet_py.compile.compiler import StarknetCompilationSource
 from starknet_py.net.client_models import ContractClass
 from starknet_py.net.models import Deploy
 
 
+# pylint: disable=too-many-arguments
 def make_deploy_tx(
-        compiled_contract: Union[ContractClass, str],
-        constructor_calldata: List[int],
+        compilation_source: Optional[StarknetCompilationSource] = None,
+        compiled_contract: Optional[Union[str, ContractClass]] = None,
+        constructor_calldata: List[int] = None,
         salt: Optional[int] = None,
         version: int = 0,
+        cairo_path: Optional[List[str]] = None,
 ) -> Deploy:
+    if not constructor_calldata:
+        constructor_calldata = []
+
     if isinstance(compiled_contract, str):
         compiled_contract = ContractClass.loads(compiled_contract)
+
+    if not compiled_contract:
+        compiled_contract = create_compiled_contract(
+            compilation_source, compiled_contract, cairo_path
+        )
 
     return Deploy(
         contract_address_salt=ContractAddressSalt.get_random_value()
