b14c0e7de361d214825ce30fa26ae95c3332199e
Arcticae <tomekgsd@gmail.com>
2022-07-05 14:14:32 +0200

    Format and lint

--- starknet_py/transactions/declare.py
+++ starknet_py/transactions/declare.py
@@ -1,10 +1,9 @@
-from typing import Optional, List, Union
+from typing import Optional, List
 
 from starkware.starknet.services.api.gateway.transaction import DECLARE_SENDER_ADDRESS
 
 from starknet_py.common import create_compiled_contract
 from starknet_py.compile.compiler import StarknetCompilationSource
-from starknet_py.net.client_models import ContractClass
 from starknet_py.net.models.transaction import Declare
 
 
@@ -19,7 +18,8 @@ def make_declare_tx(
     Either `compilation_source` or `compiled_contract` is required.
 
     :param compilation_source: string containing source code or a list of source files paths
-    :param compiled_contract: string containing compiled contract bytecode. Useful for reading compiled contract from a file
+    :param compiled_contract: string containing compiled contract bytecode.
+                              Useful for reading compiled contract from a file
     :param version: PreparedFunctionCall version
     :param cairo_path: a ``list`` of paths used by starknet_compile to resolve dependencies within contracts
     :return: A "Declare" transaction object
