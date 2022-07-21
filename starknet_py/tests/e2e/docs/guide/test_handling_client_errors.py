6d104e0e3a400b3b8d8da73dcadb5fff33898ca7
Artur Michalek <artur.michalek@swmansion.com>
2022-07-21 10:35:21 +0200

    Make example in guide broader

--- starknet_py/tests/e2e/docs/guide/test_handling_client_errors.py
+++ starknet_py/tests/e2e/docs/guide/test_handling_client_errors.py
@@ -7,7 +7,7 @@ async def test_handling_client_errors(account_client):
     # add to docs: start
     from starknet_py.net.gateway_client import GatewayClient
     from starknet_py.net.networks import TESTNET
-    from starknet_py.net.client_errors import ContractNotFoundError
+    from starknet_py.net.client_errors import ClientError
     from starknet_py.contract import Contract
 
     try:
@@ -17,6 +17,6 @@ async def test_handling_client_errors(account_client):
         client = account_client
         # add to docs: start
         await Contract.from_address(contract_address, client)
-    except ContractNotFoundError as error:
+    except ClientError as error:
         print(error.code, error.message)
     # add to docs: end
