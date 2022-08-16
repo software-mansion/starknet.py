9c0685a775c61b57646ac009e5e39f04b705dce3
drknzz <kamil.jankowski.x@gmail.com>
2022-08-16 12:17:20 +0200

    Add check for unnecessary kwargs

--- starknet_py/utils/data_transformer/data_transformer.py
+++ starknet_py/utils/data_transformer/data_transformer.py
@@ -344,6 +344,10 @@ class CairoSerializer:
                 f"Provided {len(args)} positional arguments, {len(type_by_name)} possible."
             )
 
+        for key, val in named_arguments.items():
+            if key not in type_by_name:
+                raise TypeError(f"Unnecessary named argument {key}={val} provided.")
+
         # Assign args to named arguments
         for arg, input_name in zip(args, type_by_name.keys()):
             if input_name in named_arguments:
