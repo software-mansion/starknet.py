42c7e64f8d4685909e2e6b880498747d9f8930f0
war-in <marcin.warchol20@gmail.com>
2022-09-07 12:11:20 +0200

    get rid of _sync methods in the script

--- starknet_py/tests/e2e/client/prepare_net_for_gateway_test.py
+++ starknet_py/tests/e2e/client/prepare_net_for_gateway_test.py
@@ -3,39 +3,41 @@ from starknet_py.net import AccountClient
 from starknet_py.transactions.declare import make_declare_tx
 
 
-def prepare_net_for_tests(account_client: AccountClient, compiled_contract: str):
+async def prepare_net_for_tests(account_client: AccountClient, compiled_contract: str):
     # pylint: disable=no-member, too-many-locals
-    deployment_result = Contract.deploy_sync(
+    deployment_result = await Contract.deploy(
         client=account_client, compiled_contract=compiled_contract
     )
-    deployment_result = deployment_result.wait_for_acceptance_sync(wait_for_accept=True)
+    deployment_result = await deployment_result.wait_for_acceptance(
+        wait_for_accept=True
+    )
     contract = deployment_result.deployed_contract
 
     contract_address = contract.address
     deploy_transaction_hash = deployment_result.hash
-    deploy_receipt = account_client.get_transaction_receipt_sync(
+    deploy_receipt = await account_client.get_transaction_receipt(
         deploy_transaction_hash
     )
     block_with_deploy_number = deploy_receipt.block_number
     block_with_deploy_hash = deploy_receipt.block_hash
 
-    invoke_res = contract.functions["increase_balance"].invoke_sync(
+    invoke_res = await contract.functions["increase_balance"].invoke(
         amount=1234, max_fee=int(1e20)
     )
-    invoke_res.wait_for_acceptance_sync()
+    await invoke_res.wait_for_acceptance()
 
     invoke_transaction_hash = invoke_res.hash
-    block_with_invoke_number = account_client.get_transaction_receipt_sync(
-        invoke_transaction_hash
+    block_with_invoke_number = (
+        await account_client.get_transaction_receipt(invoke_transaction_hash)
     ).block_number
 
     declare_tx = make_declare_tx(compiled_contract=compiled_contract)
-    declare_result = account_client.declare_sync(declare_tx)
-    account_client.wait_for_tx_sync(declare_result.transaction_hash)
+    declare_result = await account_client.declare(declare_tx)
+    await account_client.wait_for_tx(declare_result.transaction_hash)
 
     declare_transaction_hash = declare_result.transaction_hash
-    block_with_declare_number = account_client.get_transaction_receipt_sync(
-        declare_transaction_hash
+    block_with_declare_number = (
+        await account_client.get_transaction_receipt(declare_transaction_hash)
     ).block_number
 
     return (
