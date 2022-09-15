17999926d0a5dd8889a77d830683d50afc2954cc
war-in <61014013+war-in@users.noreply.github.com>
2022-09-15 11:08:54 +0200

    Integration testing improved (#368)
    * done!
    
    * format
    
    * add possibility to do this: "--net integration"

--- starknet_py/tests/e2e/client/full_node_test.py
+++ starknet_py/tests/e2e/client/full_node_test.py
@@ -9,6 +9,7 @@ from starknet_py.net.client_models import (
 )
 
 
+@pytest.mark.run_on_devnet
 @pytest.mark.asyncio
 async def test_node_get_transaction_by_block_id_and_index(
     block_with_deploy_number,
@@ -32,6 +33,7 @@ async def test_node_get_transaction_by_block_id_and_index(
     )
 
 
+@pytest.mark.run_on_devnet
 @pytest.mark.asyncio
 async def test_node_get_deploy_transaction_by_block_id_and_index(
     deploy_transaction_hash,
@@ -55,6 +57,7 @@ async def test_node_get_deploy_transaction_by_block_id_and_index(
     )
 
 
+@pytest.mark.run_on_devnet
 @pytest.mark.asyncio
 async def test_node_get_declare_transaction_by_block_number_and_index(
     declare_transaction_hash, block_with_declare_number, rpc_client, class_hash
@@ -68,6 +71,7 @@ async def test_node_get_declare_transaction_by_block_number_and_index(
     assert tx.version == 1
 
 
+@pytest.mark.run_on_devnet
 @pytest.mark.asyncio
 async def test_get_class_at(rpc_client, contract_address):
     declared_contract = await rpc_client.get_class_at(
@@ -78,6 +82,7 @@ async def test_get_class_at(rpc_client, contract_address):
     assert declared_contract.entry_points_by_type is not None
 
 
+@pytest.mark.run_on_devnet
 @pytest.mark.asyncio
 async def test_get_class_at_throws_on_wrong_address(rpc_client):
     with pytest.raises(ClientError) as err:
@@ -86,6 +91,7 @@ async def test_get_class_at_throws_on_wrong_address(rpc_client):
     assert "Client failed with code 20: Contract not found" == str(err.value)
 
 
+@pytest.mark.run_on_devnet
 @pytest.mark.asyncio
 async def test_block_transaction_count(rpc_client):
     latest_block = (await rpc_client.get_block("latest")).block_number
