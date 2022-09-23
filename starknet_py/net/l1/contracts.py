5d889809b59ea1dc0bca836d2968fe6d3e355861
Jakub Ptak <jakub.ptak@swmansion.com>
2022-09-23 11:31:47 +0200

    Remove reportGeneralTypeIssues from net/l1/contracts

--- starknet_py/net/l1/contracts.py
+++ starknet_py/net/l1/contracts.py
@@ -1,5 +1,3 @@
-# pyright: reportGeneralTypeIssues=false
-
 import typing
 from typing import Sequence
 
@@ -10,7 +8,7 @@ from web3 import Web3
 from web3.types import (
     BlockIdentifier as EthBlockIdentifier,
     ABIFunction,
-    ABIFunctionParams,
+    ABIFunctionParams, TxParams,
 )
 from eth_typing import (
     ChecksumAddress,
@@ -39,7 +37,7 @@ class StarknetL1Contract:
         )
 
     def l2_to_l1_messages(
-        self, msg_hash: bytes, block_number: EthBlockIdentifier = None
+        self, msg_hash: bytes, block_number: typing.Optional[EthBlockIdentifier] = None
     ) -> bytes:
         abi: ABIFunction = {
             "inputs": typing.cast(
@@ -59,7 +57,7 @@ class StarknetL1Contract:
             prepare_transaction(
                 address=typing.cast(ChecksumAddress, self.contract_address),
                 w3=self.w3,
-                transaction={"value": 0},
+                transaction=typing.cast(TxParams, {"value": 0}),
                 fn_identifier="l2ToL1Messages",
                 fn_abi=abi,
                 fn_args=[msg_hash],
@@ -68,7 +66,7 @@ class StarknetL1Contract:
         )
 
     def l1_to_l2_messages(
-        self, msg_hash: bytes, block_number: EthBlockIdentifier = None
+        self, msg_hash: bytes, block_number: typing.Optional[EthBlockIdentifier] = None
     ) -> bytes:
         abi: ABIFunction = {
             "inputs": typing.cast(
@@ -88,7 +86,7 @@ class StarknetL1Contract:
             prepare_transaction(
                 address=self.contract_address,
                 w3=self.w3,
-                transaction={"value": 0},
+                transaction=typing.cast(TxParams, {"value": 0}),
                 fn_identifier="l1ToL2Messages",
                 fn_abi=abi,
                 fn_args=[msg_hash],
