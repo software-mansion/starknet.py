36e5e6dbdcf9000822426ed797de72aaa129e533
Artur Michalek <artur.michalek@swmansion.com>
2022-10-07 13:08:58 +0200

    Initial support for signing DeployAccount

--- starknet_py/net/signer/stark_curve_signer.py
+++ starknet_py/net/signer/stark_curve_signer.py
@@ -4,19 +4,23 @@ from typing import List
 from starkware.crypto.signature.signature import (
     private_to_stark_key,
 )
+from starkware.starknet.core.os.contract_address.contract_address import (
+    calculate_contract_address_from_hash,
+)
 from starkware.starknet.core.os.transaction_hash.transaction_hash import (
     calculate_transaction_hash_common,
     TransactionHashPrefix,
     calculate_declare_transaction_hash,
+    calculate_deploy_account_transaction_hash,
 )
 
-from starknet_py.net.client_models import Declare, InvokeFunction
 from starknet_py.net.models import (
     AddressRepresentation,
     StarknetChainId,
     Transaction,
     parse_address,
 )
+from starknet_py.net.models.transaction import DeployAccount, Declare, InvokeFunction
 from starknet_py.net.signer.base_signer import BaseSigner
 from starknet_py.utils.crypto.facade import message_signature
 from starknet_py.utils.typed_data import TypedData as TypedDataDataclass
@@ -58,6 +62,8 @@ class StarkCurveSigner(BaseSigner):
     ) -> List[int]:
         if isinstance(transaction, Declare):
             return self._sign_declare_transaction(transaction)
+        if isinstance(transaction, DeployAccount):
+            return self._sign_deploy_account_transaction(transaction)
         return self._sign_transaction(transaction)
 
     def _sign_transaction(self, transaction: InvokeFunction):
@@ -90,7 +96,30 @@ class StarkCurveSigner(BaseSigner):
         r, s = message_signature(msg_hash=tx_hash, priv_key=self.private_key)
         return [r, s]
 
+    def _sign_deploy_account_transaction(self, transaction: DeployAccount) -> List[int]:
+        contract_address = calculate_contract_address_from_hash(
+            salt=transaction.contract_address_salt,
+            class_hash=transaction.class_hash,
+            constructor_calldata=transaction.constructor_calldata,
+            deployer_address=0,
+        )
+        tx_hash = calculate_deploy_account_transaction_hash(
+            contract_address=contract_address,
+            class_hash=transaction.class_hash,
+            constructor_calldata=transaction.constructor_calldata,
+            salt=transaction.contract_address_salt,
+            max_fee=transaction.max_fee,
+            version=transaction.version,
+            chain_id=self.chain_id,
+            nonce=transaction.nonce,
+        )
+        # pylint: disable=invalid-name
+        r, s = message_signature(msg_hash=tx_hash, priv_key=self.private_key)
+        return [r, s]
+
     def sign_message(self, typed_data: TypedData, account_address: int) -> List[int]:
         typed_data_dataclass = TypedDataDataclass.from_dict(data=typed_data)
         msg_hash = typed_data_dataclass.message_hash(account_address)
-        return message_signature(msg_hash=msg_hash, priv_key=self.private_key)
+        # pylint: disable=invalid-name
+        r, s = message_signature(msg_hash=msg_hash, priv_key=self.private_key)
+        return [r, s]
