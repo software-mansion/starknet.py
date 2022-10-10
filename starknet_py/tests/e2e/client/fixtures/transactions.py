593f800439e883759d6d3e2101616b8aefe84432
war-in <61014013+war-in@users.noreply.github.com>
2022-10-10 13:25:45 +0200

    E2E tests for signing and deploying account (#442)
    * tests added
    
    * fix import
    
    * review suggestion
    
    * add docstrings

--- starknet_py/tests/e2e/client/fixtures/transactions.py
+++ starknet_py/tests/e2e/client/fixtures/transactions.py
@@ -1,30 +1,31 @@
-from pathlib import Path
-
 import pytest_asyncio
 
 from starknet_py.net import AccountClient
+from starknet_py.net.gateway_client import GatewayClient
+from starknet_py.net.models import StarknetChainId
 from starknet_py.net.models.transaction import DeployAccount
-from starknet_py.tests.e2e.conftest import contracts_dir, MAX_FEE
+from starknet_py.tests.e2e.conftest import MAX_FEE
 
 
 @pytest_asyncio.fixture
 async def deploy_account_transaction(
-    new_gateway_account_client: AccountClient,
+    details_of_account_to_be_deployed, network: str
 ) -> DeployAccount:
-    account_client = new_gateway_account_client
+    """
+    Returns a DeployAccount transaction
+    """
+    address, key_pair, salt, class_hash = details_of_account_to_be_deployed
 
-    contract_source = Path(
-        contracts_dir / "contract_with_validate_deploy.cairo"
-    ).read_text("utf-8")
-    declare_tx = await account_client.sign_declare_transaction(
-        compilation_source=contract_source, max_fee=MAX_FEE
+    account = AccountClient(
+        address=address,
+        client=GatewayClient(net=network),
+        key_pair=key_pair,
+        chain=StarknetChainId.TESTNET,
+        supported_tx_version=1,
     )
-    declare_result = await account_client.declare(declare_tx)
-    await account_client.wait_for_tx(declare_result.transaction_hash)
-
-    return await new_gateway_account_client.sign_deploy_account_transaction(
-        class_hash=declare_result.class_hash,
-        contract_address_salt=10,
-        constructor_calldata=[],
+    return await account.sign_deploy_account_transaction(
+        class_hash=class_hash,
+        contract_address_salt=salt,
+        constructor_calldata=[key_pair.public_key],
         max_fee=MAX_FEE,
     )
