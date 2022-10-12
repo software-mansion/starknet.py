9dab2801e12dc122054c1dc612e03158c6196676
war-in <marcin.warchol20@gmail.com>
2022-10-12 15:35:09 +0200

    test sign_declare

--- starknet_py/net/signer/test_stark_curve_signer.py
+++ starknet_py/net/signer/test_stark_curve_signer.py
@@ -1,8 +1,16 @@
+import os
+from pathlib import Path
+
 import pytest
 
+from starknet_py.common import create_compiled_contract
 from starknet_py.net.models import StarknetChainId
-from starknet_py.net.models.transaction import InvokeFunction, DeployAccount
+from starknet_py.net.models.transaction import InvokeFunction, DeployAccount, Declare
 from starknet_py.net.signer.stark_curve_signer import StarkCurveSigner, KeyPair
+
+contract_source = (
+    Path(os.path.dirname(__file__)) / "../../tests/e2e/mock/contracts/erc20.cairo"
+).read_text("utf-8")
 
 
 @pytest.mark.parametrize(
@@ -20,6 +28,14 @@ from starknet_py.net.signer.stark_curve_signer import StarkCurveSigner, KeyPair
             class_hash=0x1,
             contract_address_salt=0x2,
             constructor_calldata=[1, 2, 3, 4],
+            max_fee=10000,
+            signature=[],
+            nonce=23,
+            version=1,
+        ),
+        Declare(
+            contract_class=create_compiled_contract(compilation_source=contract_source),
+            sender_address=123,
             max_fee=10000,
             signature=[],
             nonce=23,
