f727bb745a49455b22d52d4d650add5f1b06cb59
war-in <marcin.warchol20@gmail.com>
2022-10-21 11:29:45 +0200

    estimate declare transaction

--- starknet_py/tests/e2e/account/account_client_test.py
+++ starknet_py/tests/e2e/account/account_client_test.py
@@ -108,6 +108,24 @@ async def test_estimated_fee_greater_than_zero(erc20_contract, account_client):
     )
 
 
+@pytest.mark.asyncio
+async def test_estimate_fee_for_declare_transaction(
+    new_gateway_account_client, map_source_code
+):
+    # TODO: add test for new_full_node_account_client once devnet supports RPC 0.2.0
+    declare_tx = await new_gateway_account_client.sign_declare_transaction(
+        compilation_source=map_source_code, max_fee=MAX_FEE
+    )
+
+    estimated_fee = await new_gateway_account_client.estimate_fee(tx=declare_tx)
+
+    assert isinstance(estimated_fee.overall_fee, int)
+    assert estimated_fee.overall_fee > 0
+    assert (
+        estimated_fee.gas_usage * estimated_fee.gas_price == estimated_fee.overall_fee
+    )
+
+
 @pytest.mark.run_on_devnet
 @pytest.mark.asyncio
 async def test_create_account_client(network):
