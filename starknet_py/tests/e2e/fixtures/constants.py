83c54e370f69bf8f1d1507da7246ab2f021ff7ed
war-in <marcin.warchol20@gmail.com>
2022-10-26 09:03:21 +0200

    path repaired

--- starknet_py/tests/e2e/fixtures/constants.py
+++ starknet_py/tests/e2e/fixtures/constants.py
@@ -31,6 +31,6 @@ INTEGRATION_NEW_ACCOUNT_ADDRESS = (
 
 MAX_FEE = int(1e20)
 
-mock_dir = Path(os.path.dirname(__file__)) / "mock"
+mock_dir = Path(os.path.dirname(__file__)) / "../mock"
 typed_data_dir = mock_dir / "typed_data"
 contracts_dir = mock_dir / "contracts"
