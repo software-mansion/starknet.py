67e53f464af982a66920899c89f279db07ae76f2
war-in <marcin.warchol20@gmail.com>
2022-11-09 12:28:25 +0100

    remove prepare_contract_deployment and unique

--- starknet_py/net/udc_deployer/deployer.py
+++ starknet_py/net/udc_deployer/deployer.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from collections import namedtuple
 from typing import Optional, List, Union, cast, NamedTuple
 
 from starkware.starknet.definitions.fields import ContractAddressSalt
@@ -8,8 +7,7 @@ from starkware.starknet.public.abi import get_selector_from_name
 
 from starknet_py.common import int_from_hex
 from starknet_py.constants import DEFAULT_DEPLOYER_ADDRESS
-from starknet_py.net import AccountClient
-from starknet_py.net.client_models import Hash, InvokeFunction, Call
+from starknet_py.net.client_models import Hash, Call
 from starknet_py.net.models import AddressRepresentation, parse_address, compute_address
 from starknet_py.utils.contructor_args_translator import translate_constructor_args
 from starknet_py.utils.crypto.facade import pedersen_hash
@@ -31,55 +29,21 @@ class Deployer:
 
     def __init__(
         self,
-        account: AccountClient,
         *,
         deployer_address: AddressRepresentation = DEFAULT_DEPLOYER_ADDRESS,
-        unique: bool = True,
+        account_address: Optional[AddressRepresentation] = None,
     ):
         """
-        :param account: AccountClient used to sign and send transactions
         :param deployer_address: Address of the UDC. Is set to the address of
             the default UDC (same address on real nets and devnet) by default.
             Must be set when using custom network other than devnet.
-        :param unique: Boolean determining if the salt should be connected with the account's address. Default to True
+        :param account_address: Address of an account which will send the transaction.
+            If passed, address of the contract to be deployed will be connected with it.
         """
 
-        self.account = account
         self.deployer_address = parse_address(deployer_address)
-        self.unique = unique
-
-    async def prepare_contract_deployment(
-        self,
-        *,
-        class_hash: Hash,
-        salt: Optional[int] = None,
-        abi: Optional[List] = None,
-        calldata: Optional[Union[List, dict]] = None,
-        max_fee: Optional[int] = None,
-        auto_estimate: bool = False,
-    ) -> InvokeFunction:
-        """
-        Prepares deploy invoke transaction
-
-        :param class_hash: The class_hash of the contract to be deployed
-        :param salt: The salt for a contract to be deployed. Random value is selected if it is not provided
-        :param abi: ABI of the contract to be deployed
-        :param calldata: Constructor args of the contract to be deployed
-        :param max_fee: Max amount of Wei to be paid when executing transaction
-        :param auto_estimate: Use automatic fee estimation, not recommend as it may lead to high costs
-        :return: NamedTuple with invoke_tx and address fields
-        """
-        call, address = await self.create_deployment_call(
-            class_hash=class_hash, salt=salt, abi=abi, calldata=calldata
-        )
-
-        transaction = await self.account.sign_invoke_transaction(
-            calls=call, max_fee=max_fee, auto_estimate=auto_estimate
-        )
-
-        return namedtuple(
-            typename="InvokeAndAddress", field_names=["invoke_tx", "address"]
-        )(transaction, address)
+        self.account_address = account_address
+        self.unique = True if account_address is not None else False
 
     async def create_deployment_call(
         self,
@@ -122,7 +86,7 @@ class Deployer:
         )
 
         deployer_address = self.deployer_address if self.unique else 0
-        salt = pedersen_hash(self.account.address, salt) if self.unique else salt
+        salt = pedersen_hash(self.account_address, salt) if self.unique else salt
         address = compute_address(
             class_hash=class_hash,
             constructor_calldata=constructor_calldata,
