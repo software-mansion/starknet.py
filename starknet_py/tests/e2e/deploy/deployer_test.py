93401336b78f2abb712d8875bbcec1c9af31dcd3
war-in <marcin.warchol20@gmail.com>
2022-11-14 10:05:50 +0100

    check environment before tests

--- starknet_py/tests/e2e/deploy/deployer_test.py
+++ starknet_py/tests/e2e/deploy/deployer_test.py
@@ -1,12 +1,8 @@
 import pytest
 
-from starknet_py.constants import DEFAULT_DEPLOYER_ADDRESS
 from starknet_py.contract import Contract
 from starknet_py.net.udc_deployer.deployer import Deployer
 from starknet_py.tests.e2e.utils import MAX_FEE
-from starknet_py.utils.data_transformer.universal_deployer_serializer import (
-    universal_deployer_abi,
-)
 
 
 @pytest.mark.asyncio
@@ -100,23 +96,6 @@ async def test_constructor_arguments_contract_deploy(
         sum([1, 2, 3]),
         {"value": 12, "nested_struct": {"value": 99}},
     )
-
-
-@pytest.mark.asyncio
-async def test_if_udc_is_deployed(client):
-    class_hash = await client.get_class_hash_at(
-        contract_address=DEFAULT_DEPLOYER_ADDRESS
-    )
-
-    assert isinstance(class_hash, int)
-    assert class_hash != 0
-
-
-@pytest.mark.asyncio
-async def test_if_udc_has_expected_abi(gateway_client):
-    code = await gateway_client.get_code(contract_address=DEFAULT_DEPLOYER_ADDRESS)
-
-    assert code.abi == universal_deployer_abi
 
 
 @pytest.mark.asyncio
