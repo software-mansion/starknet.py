1eb7921cc9acea24431838aab8d4fda56c5915a7
Artur Michalek <artur.michalek@swmansion.com>
2022-11-16 15:24:21 +0100

    Test deploy account general flow with new Account

--- starknet_py/tests/e2e/deploy_account/deploy_account_test.py
+++ starknet_py/tests/e2e/deploy_account/deploy_account_test.py
@@ -1,6 +1,7 @@
 import pytest
 
 from starknet_py.net import AccountClient
+from starknet_py.net.account.account import Account
 from starknet_py.net.gateway_client import GatewayClient
 from starknet_py.net.models import StarknetChainId
 from starknet_py.tests.e2e.account.account_client_test import MAX_FEE
@@ -26,6 +27,28 @@ async def test_general_flow(deploy_account_details_factory, network):
         max_fee=MAX_FEE,
     )
     resp = await account.deploy_account(transaction=deploy_account_tx)
+
+    assert resp.address == address
+
+
+@pytest.mark.asyncio
+async def test_general_flow_base_account(deploy_account_details_factory, network):
+    address, key_pair, salt, class_hash = await deploy_account_details_factory.get()
+
+    account = Account(
+        address=address,
+        client=GatewayClient(net=network),
+        key_pair=key_pair,
+        chain=StarknetChainId.TESTNET,
+    )
+
+    deploy_account_tx = await account.sign_deploy_account_transaction(
+        class_hash=class_hash,
+        contract_address_salt=salt,
+        constructor_calldata=[key_pair.public_key],
+        max_fee=MAX_FEE,
+    )
+    resp = await account.client.deploy_account(transaction=deploy_account_tx)
 
     assert resp.address == address
 
