c49a5ef4e6cbbe81a567093b6f25cc9781af9ac0
war-in <61014013+war-in@users.noreply.github.com>
2022-11-21 10:13:34 +0100

    `Deploying` section in the docs (#514)
    * simplified deployment flow poc
    
    * format
    
    * Update starknet_py/contract.py
    
    Co-authored-by: Artur Michałek <52135326+cptartur@users.noreply.github.com>
    
    * Contract.deploy_contract wip
    
    * Update starknet_py/contract.py
    
    Co-authored-by: Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
    
    * Update starknet_py/contract.py
    
    Co-authored-by: Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
    
    * Update starknet_py/contract.py
    
    Co-authored-by: Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
    
    * Contract.deploy_contract done
    
    * change assert to if
    
    * format
    
    * add DeclareResult to the API
    
    * update devnet fee contract address
    
    * improve an error
    
    * requested changes
    
    * add Deploying section, remove old `Deploying new contracts`
    
    * Update starknet_py/tests/e2e/docs/guide/test_deploying_in_multicall.py
    
    Co-authored-by: Artur Michałek <52135326+cptartur@users.noreply.github.com>
    
    * improve descriptions
    
    * mention `deploy_contract` in the deprecated `deploy`
    
    * make `deployer_address` keyword only
    
    Co-authored-by: Artur Michałek <52135326+cptartur@users.noreply.github.com>
    Co-authored-by: Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>

---
+++ starknet_py/tests/e2e/docs/guide/test_simple_deploy.py
@@ -0,0 +1,51 @@
+import pytest
+
+from starknet_py.common import create_compiled_contract
+
+
+@pytest.mark.asyncio
+async def test_simple_declare_and_deploy(
+    account_client, map_class_hash, map_compiled_contract
+):
+    # pylint: disable=import-outside-toplevel
+    # docs: start
+    from starknet_py.contract import Contract
+
+    # docs: end
+
+    class_hash = map_class_hash
+    abi = create_compiled_contract(compiled_contract=map_compiled_contract).abi
+
+    # docs: start
+    # To deploy contract just use `Contract.deploy_contract` method
+    # Note that class_hash and abi of the contract must be known
+
+    # If constructor of the contract requires arguments, pass constructor_args parameter
+    constructor_args = {"value": 10}
+
+    # docs: end
+    constructor_args = None
+
+    # docs: start
+    deploy_result = await Contract.deploy_contract(
+        account=account_client,
+        class_hash=class_hash,
+        abi=abi,
+        constructor_args=constructor_args,
+        max_fee=int(1e16),
+    )
+
+    # `Contract.deploy_contract` method have one more optional parameter
+    # `deployer_address` needs to be specified when using net other than mainnet/testnet or devnet
+    # Read more about it in the API section
+
+    # Wait for the transaction
+    await deploy_result.wait_for_acceptance()
+
+    # To interact with just deployed contract get its instance from the deploy_result
+    contract = deploy_result.deployed_contract
+
+    # Now, any of the contract functions can be called
+    # docs: end
+
+    assert contract.address != 0
