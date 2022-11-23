dc2fac8f99cac9d958f274245d1bbd51c108c70a
war-in <61014013+war-in@users.noreply.github.com>
2022-11-23 14:33:43 +0100

    fix tests (#529)

--- starknet_py/tests/e2e/proxy/proxy_test.py
+++ starknet_py/tests/e2e/proxy/proxy_test.py
@@ -9,6 +9,7 @@ from starknet_py.net.client_errors import ContractNotFoundError
 from starknet_py.net.models import Address
 from starknet_py.proxy.contract_abi_resolver import ProxyResolutionError
 from starknet_py.proxy.proxy_check import ProxyCheck
+from starknet_py.tests.e2e.fixtures.constants import MAX_FEE
 
 
 async def is_map_working_properly(map_contract: Contract, key: int, val: int) -> bool:
@@ -21,20 +22,14 @@ async def is_map_working_properly(map_contract: Contract, key: int, val: int) ->
 
 
 @pytest.mark.asyncio
-async def test_contract_from_address_no_proxy(account_client, map_compiled_contract):
-    deployment_result = await Contract.deploy(
-        compiled_contract=map_compiled_contract,
-        client=account_client,
-    )
-    await deployment_result.wait_for_acceptance()
-
+async def test_contract_from_address_no_proxy(account_client, map_contract):
     contract = await Contract.from_address(
-        address=deployment_result.deployed_contract.address,
+        address=map_contract.address,
         client=account_client,
     )
 
     assert contract.functions.keys() == {"put", "get"}
-    assert contract.address == deployment_result.deployed_contract.address
+    assert contract.address == map_contract.address
     assert await is_map_working_properly(map_contract=contract, key=69, val=13)
 
 
@@ -112,29 +107,25 @@ async def test_contract_from_address_custom_proxy_check(
 
 @pytest.mark.asyncio
 async def test_contract_from_address_with_old_address_proxy(
-    account_client, old_proxy, map_compiled_contract
+    new_account_client, old_proxy, map_contract
 ):
-    map_deployment_result = await Contract.deploy(
-        compiled_contract=map_compiled_contract,
-        client=account_client,
+    account_client = new_account_client
+    declare_result = await Contract.declare(
+        account=account_client, compiled_contract=old_proxy, max_fee=MAX_FEE
     )
-    await map_deployment_result.wait_for_acceptance()
-
-    deployment_result = await Contract.deploy(
-        compiled_contract=old_proxy,
-        constructor_args={
-            "implementation_address": map_deployment_result.deployed_contract.address
-        },
-        client=account_client,
+    await declare_result.wait_for_acceptance()
+    deploy_result = await declare_result.deploy(
+        constructor_args={"implementation_address": map_contract.address},
+        max_fee=MAX_FEE,
     )
-    await deployment_result.wait_for_acceptance()
+    await deploy_result.wait_for_acceptance()
 
     proxy_contract = await Contract.from_address(
-        address=deployment_result.deployed_contract.address,
+        address=deploy_result.deployed_contract.address,
         client=account_client,
     )
     proxied_contract = await Contract.from_address(
-        address=deployment_result.deployed_contract.address,
+        address=deploy_result.deployed_contract.address,
         client=account_client,
         proxy_config=True,
     )
