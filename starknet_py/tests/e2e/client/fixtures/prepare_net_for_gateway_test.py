ce9c31173414041f8147721265f34369e5541241
war-in <marcin.warchol20@gmail.com>
2022-11-23 10:53:12 +0100

    update prepare_net_for_tests function

--- starknet_py/tests/e2e/client/fixtures/prepare_net_for_gateway_test.py
+++ starknet_py/tests/e2e/client/fixtures/prepare_net_for_gateway_test.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from starknet_py.contract import Contract
 from starknet_py.net import AccountClient
+from starknet_py.tests.e2e.fixtures.constants import MAX_FEE
 from starknet_py.tests.e2e.utils import (
     get_deploy_account_transaction,
     AccountToBeDeployedDetails,
@@ -12,13 +13,11 @@ from starknet_py.tests.e2e.utils import (
 class PreparedNetworkData:
     # pylint: disable=too-many-instance-attributes
     contract_address: int
-    deploy_transaction_hash: int
-    block_with_deploy_number: int
-    block_with_deploy_hash: int
     invoke_transaction_hash: int
     block_with_invoke_number: int
     declare_transaction_hash: int
     block_with_declare_number: int
+    block_with_declare_hash: int
     deploy_account_transaction_hash: int
     block_with_deploy_account_number: int
     block_with_deploy_account_hash: int
@@ -30,20 +29,20 @@ async def prepare_net_for_tests(
     deploy_account_details: AccountToBeDeployedDetails,
 ) -> PreparedNetworkData:
     # pylint: disable=too-many-locals
+    declare_result = await Contract.declare(
+        account=account_client, compiled_contract=compiled_contract, max_fee=MAX_FEE
+    )
+    await declare_result.wait_for_acceptance()
+    deploy_result = await declare_result.deploy(max_fee=MAX_FEE)
+    await deploy_result.wait_for_acceptance()
 
-    deployment_result = await Contract.deploy(
-        client=account_client, compiled_contract=compiled_contract
+    declare_receipt = await account_client.get_transaction_receipt(
+        declare_result.transaction_hash
     )
-    deployment_result = await deployment_result.wait_for_acceptance(
-        wait_for_accept=True
-    )
-    contract = deployment_result.deployed_contract
+    block_with_declare_number = declare_receipt.block_number
+    block_with_declare_hash = declare_receipt.block_hash
 
-    deploy_receipt = await account_client.get_transaction_receipt(
-        deployment_result.hash
-    )
-    block_with_deploy_number = deploy_receipt.block_number
-    block_with_deploy_hash = deploy_receipt.block_hash
+    contract = deploy_result.deployed_contract
 
     invoke_res = await contract.functions["increase_balance"].invoke(
         amount=1234, max_fee=int(1e20)
@@ -52,16 +51,6 @@ async def prepare_net_for_tests(
 
     block_with_invoke_number = (
         await account_client.get_transaction_receipt(invoke_res.hash)
-    ).block_number
-
-    declare_tx = await account_client.sign_declare_transaction(
-        compiled_contract=compiled_contract, max_fee=int(1e16)
-    )
-    declare_result = await account_client.declare(declare_tx)
-    await account_client.wait_for_tx(declare_result.transaction_hash)
-
-    block_with_declare_number = (
-        await account_client.get_transaction_receipt(declare_result.transaction_hash)
     ).block_number
 
     address, key_pair, salt, class_hash = deploy_account_details
@@ -81,22 +70,19 @@ async def prepare_net_for_tests(
     block_with_deploy_account_number = declare_account_receipt.block_number
     block_with_deploy_account_hash = declare_account_receipt.block_hash
 
-    assert block_with_deploy_number is not None
-    assert block_with_deploy_hash is not None
     assert block_with_invoke_number is not None
     assert block_with_declare_number is not None
+    assert block_with_declare_hash is not None
     assert block_with_deploy_account_number is not None
     assert block_with_deploy_account_hash is not None
 
     return PreparedNetworkData(
         contract_address=contract.address,
-        deploy_transaction_hash=deployment_result.hash,
-        block_with_deploy_number=block_with_deploy_number,
-        block_with_deploy_hash=block_with_deploy_hash,
         invoke_transaction_hash=invoke_res.hash,
         block_with_invoke_number=block_with_invoke_number,
         declare_transaction_hash=declare_result.transaction_hash,
         block_with_declare_number=block_with_declare_number,
+        block_with_declare_hash=block_with_declare_hash,
         deploy_account_transaction_hash=deploy_account_result.transaction_hash,
         block_with_deploy_account_hash=block_with_deploy_account_hash,
         block_with_deploy_account_number=block_with_deploy_account_number,
