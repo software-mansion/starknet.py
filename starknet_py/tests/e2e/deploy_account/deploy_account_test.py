ddca09c56d23d943d86cb79126e96559561fdb0e
Artur Michalek <artur.michalek@swmansion.com>
2022-11-30 10:40:06 +0100

    Use account in deploy_account_test.py

--- starknet_py/tests/e2e/deploy_account/deploy_account_test.py
+++ starknet_py/tests/e2e/deploy_account/deploy_account_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from starknet_py.net import AccountClient
+from starknet_py.net.account.account import Account
 from starknet_py.net.models import StarknetChainId
 from starknet_py.tests.e2e.account.account_client_test import MAX_FEE
 
@@ -9,12 +9,11 @@ from starknet_py.tests.e2e.account.account_client_test import MAX_FEE
 async def test_general_flow(client, deploy_account_details_factory):
     address, key_pair, salt, class_hash = await deploy_account_details_factory.get()
 
-    account = AccountClient(
+    account = Account(
         address=address,
         client=client,
         key_pair=key_pair,
         chain=StarknetChainId.TESTNET,
-        supported_tx_version=1,
     )
 
     deploy_account_tx = await account.sign_deploy_account_transaction(
@@ -23,7 +22,7 @@ async def test_general_flow(client, deploy_account_details_factory):
         constructor_calldata=[key_pair.public_key],
         max_fee=MAX_FEE,
     )
-    resp = await account.deploy_account(transaction=deploy_account_tx)
+    resp = await account.client.deploy_account(transaction=deploy_account_tx)
 
     assert resp.address == address
 
