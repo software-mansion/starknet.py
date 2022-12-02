393fc40e6df917131fb880aae87404614f05ab69
war-in <marcin.warchol20@gmail.com>
2022-12-02 14:17:06 +0100

    add int_from_bytes method

--- starknet_py/net/models/chains.py
+++ starknet_py/net/models/chains.py
@@ -1,20 +1,19 @@
 from enum import Enum
 from typing import Optional
 
+from starknet_py.common import int_from_bytes
 from starknet_py.net.networks import Network, MAINNET, TESTNET, TESTNET2
 
 
 class StarknetChainId(Enum):
-    MAINNET = int.from_bytes(b"SN_MAIN", byteorder="big", signed=False)
-    TESTNET = int.from_bytes(b"SN_GOERLI", byteorder="big", signed=False)
-    TESTNET2 = int.from_bytes(b"SN_GOERLI2", byteorder="big", signed=False)
+    MAINNET = int_from_bytes(b"SN_MAIN")
+    TESTNET = int_from_bytes(b"SN_GOERLI")
+    TESTNET2 = int_from_bytes(b"SN_GOERLI2")
 
 
-# Pyright ignores were added because it doesn't allow StarknetChainId as a type here.
-# It is ok when it is used as type in a different module.
 def chain_from_network(
-    net: Network, chain: Optional[StarknetChainId]  # pyright: ignore
-) -> StarknetChainId:  # pyright: ignore
+    net: Network, chain: Optional[StarknetChainId]
+) -> StarknetChainId:
     mapping = {
         MAINNET: StarknetChainId.MAINNET,
         TESTNET: StarknetChainId.TESTNET,
