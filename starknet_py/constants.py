1554d425889be794344c5edfd153c23ce9d93866
Kamil Jankowski <kamil.jankowski.x@gmail.com>
2022-12-13 14:46:03 +0100

    Replace `FIELD_PRIME` in felt.py (#588)
    * Add FIELD_PRIME constant
    
    * Change FIELD_PRIME to hex

--- starknet_py/constants.py
+++ starknet_py/constants.py
@@ -1,3 +1,6 @@
+# pylint: disable=line-too-long
+# fmt: off
+
 from starkware.starknet.services.api.feeder_gateway.response_objects import (
     TransactionStatus,
 )
@@ -7,13 +10,10 @@ TxStatus = TransactionStatus
 ACCEPTED_STATUSES = (TxStatus.ACCEPTED_ON_L1, TxStatus.ACCEPTED_ON_L2)
 
 # Address came from starkware-libs/starknet-addresses repository: https://github.com/starkware-libs/starknet-addresses
-FEE_CONTRACT_ADDRESS = (
-    "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
-)
-
-DEFAULT_DEPLOYER_ADDRESS = (
-    "0x041a78e741e5aF2fEc34B695679bC6891742439f7AFB8484Ecd7766661aD02BF"
-)
+FEE_CONTRACT_ADDRESS = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+DEFAULT_DEPLOYER_ADDRESS = "0x041a78e741e5aF2fEc34B695679bC6891742439f7AFB8484Ecd7766661aD02BF"
 
 RPC_INVALID_MESSAGE_SELECTOR_ERROR = 21
 RPC_CLASS_HASH_NOT_FOUND_ERROR = 28
+
+FIELD_PRIME = 0x800000000000011000000000000000000000000000000000000000000000001
