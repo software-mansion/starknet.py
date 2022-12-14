a0397956d09d7ce491ec2d1d39eb788e032e112f
war-in <marcin.warchol20@gmail.com>
2022-12-14 15:09:15 +0100

    replace compute_hash_on_elements

--- starknet_py/utils/crypto/facade.py
+++ starknet_py/utils/crypto/facade.py
@@ -1,3 +1,4 @@
+import functools
 import os
 from typing import Optional
 
@@ -6,7 +7,6 @@ from crypto_cpp_py.cpp_bindings import (
     get_cpp_lib_file,
     ECSignature,
 )
-from starkware.cairo.common.hash_state import compute_hash_on_elements
 from starkware.cairo.lang.vm.crypto import pedersen_hash as default_hash
 from starkware.crypto.signature.signature import sign
 
@@ -14,6 +14,33 @@ from starknet_py.net.client_models import Call
 
 # PREFIX_TRANSACTION = encoded 'StarkNet Transaction'
 PREFIX_TRANSACTION = 476441609247967894954472788179128007176248455022
+
+
+# Interface
+def use_cpp_variant() -> bool:
+    force_disable_ext = (
+        os.getenv("DISABLE_CRYPTO_C_EXTENSION", "false").lower() == "true"
+    )
+    cpp_lib_file = get_cpp_lib_file()
+    return not force_disable_ext and bool(cpp_lib_file)
+
+
+def pedersen_hash(left: int, right: int) -> int:
+    if use_cpp_variant():
+        return cpp_hash(left, right)
+    return default_hash(left, right)
+
+
+def compute_hash_on_elements(data, hash_func=pedersen_hash):
+    """
+    Computes a hash chain over the data, in the following order:
+        h(h(h(h(0, data[0]), data[1]), ...), data[n-1]), n).
+
+    The hash is initialized with 0 and ends with the data length appended.
+    The length is appended in order to avoid collisions of the following kind:
+    H([x,y,z]) = h(h(x,y),z) = H([w, z]) where w = h(x,y).
+    """
+    return functools.reduce(hash_func, [*data, len(data)], 0)
 
 
 def hash_call_with(call: Call, hash_fun):
@@ -29,20 +56,5 @@ def hash_call_with(call: Call, hash_fun):
     )
 
 
-# Interface
-def use_cpp_variant() -> bool:
-    force_disable_ext = (
-        os.getenv("DISABLE_CRYPTO_C_EXTENSION", "false").lower() == "true"
-    )
-    cpp_lib_file = get_cpp_lib_file()
-    return not force_disable_ext and bool(cpp_lib_file)
-
-
 def message_signature(msg_hash, priv_key, seed: Optional[int] = 32) -> ECSignature:
     return sign(msg_hash, priv_key, seed)
-
-
-def pedersen_hash(left: int, right: int) -> int:
-    if use_cpp_variant():
-        return cpp_hash(left, right)
-    return default_hash(left, right)
