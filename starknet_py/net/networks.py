3ed09ebc39126d7ded98938f776c60aa131f7a9d
Artur Michalek <artur.michalek@swmansion.com>
2022-12-14 11:54:50 +0100

    Fix default_token_address_for_network typing

--- starknet_py/net/networks.py
+++ starknet_py/net/networks.py
@@ -25,7 +25,7 @@ def net_address_from_net(net: str) -> str:
     }.get(net, net)
 
 
-def default_token_address_for_network(net: str) -> str:
+def default_token_address_for_network(net: Network) -> str:
     if net not in [TESTNET, TESTNET2, MAINNET]:
         raise ValueError(
             "Token_address must be specified when using a custom net address"
