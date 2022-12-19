8bba8b45b1c8c409675578a4603835bcd4e4db14
Jakub Ptak <jakub.ptak@swmansion.com>
2022-12-19 17:01:52 +0100

    Add tuple serializers

---
+++ starknet_py/cairo/serialization/data_serializers/named_tuple_serializer.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+from typing import Union, Dict, NamedTuple, OrderedDict, Generator
+
+from starknet_py.cairo.serialization._context import (
+    DeserializationContext,
+    SerializationContext,
+)
+from starknet_py.cairo.serialization.data_serializers._common import (
+    deserialize_to_dict,
+    serialize_from_dict,
+)
+from starknet_py.cairo.serialization.data_serializers.cairo_data_serializer import (
+    CairoDataSerializer,
+)
+from starknet_py.utils.tuple_dataclass import TupleDataclass
+
+
+@dataclass
+class NamedTupleSerializer(
+    CairoDataSerializer[Union[Dict, NamedTuple, TupleDataclass], TupleDataclass]
+):
+    """
+    Serializer for tuples with named fields.
+    Can serialize a dictionary, a named tuple and TupleDataclass.
+    Deserializes data to a TupleDataclass.
+    """
+
+    types: OrderedDict[str, CairoDataSerializer]
+
+    def deserialize_with_context(
+        self, context: DeserializationContext
+    ) -> TupleDataclass:
+        as_dictionary = deserialize_to_dict(self.types, context)
+        return TupleDataclass.from_dict(as_dictionary)
+
+    def serialize_with_context(
+        self,
+        context: SerializationContext,
+        value: Union[Dict, NamedTuple, TupleDataclass],
+    ) -> Generator[int, None, None]:
+        # We can't use isinstance(value, NamedTuple), because there is no NamedTuple type.
+        context.ensure_valid_type(
+            value,
+            isinstance(value, (dict, TupleDataclass)) or self._is_namedtuple(value),
+            "dict, NamedTuple or TupleDataclass",
+        )
+
+        # noinspection PyUnresolvedReferences, PyProtectedMember
+        values: Dict = value if isinstance(value, dict) else value._asdict()
+
+        yield from serialize_from_dict(self.types, context, values)
+
+    @staticmethod
+    def _is_namedtuple(value) -> bool:
+        return isinstance(value, tuple) and hasattr(value, "_fields")
