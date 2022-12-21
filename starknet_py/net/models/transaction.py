a4b8c41a5a5451f32ed5645839ff5bfa6c986da7
war-in <marcin.warchol20@gmail.com>
2022-12-21 13:35:47 +0100

    replace usages from starkware

--- starknet_py/net/models/transaction.py
+++ starknet_py/net/models/transaction.py
@@ -7,17 +7,16 @@ from starkware.starknet.services.api.gateway.transaction import (
     Declare as DCL,
     DeployAccount as DAC,
 )
-from starkware.starknet.core.os.transaction_hash.transaction_hash import (
-    calculate_transaction_hash_common,
-    TransactionHashPrefix,
-)
 
 # noinspection PyPep8Naming
 from starkware.starknet.definitions.transaction_type import TransactionType as TT
 
 from starknet_py.cairo.selector import get_selector_from_name
 from starknet_py.net.models.chains import StarknetChainId
-from starknet_py.utils.crypto.facade import pedersen_hash
+from starknet_py.utils.crypto.transaction_hash import (
+    compute_transaction_hash_common,
+    TransactionHashPrefix,
+)
 from starknet_py.utils.docs import as_our_module
 
 Invoke = InvokeFunction = as_our_module(IF)
@@ -50,13 +49,12 @@ def compute_invoke_hash(
     if isinstance(entry_point_selector, str):
         entry_point_selector = get_selector_from_name(entry_point_selector)
 
-    return calculate_transaction_hash_common(
+    return compute_transaction_hash_common(
         tx_hash_prefix=TransactionHashPrefix.INVOKE,
         contract_address=contract_address,
         entry_point_selector=entry_point_selector,
         calldata=calldata,
         chain_id=chain_id.value,
-        hash_function=pedersen_hash,
         additional_data=[],
         max_fee=max_fee,
         version=version,
