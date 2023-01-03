3a5194eaecc81575b2ce2087770a3b017f15a357
war-in <marcin.warchol20@gmail.com>
2023-01-03 09:19:32 +0100

    use manni for pygments_style

--- docs/conf.py
+++ docs/conf.py
@@ -67,5 +67,5 @@ html_css_files = ["custom.css"]
 autodoc_class_signature = "separated"
 autodoc_default_options = {"exclude-members": "__new__"}
 
-pygments_style = "emacs"
+pygments_style = "manni"
 pygments_dark_style = "fruity"
