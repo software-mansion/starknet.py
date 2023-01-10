327e381a999cb5f79a75fc9db0cd2059c511c7c0
Jakub Ptak <jakub.ptak@swmansion.com>
2023-01-10 09:49:41 +0100

    Add missing dots

--- starknet_py/utils/data_transformer/errors.py
+++ starknet_py/utils/data_transformer/errors.py
@@ -1,4 +1,4 @@
-# This has to be there for forward compatibility.
+# This has to be there for backward compatibility.
 # This file will be removed with the whole data_transformer module.
 # pylint: disable=unused-import
 from starknet_py.cairo.serialization.errors import (
