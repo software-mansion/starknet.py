b2e1991b1d3e3dd110a2d588e7cfe2d48976b5d5
drknzz <kamil.jankowski.x@gmail.com>
2023-01-13 17:01:47 +0100

    Add comment about None

--- starknet_py/tests/e2e/docs/guide/test_resolving_proxies.py
+++ starknet_py/tests/e2e/docs/guide/test_resolving_proxies.py
@@ -49,6 +49,8 @@ async def test_resolving_proxies(
         async def implementation_address(
             self, address: Address, client: Client
         ) -> Optional[int]:
+            # Note that None is returned, since the Proxy uses
+            # the address of another contract as implementation and not the class hash
             return None
 
         async def implementation_hash(
