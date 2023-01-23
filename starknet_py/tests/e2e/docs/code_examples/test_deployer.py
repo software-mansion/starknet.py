3810a4f9886d30af36918799af26a72dbbc56a48
war-in <marcin.warchol20@gmail.com>
2023-01-23 14:25:41 +0100

    do not include unnecessary hints

--- starknet_py/tests/e2e/docs/code_examples/test_deployer.py
+++ starknet_py/tests/e2e/docs/code_examples/test_deployer.py
@@ -12,18 +12,6 @@ def test_init():
     # docs: init_end
 
 
-def test_create_deployment_call():
-    abi = []
-    calldata = []
-    deployer = Deployer()
-
-    # docs: create_deployment_call_start
-    contract_deployment = deployer.create_deployment_call(
-        class_hash=0x123, salt=1, abi=abi, calldata=calldata
-    )
-    # docs: create_deployment_call_end
-
-
 def test_create_deployment_call_raw():
     deployer = Deployer()
 
--- starknet_py/utils/decorators/code_examples/code_examples.py
+++ starknet_py/utils/decorators/code_examples/code_examples.py
@@ -1,30 +1,40 @@
 import re
+from pathlib import Path
 from typing import Any
+
+CODE_EXAMPLES_DIR = Path("../starknet_py/tests/e2e/docs/code_examples")
 
 
 def add_code_examples(original_class: Any) -> Any:
     base_class = original_class.__base__
-    for method_name, method in original_class.__dict__.items():
-        file_name = "test_" + _camel_to_snake(original_class.__name__)
-        docstring = f"""
-        .. hint::
-        
-            .. codesnippet:: ../../starknet_py/tests/e2e/docs/code_examples/{file_name}.py
-                :language: python
-                :start-after: docs: {method_name.strip("_")}_start
-                :end-before: docs: {method_name.strip("_")}_end
-                :dedent: 4
-        """
+    file_name = "test_" + _camel_to_snake(original_class.__name__) + ".py"
+    file_path = CODE_EXAMPLES_DIR / file_name
 
-        # if method does not have __doc__ take it from the base method
-        if (
-            callable(method)
-            and method.__doc__ is None
-            and method_name in base_class.__dict__
-        ):
-            method.__doc__ = getattr(base_class, method_name).__doc__ + docstring
-        elif callable(method):
-            method.__doc__ = (method.__doc__ or "") + docstring
+    with open(file_path, "r", encoding="utf-8") as file:
+        file_content = file.read()
+
+    for method_name, method in original_class.__dict__.items():
+        stripped_method_name = method_name.strip("_")
+        if f"""docs: {stripped_method_name}_start""" in file_content:
+            docstring = f"""
+            .. hint::
+            
+                .. codesnippet:: {".." / file_path}
+                    :language: python
+                    :start-after: docs: {stripped_method_name}_start
+                    :end-before: docs: {stripped_method_name}_end
+                    :dedent: 4
+            """
+
+            # if method does not have __doc__ take it from the base method
+            if (
+                callable(method)
+                and method.__doc__ is None
+                and method_name in base_class.__dict__
+            ):
+                method.__doc__ = getattr(base_class, method_name).__doc__ + docstring
+            elif callable(method):
+                method.__doc__ = (method.__doc__ or "") + docstring
 
     return original_class
 
