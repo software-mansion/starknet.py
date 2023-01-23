7829be7a34470d5cf7f458a30a9aebccdf1c216b
war-in <marcin.warchol20@gmail.com>
2023-01-23 16:00:36 +0100

    fix tests

--- starknet_py/utils/decorators/code_examples/code_examples.py
+++ starknet_py/utils/decorators/code_examples/code_examples.py
@@ -1,19 +1,18 @@
 import re
-from pathlib import Path
 from typing import Any, Tuple
 
-CODE_EXAMPLES_DIR = Path("../starknet_py/tests/e2e/docs/code_examples")
+from starknet_py.constants import ROOT_PATH
 
 
 def add_code_examples(original_class: Any) -> Any:
     base_class = original_class.__base__
 
-    file_path, file_content = _extract_file_properties(original_class.__name__)
+    file_name, file_content = _extract_file_properties(original_class.__name__)
 
     for method_name, method in original_class.__dict__.items():
         stripped_method_name = method_name.strip("_")
         if f"""docs: {stripped_method_name}_start""" in file_content:
-            hint = create_hint(file_path, stripped_method_name)
+            hint = create_hint(file_name, stripped_method_name)
 
             # if method does not have __doc__ take it from the base method
             if (
@@ -28,11 +27,11 @@ def add_code_examples(original_class: Any) -> Any:
     return original_class
 
 
-def create_hint(path: Path, method_name: str) -> str:
+def create_hint(file_name: str, method_name: str) -> str:
     return f"""
             .. hint::
             
-                .. codesnippet:: {".." / path}
+                .. codesnippet:: ../../starknet_py/tests/e2e/docs/code_examples/{file_name}
                     :language: python
                     :start-after: docs: {method_name}_start
                     :end-before: docs: {method_name}_end
@@ -40,12 +39,12 @@ def create_hint(path: Path, method_name: str) -> str:
             """
 
 
-def _extract_file_properties(class_name: str) -> Tuple[Path, str]:
+def _extract_file_properties(class_name: str) -> Tuple[str, str]:
     file_name = "test_" + _camel_to_snake(class_name) + ".py"
-    file_path = CODE_EXAMPLES_DIR / file_name
+    file_path = ROOT_PATH / "tests/e2e/docs/code_examples" / file_name
 
     with open(file_path, "r", encoding="utf-8") as file:
-        return file_path, file.read()
+        return file_name, file.read()
 
 
 def _camel_to_snake(text: str) -> str:
