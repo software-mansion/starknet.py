70ffc91f6f171f856fb0fed82436242fd5df4387
Artur Michalek <artur.michalek@swmansion.com>
2023-01-27 12:41:33 +0100

    Rename variable in tests

--- starknet_py/tests/e2e/account/account_test.py
+++ starknet_py/tests/e2e/account/account_test.py
@@ -338,14 +338,14 @@ async def test_sign_invoke_tx_for_fee_estimation(account, map_contract):
     call = map_contract.functions["put"].prepare(key=40, value=50)
     transaction = await account.sign_invoke_transaction(calls=call, max_fee=MAX_FEE)
 
-    signed_transaction = await account.sign_for_fee_estimate(transaction)
+    estimate_fee_transaction = await account.sign_for_fee_estimate(transaction)
 
-    estimation = await account.client.estimate_fee(signed_transaction)
+    estimation = await account.client.estimate_fee(estimate_fee_transaction)
     assert estimation.overall_fee > 0
 
     # Verify that the transaction signed for fee estimation cannot be sent
     with pytest.raises(ClientError):
-        await account.client.send_transaction(signed_transaction)
+        await account.client.send_transaction(estimate_fee_transaction)
 
     # Verify that original transaction can be sent
     result = await account.client.send_transaction(transaction)
@@ -358,14 +358,14 @@ async def test_sign_declare_tx_for_fee_estimation(account, map_compiled_contract
         compiled_contract=map_compiled_contract, max_fee=MAX_FEE
     )
 
-    signed_transaction = await account.sign_for_fee_estimate(transaction)
+    estimate_fee_transaction = await account.sign_for_fee_estimate(transaction)
 
-    estimation = await account.client.estimate_fee(signed_transaction)
+    estimation = await account.client.estimate_fee(estimate_fee_transaction)
     assert estimation.overall_fee > 0
 
     # Verify that the transaction signed for fee estimation cannot be sent
     with pytest.raises(ClientError):
-        await account.client.declare(signed_transaction)
+        await account.client.declare(estimate_fee_transaction)
 
     # Verify that original transaction can be sent
     result = await account.client.declare(transaction)
@@ -404,14 +404,14 @@ async def test_sign_deploy_account_tx_for_fee_estimation(
         max_fee=MAX_FEE,
     )
 
-    signed_transaction = await account.sign_for_fee_estimate(transaction)
+    estimate_fee_transaction = await account.sign_for_fee_estimate(transaction)
 
     estimation = await account.client.estimate_fee(transaction)
     assert estimation.overall_fee > 0
 
     # Verify that the transaction signed for fee estimation cannot be sent
     with pytest.raises(ClientError):
-        await account.client.deploy_account(signed_transaction)
+        await account.client.deploy_account(estimate_fee_transaction)
 
     # Verify that original transaction can be sent
     result = await account.client.deploy_account(transaction)
