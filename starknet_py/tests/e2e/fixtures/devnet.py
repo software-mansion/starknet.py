0d432e29ab1f993e4ef42f2aa12328eef26c4d54
war-in <61014013+war-in@users.noreply.github.com>
2023-02-01 13:42:16 +0100

    Speed up tests (#732)
    * change module scope to package
    
    * uncomment the line

--- starknet_py/tests/e2e/fixtures/devnet.py
+++ starknet_py/tests/e2e/fixtures/devnet.py
@@ -36,7 +36,7 @@ def start_devnet():
     return devnet_port, proc
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="package")
 def run_devnet() -> Generator[str, None, None]:
     """
     Runs devnet instance once per module and returns it's address.
