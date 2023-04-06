f024a3413ee0d37287e60e730a5a9c503e6b30f2
war-in <marcin.warchol20@gmail.com>
2023-04-06 14:49:02 +0200

    add docstrings to the ParserTransformer

--- starknet_py/abi/v1/parser_transformer.py
+++ starknet_py/abi/v1/parser_transformer.py
@@ -1,7 +1,8 @@
 import os
+from typing import Any, List, Optional
 
 import lark
-from lark import Transformer, v_args
+from lark import Token, Transformer, v_args
 
 from starknet_py.cairo.data_types import (
     ArrayType,
@@ -25,35 +26,74 @@ class ParserTransformer(Transformer):
         raise TypeError(f"Unable to parse tree node of type {data}")
 
     @v_args(inline=True)
-    def start(self, value):
+    def start(self, value) -> Optional[CairoType]:
+        """
+        Method all the entries starts from.
+        """
         return value
 
-    def type(self, value):
+    def type(self, value: List[Optional[CairoType]]) -> Optional[CairoType]:
+        """
+        Types starting with "core" or unit type "()".
+
+        Tokens are read bottom-up, so here all of them are parsed and should be just returned.
+        `Optional` is added in case of the unit type.
+        """
+        assert len(value) == 1
         return value[0]
 
-    def type_felt(self, _value):
+    def type_felt(self, _value: List[Any]) -> FeltType:
+        """
+        Felt does not contain any additional arguments, so `_value` is just an empty list.
+        """
         return FeltType()
 
-    def type_uint(self, value):
+    def type_uint(self, value: List[Token]) -> UintType:
+        """
+        Uint type contains information about it's size. It is present in the value[0].
+        """
         return UintType(int(value[0]))
 
-    def type_unit(self, _value):
+    def type_unit(self, _value: List[Any]) -> None:
+        """
+        () type.
+        """
         return None
 
-    def type_option(self, _value):
-        return Option(_value[0])
+    def type_option(self, value: List[Optional[CairoType]]) -> Option:
+        """
+        Option includes an information about which type it eventually represents.
+        `Optional` is added in case of the unit type.
+        """
+        return Option(value[0])
 
-    def type_array(self, value):
+    def type_array(self, value: List[CairoType]) -> ArrayType:
+        """
+        Array contains values of type under `value[0]`.
+        """
         return ArrayType(value[0])
 
-    def struct(self, tokens):
+    def struct(self, tokens: List[Token]) -> TypeIdentifier:
+        """
+        Structs are defined as follows: (IDENTIFIER | "::")+ ("<" type ">")*
+        where IDENTIFIER is some string.
+
+        Tokens would contain strings and type (if it is present).
+        We are interested only in the strings because a structure name can be built from them.
+        """
         name = "::".join(token for token in tokens if isinstance(token, str))
         return TypeIdentifier(name)
 
-    def type_address(self, _value):
+    def type_address(self, _value: List[Any]) -> FeltType:
+        """
+        ContractAddress is represented by the felt252.
+        """
         return FeltType()
 
-    def tuple(self, types):
+    def tuple(self, types: List[CairoType]) -> TupleType:
+        """
+        Tuple contains values defined in the `types` argument.
+        """
         return TupleType(types)
 
 
