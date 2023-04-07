1948df02437e166308c64fd229c967a03f7a2c8d
war-in <marcin.warchol20@gmail.com>
2023-04-07 14:17:24 +0200

    remove more unused code

--- starknet_py/cairo/deprecated_parse/cairo_types.py
+++ starknet_py/cairo/deprecated_parse/cairo_types.py
@@ -1,51 +1,26 @@
 import dataclasses
-from abc import ABC, abstractmethod
-from typing import Iterator, List, Optional, Sequence
+from typing import List, Optional
 
 
-class AstNode(ABC):
-    @abstractmethod
-    def get_children(self) -> Sequence[Optional["AstNode"]]:
-        """
-        Returns a list of the node's children (notes are not included).
-        """
-
-    def get_subtree(self) -> Iterator["AstNode"]:
-        """
-        Returns an iterator of all non-None nodes in the subtree rooted at this node, preorder
-        (visit each node before its children).
-        """
-        yield self
-        for child in filter(None, self.get_children()):
-            yield from child.get_subtree()
-
-
-class CairoType(AstNode):
-    def get_pointer_type(self) -> "CairoType":
-        """
-        Returns a type of a pointer to the current type.
-        """
-        return TypePointer(pointee=self)
+class CairoType:
+    """
+    Base class for cairo types.
+    """
 
 
 @dataclasses.dataclass
 class TypeFelt(CairoType):
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return []
+    pass
 
 
 @dataclasses.dataclass
 class TypeCodeoffset(CairoType):
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return []
+    pass
 
 
 @dataclasses.dataclass
 class TypePointer(CairoType):
     pointee: CairoType
-
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return [self.pointee]
 
 
 @dataclasses.dataclass
@@ -57,16 +32,10 @@ class TypeIdentifier(CairoType):
 
     name: str
 
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return []
-
 
 @dataclasses.dataclass
 class TypeStruct(CairoType):
     scope: str
-
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return []
 
 
 @dataclasses.dataclass
@@ -77,9 +46,6 @@ class TypeFunction(CairoType):
 
     scope: str
 
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return []
-
 
 @dataclasses.dataclass
 class TypeTuple(CairoType):
@@ -89,7 +55,7 @@ class TypeTuple(CairoType):
     """
 
     @dataclasses.dataclass
-    class Item(AstNode):
+    class Item(CairoType):
         """
         Represents a possibly named type item of a TypeTuple.
         For example: "felt" or "a: felt".
@@ -98,40 +64,8 @@ class TypeTuple(CairoType):
         name: Optional[str]
         typ: CairoType
 
-        def get_children(self) -> Sequence[Optional[AstNode]]:
-            return [self.typ]
-
     members: List["TypeTuple.Item"]
     has_trailing_comma: bool = dataclasses.field(hash=False, compare=False)
-
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return self.members
-
-    @property
-    def types(self) -> List[CairoType]:
-        """
-        Returns the unnamed types of the tuple.
-        """
-        return [x.typ for x in self.members]
-
-    @classmethod
-    def unnamed(cls, types: List[CairoType]):
-        """
-        Creates an unnamed tuple type from the given types.
-        """
-        return cls.from_members(
-            members=[TypeTuple.Item(name=None, typ=typ) for typ in types],
-        )
-
-    @classmethod
-    def from_members(cls, members: List["TypeTuple.Item"]):
-        """
-        Creates a tuple (with no notes) from the given members.
-        """
-        return cls(
-            members=members,
-            has_trailing_comma=False,
-        )
 
     @property
     def is_named(self) -> bool:
@@ -139,8 +73,5 @@ class TypeTuple(CairoType):
 
 
 @dataclasses.dataclass
-class ExprIdentifier(AstNode):
+class ExprIdentifier(CairoType):
     name: str
-
-    def get_children(self) -> Sequence[Optional[AstNode]]:
-        return []
