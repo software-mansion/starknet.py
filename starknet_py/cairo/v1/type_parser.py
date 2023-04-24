fcc4f676f46599eba9057face66bbff139396c04
war-in <marcin.warchol20@gmail.com>
2023-04-24 12:07:45 +0200

    strip trailing `::` in struct name

--- starknet_py/cairo/v1/type_parser.py
+++ starknet_py/cairo/v1/type_parser.py
@@ -54,6 +54,6 @@ class TypeParser:
 
     def _get_struct(self, identifier: TypeIdentifier):
         for struct_name in self.defined_types.keys():
-            if identifier.name == struct_name.split("<")[0]:
+            if identifier.name == struct_name.split("<")[0].strip(":"):
                 return self.defined_types[struct_name]
         raise UnknownCairoTypeError(identifier.name)
