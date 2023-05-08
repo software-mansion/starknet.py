5ff06446839ca1627b1ffd2b244bcca260f14e50
war-in <marcin.warchol20@gmail.com>
2023-05-08 10:04:25 +0200

    move some code to helper functions

--- starknet_py/utils/contructor_args_translator.py
+++ starknet_py/utils/contructor_args_translator.py
@@ -2,30 +2,24 @@ from typing import List, Optional, Union
 
 from starknet_py.abi.parser import AbiParser
 from starknet_py.abi.v1.parser import AbiParser as AbiV1Parser
-from starknet_py.serialization import serializer_for_function
+from starknet_py.serialization import (
+    FunctionSerializationAdapter,
+    serializer_for_function,
+)
 from starknet_py.serialization.factory import serializer_for_function_v1
 
 
 def translate_constructor_args(
     abi: List, constructor_args: Optional[Union[List, dict]], cairo_version: int = 0
 ) -> List[int]:
-    if cairo_version == 1:
-        parsed = AbiV1Parser(abi).parse()
-        constructor = parsed.functions.get("constructor", None)
+    serializer = (
+        _get_constructor_serializer_v1(abi)
+        if cairo_version == 1
+        else _get_constructor_serializer_v0(abi)
+    )
 
-        # Constructor might not accept any arguments
-        if constructor is None or not constructor.inputs:
-            return []
-
-        serializer = serializer_for_function_v1(constructor)
-    else:
-        parsed = AbiParser(abi).parse()
-
-        # Constructor might not accept any arguments
-        if not parsed.constructor or not parsed.constructor.inputs:
-            return []
-
-        serializer = serializer_for_function(parsed.constructor)
+    if serializer is None:
+        return []
 
     if not constructor_args:
         raise ValueError(
@@ -38,3 +32,24 @@ def translate_constructor_args(
         else (constructor_args, {})
     )
     return serializer.serialize(*args, **kwargs)
+
+
+def _get_constructor_serializer_v1(abi: List) -> Optional[FunctionSerializationAdapter]:
+    parsed = AbiV1Parser(abi).parse()
+    constructor = parsed.functions.get("constructor", None)
+
+    # Constructor might not accept any arguments
+    if constructor is None or not constructor.inputs:
+        return None
+
+    return serializer_for_function_v1(constructor)
+
+
+def _get_constructor_serializer_v0(abi: List) -> Optional[FunctionSerializationAdapter]:
+    parsed = AbiParser(abi).parse()
+
+    # Constructor might not accept any arguments
+    if not parsed.constructor or not parsed.constructor.inputs:
+        return None
+
+    return serializer_for_function(parsed.constructor)
