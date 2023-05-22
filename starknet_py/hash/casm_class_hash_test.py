67d640213803485936a66b2dc1ea26ca5cfd87a7
drknzz <kamil.jankowski.x@gmail.com>
2023-05-22 07:46:34 +0200

    Update hashes

--- starknet_py/hash/casm_class_hash_test.py
+++ starknet_py/hash/casm_class_hash_test.py
@@ -10,12 +10,12 @@ from starknet_py.tests.e2e.fixtures.misc import read_contract
 @pytest.mark.parametrize(
     "casm_contract_class_source, expected_casm_class_hash",
     [
-        ("account_compiled.casm", 0x72B483338596AE5613652402A3B86B15B967C5F8214BECC1EB5C56BCA17CFCC),
-        ("erc20_compiled.casm", 0x1C2FA751AE6D1C3155B8BBE28704D3DF5DFE3BB66244E80A28C292271A7410E),
-        ("hello_starknet_compiled.casm", 0x395097ed3bda54f78824d00e6e5898d5e16a0e57fa021e54060683cb5706ce7),
-        ("minimal_contract_compiled.casm", 0x73F17E5E8C771A97CB07BF6024753D514ED9A1B5DE4EC151E06D0926B015694),
-        ("test_contract_compiled.casm", 0x58200D59D5C4B43145D9CCF9232D6BE12483923ED788DB7C3C06BDE5CDAE6EF),
-        ("token_bridge_compiled.casm", 0x71AD7A62064E7BA2681CAC07DF817701B24A92C3E752739769EAC1812F105),
+        ("account_compiled.casm", 0x3a5f170fb6c78cf35d952af21576bc9afa56f430c653bab2fe577dd807dd9c7),
+        ("erc20_compiled.casm", 0x55ff1834cd7c0a3743032864c99a43b5a924899983b49612b0446931f9368c6),
+        ("hello_starknet_compiled.casm", 0x2c895e2f0aed646e6dec493287b9eaf4cadce8983ad3d60164e15a7b1c35f54),
+        ("minimal_contract_compiled.casm", 0x46f2882281342dea7694207216f95d925ba08ef4be0cff5e81e9057f49ef3c2),
+        ("test_contract_compiled.casm", 0x636e18eaa5730715fdf7a618c00ca03f6f18f324e22482fa97406f8a2336e0f),
+        ("token_bridge_compiled.casm", 0x762391ddfdc7c30d48ace43aa714b14e0f876489bf6b0dc5df6f1f018963754),
     ],
 )
 def test_compute_casm_class_hash(casm_contract_class_source, expected_casm_class_hash):
