0eec09ec8f509e8cb94786924c47daa938d62245
drknzz <kamil.jankowski.x@gmail.com>
2023-05-28 04:19:43 +0200

    Use cairo-lang to compute hash values

--- starknet_py/hash/casm_class_hash_test.py
+++ starknet_py/hash/casm_class_hash_test.py
@@ -1,5 +1,11 @@
 # fmt: off
 import pytest
+from starkware.starknet.core.os.contract_class.compiled_class_hash import (
+    compute_compiled_class_hash as sw_compute_compiled_class_hash,
+)
+from starkware.starknet.services.api.contract_class.contract_class import (
+    CompiledClass as SwCompiledClass,
+)
 
 from starknet_py.common import create_casm_class
 from starknet_py.hash.casm_class_hash import compute_casm_class_hash
@@ -8,17 +14,17 @@ from starknet_py.tests.e2e.fixtures.misc import read_contract
 
 
 @pytest.mark.parametrize(
-    "casm_contract_class_source, expected_casm_class_hash",
+    "casm_contract_class_source",
     [
-        ("account_compiled.casm", 0x3a5f170fb6c78cf35d952af21576bc9afa56f430c653bab2fe577dd807dd9c7),
-        ("erc20_compiled.casm", 0x55ff1834cd7c0a3743032864c99a43b5a924899983b49612b0446931f9368c6),
-        ("hello_starknet_compiled.casm", 0x2c895e2f0aed646e6dec493287b9eaf4cadce8983ad3d60164e15a7b1c35f54),
-        ("minimal_contract_compiled.casm", 0x46f2882281342dea7694207216f95d925ba08ef4be0cff5e81e9057f49ef3c2),
-        ("test_contract_compiled.casm", 0x636e18eaa5730715fdf7a618c00ca03f6f18f324e22482fa97406f8a2336e0f),
-        ("token_bridge_compiled.casm", 0x762391ddfdc7c30d48ace43aa714b14e0f876489bf6b0dc5df6f1f018963754),
+        "account_compiled.casm",
+        "erc20_compiled.casm",
+        "hello_starknet_compiled.casm",
+        "minimal_contract_compiled.casm",
+        "test_contract_compiled.casm",
+        "token_bridge_compiled.casm",
     ],
 )
-def test_compute_casm_class_hash(casm_contract_class_source, expected_casm_class_hash):
+def test_compute_casm_class_hash(casm_contract_class_source):
     casm_contract_class_str = read_contract(
         casm_contract_class_source, directory=CONTRACTS_COMPILED_V1_DIR
     )
@@ -26,4 +32,7 @@ def test_compute_casm_class_hash(casm_contract_class_source, expected_casm_class
     casm_class = create_casm_class(casm_contract_class_str)
     casm_class_hash = compute_casm_class_hash(casm_class)
 
-    assert casm_class_hash == expected_casm_class_hash
+    sw_compiled_class = SwCompiledClass.loads(casm_contract_class_str)
+    sw_compiled_class_hash = sw_compute_compiled_class_hash(sw_compiled_class)
+
+    assert casm_class_hash == sw_compiled_class_hash
--- starknet_py/hash/class_hash_test.py
+++ starknet_py/hash/class_hash_test.py
@@ -1,6 +1,12 @@
 # pylint: disable=line-too-long
 # fmt: off
 import pytest
+from starkware.starknet.core.os.contract_class.deprecated_class_hash import (
+    compute_deprecated_class_hash as sw_compute_deprecated_class_hash,
+)
+from starkware.starknet.services.api.contract_class.contract_class import (
+    DeprecatedCompiledClass as SwDeprecatedCompiledClass,
+)
 
 from starknet_py.common import create_contract_class
 from starknet_py.hash.class_hash import compute_class_hash
@@ -8,17 +14,22 @@ from starknet_py.tests.e2e.fixtures.misc import read_contract
 
 
 @pytest.mark.parametrize(
-    "contract_source, expected_class_hash", [
-        ("map_compiled.json", 0x2c73e5cf1538ca1fd44018fab311c8be3b96bd9136912512fa4789fe1733ff9),
-        ("erc20_compiled.json", 0x24cfb287ef1f88912fbd71bd1d763c53d863b473be5d54086b46a4ded8bdfb0),
-        ("oz_proxy_compiled.json", 0x5a157230e2584204fc5c0ca5f766457cbb2b5b140ee3bfbfa5e4f6178420611),
-        ("argent_proxy_compiled.json", 0x5e8378ef0c0d90472094025f6b0541e31d6bc2fcf2e268c33bcea201f2921ad),
-        ("universal_deployer_compiled.json", 0x37a1cd95937b83c4fe77b30f727cd7e3be9208cbb00bd8607122cb981032448),
-        ("precompiled/oz_proxy_address_0.8.1_compiled.json", 0x413c36c287cb410d42f9e531563f68ac60a2913b5053608d640fb9b643acfe6),
+    "contract_source",
+    [
+        "map_compiled.json",
+        "erc20_compiled.json",
+        "oz_proxy_compiled.json",
+        "argent_proxy_compiled.json",
+        "universal_deployer_compiled.json",
+        "precompiled/oz_proxy_address_0.8.1_compiled.json",
     ]
 )
-def test_compute_class_hash(contract_source, expected_class_hash):
+def test_compute_class_hash(contract_source):
     compiled_contract = read_contract(contract_source)
     contract_class = create_contract_class(compiled_contract)
     class_hash = compute_class_hash(contract_class)
-    assert class_hash == expected_class_hash
+
+    sw_deprecated_class = SwDeprecatedCompiledClass.loads(compiled_contract)
+    sw_deprecated_class_hash = sw_compute_deprecated_class_hash(sw_deprecated_class)
+
+    assert class_hash == sw_deprecated_class_hash
