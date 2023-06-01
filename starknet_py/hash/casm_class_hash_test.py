cc89a925d93594abf2f04cc0b3698b8de815c3e1
drknzz <kamil.jankowski.x@gmail.com>
2023-06-01 15:23:36 +0200

    Fix class hashes

--- starknet_py/hash/casm_class_hash_test.py
+++ starknet_py/hash/casm_class_hash_test.py
@@ -14,15 +14,16 @@ if get_python_version()[:2] == (3, 9):
         CompiledClass as SwCompiledClass,
     )
 
+
 @pytest.mark.parametrize(
     "casm_contract_class_source, expected_casm_class_hash",
     [
-        ("account_compiled.casm", 0x3A5F170FB6C78CF35D952AF21576BC9AFA56F430C653BAB2FE577DD807DD9C7),
-        ("erc20_compiled.casm", 0x55FF1834CD7C0A3743032864C99A43B5A924899983B49612B0446931F9368C6),
+        ("account_compiled.casm", 0x2645C161601C2D406225B32954497ADA29BF14A19363BCD6071DF9F25EA0620),
+        ("erc20_compiled.casm", 0x18F95714044FD5408D3BF812BCD249DDEC098AB3CD201B7916170CFBFA59E05),
         ("hello_starknet_compiled.casm", 0x2C895E2F0AED646E6DEC493287B9EAF4CADCE8983AD3D60164E15A7B1C35F54),
         ("minimal_contract_compiled.casm", 0x46F2882281342DEA7694207216F95D925BA08EF4BE0CFF5E81E9057F49EF3C2),
         ("test_contract_compiled.casm", 0x636E18EAA5730715FDF7A618C00CA03F6F18F324E22482FA97406F8A2336E0F),
-        ("token_bridge_compiled.casm", 0x762391DDFDC7C30D48ACE43AA714B14E0F876489BF6B0DC5DF6F1F018963754),
+        ("token_bridge_compiled.casm", 0x4AFFA0C3F2CD95C4DE6B94EE483912562C1F7C253D719BF1A6925C5F737BE5A),
     ],
 )
 def test_compute_casm_class_hash(casm_contract_class_source, expected_casm_class_hash):
