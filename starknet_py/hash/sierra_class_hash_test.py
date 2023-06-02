2bf2b6aa8c1efc179b92a3a12fc1737ff156d250
drknzz <kamil.jankowski.x@gmail.com>
2023-06-02 09:03:44 +0200

    Comment out cairo-lang usage

--- starknet_py/hash/sierra_class_hash_test.py
+++ starknet_py/hash/sierra_class_hash_test.py
@@ -1,19 +1,17 @@
-import json
-
 import pytest
 
 from starknet_py.common import create_sierra_compiled_contract
 from starknet_py.hash.sierra_class_hash import compute_sierra_class_hash
 from starknet_py.tests.e2e.fixtures.constants import CONTRACTS_COMPILED_V1_DIR
-from starknet_py.tests.e2e.fixtures.misc import get_python_version, read_contract
+from starknet_py.tests.e2e.fixtures.misc import read_contract
 
-if get_python_version()[:2] == (3, 9):
-    from starkware.starknet.core.os.contract_class.class_hash import (
-        compute_class_hash as sw_compute_class_hash,
-    )
-    from starkware.starknet.services.api.contract_class.contract_class import (
-        ContractClass as SwContractClass,
-    )
+# if get_python_version()[:2] == (3, 9):
+#     from starkware.starknet.core.os.contract_class.class_hash import (
+#         compute_class_hash as sw_compute_class_hash,
+#     )
+#     from starkware.starknet.services.api.contract_class.contract_class import (
+#         ContractClass as SwContractClass,
+#     )
 
 
 @pytest.mark.parametrize(
@@ -37,14 +35,14 @@ def test_compute_sierra_class_hash(sierra_contract_class_source, expected_class_
     sierra_contract_class = create_sierra_compiled_contract(sierra_contract_class_str)
     class_hash = compute_sierra_class_hash(sierra_contract_class)
 
-    if get_python_version()[:2] == (3, 9):
-        sierra_contract_class_dict = json.loads(sierra_contract_class_str)
-        sierra_contract_class_dict["abi"] = json.dumps(
-            sierra_contract_class_dict["abi"]
-        )
-        del sierra_contract_class_dict["sierra_program_debug_info"]
-
-        sw_contract_class = SwContractClass.load(sierra_contract_class_dict)
-        expected_class_hash = sw_compute_class_hash(sw_contract_class)
+    # if get_python_version()[:2] == (3, 9):
+    #     sierra_contract_class_dict = json.loads(sierra_contract_class_str)
+    #     sierra_contract_class_dict["abi"] = json.dumps(
+    #         sierra_contract_class_dict["abi"]
+    #     )
+    #     del sierra_contract_class_dict["sierra_program_debug_info"]
+    #
+    #     sw_contract_class = SwContractClass.load(sierra_contract_class_dict)
+    #     expected_class_hash = sw_compute_class_hash(sw_contract_class)
 
     assert class_hash == expected_class_hash
