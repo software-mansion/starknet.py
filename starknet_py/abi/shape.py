db7338dd6e7141c637619e78789967655e2d8872
Tomasz Miśkowicz <tomasz.jan.miskowicz@gmail.com>
2023-06-15 18:29:21 +0200

    import dependent of python version

--- starknet_py/abi/shape.py
+++ starknet_py/abi/shape.py
@@ -1,6 +1,10 @@
+import sys
 from typing import List, Literal, Union
 
-from typing_extensions import NotRequired, TypedDict
+if sys.version_info < (3, 11):
+    from typing_extensions import TypedDict, NotRequired
+else:
+    from typing import TypedDict, NotRequired
 
 STRUCT_ENTRY = "struct"
 FUNCTION_ENTRY = "function"
