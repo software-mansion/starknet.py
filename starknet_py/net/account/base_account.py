1d6f85be5c0ef739135e22839dd129ec829505e5
Kamil Jankowski <kamil.jankowski.x@gmail.com>
2023-06-20 12:06:52 +0200

    Parametrize block in Account.get_nonce (#1062)
    * Add block_number, block_hash to get_nonce
    
    * Update get_nonce test

--- starknet_py/net/account/base_account.py
+++ starknet_py/net/account/base_account.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from starknet_py.net.client import Client
-from starknet_py.net.client_models import Calls, SentTransactionResponse
+from starknet_py.net.client_models import Calls, Hash, SentTransactionResponse, Tag
 from starknet_py.net.models import AddressRepresentation, StarknetChainId
 from starknet_py.net.models.transaction import (
     Declare,
@@ -46,10 +46,17 @@ class BaseAccount(ABC):
         """
 
     @abstractmethod
-    async def get_nonce(self) -> int:
+    async def get_nonce(
+        self,
+        *,
+        block_hash: Optional[Union[Hash, Tag]] = None,
+        block_number: Optional[Union[int, Tag]] = None,
+    ) -> int:
         """
         Get the current nonce of the account.
 
+        :param block_hash: Block's hash or literals `"pending"` or `"latest"`
+        :param block_number: Block's number or literals `"pending"` or `"latest"`
         :return: nonce of the account.
         """
 
