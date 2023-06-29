fbe4002235decb5fff558add9cd7b165277aa288
Tomasz Miśkowicz <tomasz.jan.miskowicz@gmail.com>
2023-06-29 10:10:15 +0200

    fix test

--- starknet_py/tests/e2e/docs/quickstart/test_synchronous_full_node_client.py
+++ starknet_py/tests/e2e/docs/quickstart/test_synchronous_full_node_client.py
@@ -1,17 +1,11 @@
 from starknet_py.net.full_node_client import FullNodeClient
 
 
-def test_synchronous_gateway_client():
+def test_synchronous_full_node_client():
     # pylint: disable=no-member, unused-variable
     # docs: start
     node_url = "https://your.node.url"
-    synchronous_full_node_client = FullNodeClient(node_url=node_url)
-    # docs: end
+    full_node_client = FullNodeClient(node_url=node_url)
 
-    synchronous_full_node_client = FullNodeClient(
-        node_url="http://188.34.188.184:9545/rpc/v0.3"
-    )
-
-    # docs: start
-    call_result = synchronous_full_node_client.get_block_sync(block_number=1)
+    call_result = full_node_client.get_block_sync(block_number=1)
     # docs: end
