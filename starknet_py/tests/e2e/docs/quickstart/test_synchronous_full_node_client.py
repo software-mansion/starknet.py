adb129f7256cf626e7309084594bcb3cb071cb95
Tomasz Miśkowicz <tomasz.jan.miskowicz@gmail.com>
2023-06-29 12:35:12 +0200

    test is parametrized

--- starknet_py/tests/e2e/docs/quickstart/test_synchronous_full_node_client.py
+++ starknet_py/tests/e2e/docs/quickstart/test_synchronous_full_node_client.py
@@ -1,8 +1,11 @@
 from starknet_py.net.full_node_client import FullNodeClient
 
 
-def test_synchronous_full_node_client(full_node_client, map_contract, account):
-    # pylint: disable=no-member, unused-variable
+def test_synchronous_full_node_client(
+    full_node_client,
+    map_contract_declare_hash,  # pylint: disable=unused-argument
+):
+    # pylint: disable=unused-variable
     fixture_client = full_node_client
 
     # docs: start
@@ -10,16 +13,7 @@ def test_synchronous_full_node_client(full_node_client, map_contract, account):
     full_node_client = FullNodeClient(node_url=node_url)
     # docs: end
 
-    # pylint: disable=import-outside-toplevel
-    from starknet_py.contract import Contract
-
     full_node_client = fixture_client
-    key = 1234
-    contract = Contract.from_address_sync(
-        address=map_contract.address, provider=account
-    )
-    invocation = contract.functions["put"].invoke_sync(key, 7, max_fee=int(1e16))
-    invocation.wait_for_acceptance_sync()
 
     # docs: start
     call_result = full_node_client.get_block_sync(block_number=1)
