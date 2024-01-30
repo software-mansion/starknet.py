00077a39407f18ec669a9723616fc9a7b1c36b81
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2024-01-30 16:37:16 +0100

    Update docstrings for deploy methods about sepolia network

--- starknet_py/net/contract/contract.py
+++ starknet_py/net/contract/contract.py
@@ -84,7 +84,7 @@ class DeclareResult(SentTransaction):
         Deploys a contract.
 
         :param deployer_address: Address of the UDC. Is set to the address of
-            the default UDC (same address on mainnet/testnet/devnet) by default.
+            the default UDC (same address on mainnet/goerli/sepolia) by default.
             Must be set when using custom network other than ones listed above.
         :param salt: Optional salt. Random value is selected if it is not provided.
         :param unique: Determines if the contract should be salted with the account address.
@@ -126,7 +126,7 @@ class DeclareResult(SentTransaction):
         Deploys a contract.
 
         :param deployer_address: Address of the UDC. Is set to the address of
-            the default UDC (same address on mainnet/testnet/devnet) by default.
+            the default UDC (same address on mainnet/goerli/sepolia) by default.
             Must be set when using custom network other than ones listed above.
         :param salt: Optional salt. Random value is selected if it is not provided.
         :param unique: Determines if the contract should be salted with the account address.
@@ -422,7 +422,7 @@ class Contract:
         :param abi: An abi of the contract to be deployed.
         :param constructor_args: a ``list`` or ``dict`` of arguments for the constructor.
         :param deployer_address: Address of the UDC. Is set to the address of
-            the default UDC (same address on mainnet/testnet/devnet) by default.
+            the default UDC (same address on mainnet/goerli/sepolia) by default.
             Must be set when using custom network other than ones listed above.
         :param cairo_version: Version of the Cairo in which contract is written.
             By default, it is set to 0.
@@ -487,7 +487,7 @@ class Contract:
         :param abi: An abi of the contract to be deployed.
         :param constructor_args: a ``list`` or ``dict`` of arguments for the constructor.
         :param deployer_address: Address of the UDC. Is set to the address of
-            the default UDC (same address on mainnet/testnet/devnet) by default.
+            the default UDC (same address on mainnet/goerli/sepolia) by default.
             Must be set when using custom network other than ones listed above.
         :param cairo_version: Version of the Cairo in which contract is written.
             By default, it is set to 1.
