b685182f54810636c2901bcccc49a34b8e875903
ddoktorski <45050160+ddoktorski@users.noreply.github.com>
2024-02-01 10:48:06 +0100

    Support V3 transactions in the `Contract` class (#1262)
    * Remove TODO #1182 comment that was already done
    
    * Move contract deploy related test to `test_deploy` file
    
    * Split `prepare` method for: call, invoke_v1 and invoke_v3
    
    * Adjust interaction tests to the new `Contract` interface
    
    * Adjust remaining tests
    
    * Break down `Contract.declare` into v1, v2 and v3
    
    * Adjust tests to the new `Contract` declare interface
    
    * Move `_unpack_provider` function to `contract_utils`
    
    * Remove `_get_cairo_version` function from `Contract` class
    
    * Split deploy contract methods in `Contract` class
    
    * Adjust tests to the new `Contract` deploy interface
    
    * Ignore pylint locals for `deploy_contract` methods to fix lint
    
    * Add `_declare_contract` function
    
    * Rename `CallToSend` to `PreparedCallBase`
    
    * Replace Wei with Fri in docstrings
    
    * Add test to get balance in STRK
    
    * Update docstrings for deploy methods about sepolia network
    
    * Add version postfix for `Abi` and `AbiParser` imports
    
    * Use query version for fee estimation
    
    * Fix docs build and add code examples
    
    * Update docstrings

--- starknet_py/tests/e2e/docs/guide/test_contract_client_compatibility.py
+++ starknet_py/tests/e2e/docs/guide/test_contract_client_compatibility.py
@@ -7,7 +7,7 @@ async def test_create_call_from_contract(map_contract, account):
     contract = map_contract
 
     client = account.client
-    res = await map_contract.functions["put"].invoke(
+    res = await map_contract.functions["put"].invoke_v1(
         key=1234, value=9999, auto_estimate=True
     )
     await res.wait_for_acceptance()
@@ -16,7 +16,7 @@ async def test_create_call_from_contract(map_contract, account):
     from starknet_py.net.client_models import Call
 
     # Prepare a call through Contract
-    call = contract.functions["get"].prepare(key=1234)
+    call = contract.functions["get"].prepare_invoke_v1(key=1234)
     assert issubclass(type(call), Call)
 
     # Use call directly through Client
--- starknet_py/tests/e2e/docs/quickstart/test_using_account.py
+++ starknet_py/tests/e2e/docs/quickstart/test_using_account.py
@@ -16,11 +16,11 @@ async def test_using_account(account, map_compiled_contract):
     # docs: end
     # docs: start
     # Declare and deploy an example contract which implements a simple k-v store.
-    declare_result = await Contract.declare(
+    declare_result = await Contract.declare_v1(
         account=account, compiled_contract=map_compiled_contract, max_fee=MAX_FEE
     )
     await declare_result.wait_for_acceptance()
-    deploy_result = await declare_result.deploy(max_fee=MAX_FEE)
+    deploy_result = await declare_result.deploy_v1(max_fee=MAX_FEE)
     # Wait until deployment transaction is accepted
     await deploy_result.wait_for_acceptance()
 
@@ -30,7 +30,7 @@ async def test_using_account(account, map_compiled_contract):
     # Adds a transaction to mutate the state of k-v store. The call goes through account proxy, because we've used
     # Account to create the contract object
     await (
-        await map_contract.functions["put"].invoke(k, v, max_fee=int(1e16))
+        await map_contract.functions["put"].invoke_v1(k, v, max_fee=int(1e16))
     ).wait_for_acceptance()
 
     # Retrieves the value, which is equal to 4324 in this case
@@ -40,8 +40,8 @@ async def test_using_account(account, map_compiled_contract):
 
     # Creates a list of prepared function calls
     calls = [
-        map_contract.functions["put"].prepare(key=10, value=20),
-        map_contract.functions["put"].prepare(key=30, value=40),
+        map_contract.functions["put"].prepare_invoke_v1(key=10, value=20),
+        map_contract.functions["put"].prepare_invoke_v1(key=30, value=40),
     ]
 
     # Executes only one transaction with prepared calls
