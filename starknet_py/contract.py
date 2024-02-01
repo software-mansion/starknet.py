953a4c0087ab917586e076ed5c5c518ac46494f3
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-02-01 12:04:47 +0100

    linter

--- starknet_py/contract.py
+++ starknet_py/contract.py
@@ -901,6 +901,8 @@ class Contract:
         l1_resource_bounds: Optional[ResourceBounds] = None,
         auto_estimate: bool = False,
     ) -> DeclareResult:
+        # pylint: disable=too-many-arguments
+
         """
         Declares a contract.
 
