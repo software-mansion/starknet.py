efc04b720695def22a03edfb0a8cfac98ea111c4
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2024-02-06 15:06:58 +0100

    Fix failing test

--- starknet_py/tests/e2e/docs/guide/test_declaring_contracts.py
+++ starknet_py/tests/e2e/docs/guide/test_declaring_contracts.py
@@ -9,7 +9,7 @@ async def test_declaring_contracts(account, map_compiled_contract):
     # Account.sign_declare_v1 takes contract source code or compiled contract and returns
     # DeclareV1 transaction
     # Similarly, Account.sign_declare_v3 returns DeclareV3 transaction
-    declare_transaction = await account.sign_declare_v1_transaction(
+    declare_transaction = await account.sign_declare_v1(
         compiled_contract=contract_compiled, max_fee=int(1e16)
     )
 
