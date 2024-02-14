d47364a41a7395abcd14708eec5fc79abf6dfed9
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-02-14 15:19:52 +0100

    tests

--- starknet_py/tests/e2e/block_test.py
+++ starknet_py/tests/e2e/block_test.py
@@ -4,9 +4,11 @@ from starknet_py.contract import Contract
 from starknet_py.net.account.base_account import BaseAccount
 from starknet_py.net.client_models import (
     PendingStarknetBlock,
+    PendingStarknetBlockWithReceipts,
     PendingStarknetBlockWithTxHashes,
     StarknetBlock,
     StarknetBlockWithTxHashes,
+    TransactionWithReceipt,
 )
 from starknet_py.tests.e2e.fixtures.constants import MAX_FEE
 
@@ -89,3 +91,32 @@ async def test_get_block_with_txs_latest(
     assert blk.root is not None
     assert blk.timestamp is not None
     assert blk.sequencer_address is not None
+
+
+@pytest.mark.skip
+@pytest.mark.asyncio
+async def test_block_with_receipts_latest(
+    account,
+    map_contract_declare_hash,
+):
+    blk = await account.client.get_block_with_tx_hashes(block_number="latest")
+
+    assert isinstance(blk, StarknetBlockWithTxHashes)
+    assert isinstance(blk.transactions, list)
+    assert map_contract_declare_hash in blk.transactions
+    assert blk.block_hash is not None
+    assert blk.parent_block_hash is not None
+    assert blk.block_number is not None
+    assert blk.root is not None
+    assert blk.timestamp is not None
+    assert blk.sequencer_address is not None
+
+
+@pytest.mark.skip
+@pytest.mark.asyncio
+async def test_block_with_receipts_pending(account):
+    blk = await account.client.get_block_with_receipts(block_number="pending")
+
+    assert isinstance(blk, PendingStarknetBlockWithReceipts)
+    assert isinstance(blk.transactions, list)
+    assert isinstance(blk.transactions[0], TransactionWithReceipt)
