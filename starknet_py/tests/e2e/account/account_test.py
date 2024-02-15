f992bc853dfb5dc295a4e365fe30f8cfe4b69d0e
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-02-15 08:47:41 +0100

    Add estimate_fee to Account (#1279)
    * Add estimate_fee to Account
    
    * fix
    
    * add support list od transaction and allows use skip_validate
    
    * update
    
    * remove print
    
    * update
    
    * Update starknet_py/net/account/base_account.py
    
    Co-authored-by: ddoktorski <45050160+ddoktorski@users.noreply.github.com>
    
    * Update starknet_py/net/account/account.py
    
    Co-authored-by: ddoktorski <45050160+ddoktorski@users.noreply.github.com>
    
    * fmt
    
    * add test
    
    * fix test
    
    * update test
    
    * use two distinct transaction types in test
    
    * feedback
    
    ---------
    
    Co-authored-by: ddoktorski <45050160+ddoktorski@users.noreply.github.com>

--- starknet_py/tests/e2e/account/account_test.py
+++ starknet_py/tests/e2e/account/account_test.py
@@ -100,6 +100,54 @@ async def test_estimate_fee_for_declare_transaction(account, map_compiled_contra
 
 
 @pytest.mark.asyncio
+async def test_account_estimate_fee_for_declare_transaction(
+    account, map_compiled_contract
+):
+    declare_tx = await account.sign_declare_v1(
+        compiled_contract=map_compiled_contract, max_fee=MAX_FEE
+    )
+
+    estimated_fee = await account.estimate_fee(tx=declare_tx)
+
+    assert estimated_fee.unit == PriceUnit.WEI
+    assert isinstance(estimated_fee.overall_fee, int)
+    assert estimated_fee.overall_fee > 0
+    assert (
+        estimated_fee.gas_consumed * estimated_fee.gas_price
+        == estimated_fee.overall_fee
+    )
+
+
+@pytest.mark.asyncio
+async def test_account_estimate_fee_for_transactions(
+    account, map_compiled_contract, map_contract
+):
+    declare_tx = await account.sign_declare_v1(
+        compiled_contract=map_compiled_contract, max_fee=MAX_FEE
+    )
+
+    invoke_tx = await account.sign_invoke_v3(
+        calls=Call(map_contract.address, get_selector_from_name("put"), [3, 4]),
+        l1_resource_bounds=MAX_RESOURCE_BOUNDS_L1,
+        nonce=(declare_tx.nonce + 1),
+    )
+
+    estimated_fee = await account.estimate_fee(tx=[declare_tx, invoke_tx])
+
+    assert len(estimated_fee) == 2
+    assert isinstance(estimated_fee[0], EstimatedFee)
+    assert isinstance(estimated_fee[1], EstimatedFee)
+    assert estimated_fee[0].unit == PriceUnit.WEI
+    assert estimated_fee[1].unit == PriceUnit.FRI
+    assert isinstance(estimated_fee[0].overall_fee, int)
+    assert estimated_fee[0].overall_fee > 0
+    assert (
+        estimated_fee[0].gas_consumed * estimated_fee[0].gas_price
+        == estimated_fee[0].overall_fee
+    )
+
+
+@pytest.mark.asyncio
 @pytest.mark.parametrize("key, val", [(20, 20), (30, 30)])
 async def test_sending_multicall(account, map_contract, key, val):
     calls = [
