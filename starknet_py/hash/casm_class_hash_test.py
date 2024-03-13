d0b8f991e532cde5531f93cfb60fe9252fc763a2
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-03-13 12:00:52 +0100

    update casm_class_hash_tests

--- starknet_py/hash/casm_class_hash_test.py
+++ starknet_py/hash/casm_class_hash_test.py
@@ -10,12 +10,12 @@ from starknet_py.tests.e2e.fixtures.misc import read_contract
 @pytest.mark.parametrize(
     "casm_contract_class_source, expected_casm_class_hash",
     [
-        ("account_compiled.casm", 0x1cf09a2ab1c531bfd02cb26a3fa9ed02986fd3bb86e6b8874072fba4e55f504),
-        ("erc20_compiled.casm", 0x6d53db94c64d3562370253db94529b99f6837cb2f3b7062e11c657be89c1dae),
-        ("hello_starknet_compiled.casm", 0x785fa5f2bacf0bfe3bc413be5820a61e1ea63f2ec27ef00331ee9f46ad07603),
-        ("minimal_contract_compiled.casm", 0x186f6c4ca3af40dbcbf3f08f828ab0ee072938aaaedccc74ef3b9840cbd9fb3),
-        ("test_contract_compiled.casm", 0x379b75c0922c68e73f6451b69e8e50b7f0745e6fa3f67ffc0b9608238eeaf45),
-        ("token_bridge_compiled.casm", 0x1d60f20e5dd449af4e6b0d63821cfa95f3469faa942caf78eba2172e2ec3468),
+        ("account_compiled.casm", 0x18799df07f230e6ae5440dd3396afd6a2d6e627be3b8ab3e4fa64edb30f64d2),
+        ("erc20_compiled.casm", 0x5adc857416202a5902c01168542e188c3aa6380f57c911ae98cf20bc52be367),
+        ("hello_starknet_compiled.casm", 0x6ff9f7df06da94198ee535f41b214dce0b8bafbdb45e6c6b09d4b3b693b1f17),
+        ("minimal_contract_compiled.casm", 0x5fd87fec0614148ce800defb1ad3dbb9212451672e244d24fb1aa359e342055),
+        ("test_contract_compiled.casm", 0x2193add92c182c9236f0c156f11dc4f18d5a78fd9b763a3c0f4a1d3bd8b87d4),
+        ("token_bridge_compiled.casm", 0x87c179b5f09a8137eb7b28c621eb8337b61216e4381d6e200200f22c511aad),
         ("precompiled/starknet_contract_v2_6.casm", 0x603dd72504d8b0bc54df4f1102fdcf87fc3b2b94750a9083a5876913eec08e4),
     ],
 )
