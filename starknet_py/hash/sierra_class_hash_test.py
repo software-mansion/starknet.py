a2c2ef8f3eb8c4d4e64698efcda7b4b6f30bcb9b
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-03-13 13:18:04 +0100

    update test_compute_sierra_class_hash

--- starknet_py/hash/sierra_class_hash_test.py
+++ starknet_py/hash/sierra_class_hash_test.py
@@ -10,12 +10,12 @@ from starknet_py.tests.e2e.fixtures.misc import read_contract
     "sierra_contract_class_source, expected_class_hash",
     # fmt: off
     [
-        ("account_compiled.json", 0x6cb1e2b2b7a2eb8231357f8bee5cb6683476f350d35986afff6b67bf9e66b6c),
-        ("erc20_compiled.json", 0x5dc48d64a0f3852a4ac2b06f9b2a801177f35952715f32d3a7ca60af235e762),
-        ("hello_starknet_compiled.json", 0x4ec2ecf58014bc2ffd7c84843c3525e5ecb0a2cac33c47e9c347f39fc0c0944),
-        ("minimal_contract_compiled.json", 0xa298b56801319b054855d39720eab22502e77627552e564d3bf50bd7844df9),
-        ("test_contract_compiled.json", 0x28d7f03cced748cd9913afa5549edf4a02def8508fe1ca7feea2f8d403918a6),
-        ("token_bridge_compiled.json", 0x4f2d81d7c0da5ec97f19b8195748a559de42207ff8bc3ac5f818e752c431c7b),
+        ("account_compiled.json", 0x12f3bdfbb32ab45078a0af658b78617023d0ece273e7b2e3d14f03a7a9cba4e),
+        ("erc20_compiled.json", 0x9e3dbe53e170edc8b6817251a0e5a6e417cd04295015017a1d4e86a4a40d1a),
+        ("hello_starknet_compiled.json", 0x5cb6ab123cf9f06798dcd8227d46f4d5d5d157b4ce51ae2236aa5d9b1cb01ea),
+        ("minimal_contract_compiled.json", 0x614cde60994efdac8c95401809e98abd4221b02b5174f25b599f8145c11fb80),
+        ("test_contract_compiled.json", 0x40e041157a32a43e0c20389f4dcec0a97a22718ddbc24f223f7553fa0de3c08),
+        ("token_bridge_compiled.json", 0x1830073d65ce002456593dfdfbc322bee0577910fb857509822843b5e3792b),
     ],
     # fmt: on
 )
