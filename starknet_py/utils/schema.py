0bed55532f358426f8433d16be34d1171641ff16
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-03-18 16:08:49 +0100

    check value of env

--- starknet_py/utils/schema.py
+++ starknet_py/utils/schema.py
@@ -3,11 +3,14 @@ import os
 from marshmallow import EXCLUDE, RAISE
 from marshmallow import Schema as MarshmallowSchema
 
+MARSHMALLOW_UKNOWN_EXCLUDE = os.environ.get("STARKNET_PY_MARSHMALLOW_UKNOWN_EXCLUDE")
+
 
 class Schema(MarshmallowSchema):
     class Meta:
         unknown = (
             EXCLUDE
-            if os.environ.get("STARKNET_PY_MARSHMALLOW_UKNOWN_EXCLUDE").lower() == 'true'
+            if MARSHMALLOW_UKNOWN_EXCLUDE
+            and MARSHMALLOW_UKNOWN_EXCLUDE.lower() == "true"
             else RAISE
         )
