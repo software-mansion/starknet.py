6425e2278cb80bf4ba73b8591dd0e0a863532122
Tomasz Kumor <tomasz.kumor@swmansion.com>
2024-03-19 11:26:55 +0100

    fix docs tests

--- starknet_py/tests/e2e/docs/guide/test_simple_declare_and_deploy_cairo1.py
+++ starknet_py/tests/e2e/docs/guide/test_simple_declare_and_deploy_cairo1.py
@@ -1,6 +1,6 @@
 import pytest
 
-from starknet_py.tests.e2e.fixtures.misc import read_contract
+from starknet_py.tests.e2e.fixtures.misc import load_contract
 
 
 @pytest.mark.asyncio
@@ -10,15 +10,14 @@ async def test_simple_declare_and_deploy(account):
     from starknet_py.contract import Contract
 
     # docs: end
-    compiled_contract = read_contract("account_compiled.json")
-    compiled_contract_casm = read_contract("account_compiled.casm")
+    compiled_contract = load_contract("Account")
     constructor_args = {"public_key_": 0x123}
 
     # docs: start
     declare_result = await Contract.declare_v2(
         account=account,
-        compiled_contract=compiled_contract,
-        compiled_contract_casm=compiled_contract_casm,
+        compiled_contract=compiled_contract["sierra"],
+        compiled_contract_casm=compiled_contract["casm"],
         max_fee=int(1e18),
     )
     await declare_result.wait_for_acceptance()
@@ -33,5 +32,5 @@ async def test_simple_declare_and_deploy(account):
 
     assert isinstance(declare_result.hash, int)
     assert isinstance(declare_result.class_hash, int)
-    assert declare_result.compiled_contract == compiled_contract
+    assert declare_result.compiled_contract == compiled_contract["sierra"]
     assert contract.address != 0
