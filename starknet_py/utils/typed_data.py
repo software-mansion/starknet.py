199d29077685941cbf6fd182b57b43dd4255e470
Fiiranek <jellybean@onet.eu>
2024-06-27 19:48:29 +0200

    Rename `_get_basic_types_values()` to `_get_basic_types()`

--- starknet_py/utils/typed_data.py
+++ starknet_py/utils/typed_data.py
@@ -132,10 +132,7 @@ class TypedData:
             hashes = [self._encode_value(type_name, val) for val in value]
             return compute_hash_on_elements(hashes)
 
-        basic_types_values = [
-            bt.value for bt in _get_basic_types(self.domain.resolved_revision)
-        ]
-        if type_name not in basic_types_values:
+        if type_name not in _get_basic_types(self.domain.resolved_revision):
             raise ValueError(f"Type [{type_name}] is not defined in types.")
 
         basic_type = BasicType(type_name)
@@ -173,7 +170,7 @@ class TypedData:
         if self.domain.separator_name not in self.types:
             raise ValueError(f"Types must contain '{self.domain.separator_name}'.")
 
-        reserved_type_names = ["felt", "string", "selector", "merkletree"]
+        reserved_type_names = _get_basic_types(self.domain.resolved_revision)
 
         for type_name in reserved_type_names:
             if type_name in self.types:
@@ -334,21 +331,24 @@ class BasicType(Enum):
     FELT = "felt"
     SELECTOR = "selector"
     MERKLE_TREE = "merkletree"
+    STRING = "string"
     SHORT_STRING = "shortstring"
 
 
-def _get_basic_types(revision: Revision) -> List[BasicType]:
+def _get_basic_types(revision: Revision) -> List[str]:
     basic_types_v0 = [
         BasicType.FELT,
         BasicType.SELECTOR,
         BasicType.MERKLE_TREE,
+        BasicType.STRING,
     ]
 
     basic_types_v1 = basic_types_v0 + [
         BasicType.SHORT_STRING,
     ]
 
-    return basic_types_v0 if revision == Revision.V0 else basic_types_v1
+    basic_types = basic_types_v0 if revision == Revision.V0 else basic_types_v1
+    return [basic_type.value for basic_type in basic_types]
 
 
 # pylint: disable=unused-argument
