5e9bfcea0686e1e615253369771920353bf16d96
Fiiranek <jellybean@onet.eu>
2024-07-03 11:09:37 +0200

    Fix retrieving params in `TypedData._get_dependencies()`

--- starknet_py/utils/typed_data.py
+++ starknet_py/utils/typed_data.py
@@ -341,7 +341,7 @@ class TypedData:
 
         while to_visit:
             current_type = to_visit.pop(0)
-            params = self._all_types[current_type] if current_type in self.types else []
+            params = self._all_types[current_type] if current_type in self._all_types else []
 
             for param in params:
                 if isinstance(param, EnumParameter):
