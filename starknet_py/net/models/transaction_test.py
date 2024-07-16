ea9556e2105cba5c46190766cc59dcb64c0be6d7
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2024-07-16 14:53:08 +0200

    Remove `test_serialize_deserialize_invoke`

--- starknet_py/net/models/transaction_test.py
+++ starknet_py/net/models/transaction_test.py
@@ -36,22 +36,3 @@ def test_declare_compress_program(balance_contract):
 
     deserialized = cast(Declare, schema.load(serialized))
     assert deserialized.contract_class == contract_class
-
-
-def test_serialize_deserialize_invoke():
-    data = {
-        "sender_address": "0x1",
-        "calldata": ["0x1", "0x2", "0x3"],
-        "max_fee": "0x1",
-        "signature": [],
-        "nonce": "0x1",
-        "version": "0x1",
-        "type": "INVOKE_FUNCTION",
-    }
-    invoke = InvokeV1Schema().load(data)
-    serialized_invoke = InvokeV1Schema().dump(invoke)
-
-    assert isinstance(invoke, InvokeV1)
-    assert invoke.type == TransactionType.INVOKE
-    assert isinstance(serialized_invoke, dict)
-    assert serialized_invoke["type"] == "INVOKE_FUNCTION"
