6edfb7e2db03876df63686e615c055793196ce75
Fiiranek <jellybean@onet.eu>
2024-07-18 11:22:46 +0200

    Add `LedgerStarknetApp.sign_hash()`; Implement `sign_transaction()` and `sign_message()` in `LedgerSigner`

--- starknet_py/net/signer/ledger_signer.py
+++ starknet_py/net/signer/ledger_signer.py
@@ -3,17 +3,19 @@ from typing import List
 from bip_utils import Bip32KeyIndex, Bip32Path, Bip32Utils
 from ledgerwallet.client import LedgerClient
 
+from starknet_py.constants import EIP_2645_PATH_LENGTH, EIP_2645_PURPOSE, STARKNET_CLA, PUBLIC_KEY_SIZE, SIGNATURE_SIZE
 from starknet_py.net.models import AccountTransaction
+from starknet_py.net.models.chains import ChainId
 from starknet_py.net.signer import BaseSigner
 from starknet_py.utils.typed_data import TypedData
 
-STARKNET_CLA = 0x5A
-EIP_2645_PURPOSE = 0x80000A55
-EIP_2645_PATH_LENGTH = 6
-PUBLIC_KEY_SIZE = 65
-
 
 def _parse_derivation_path_str(derivation_path_str) -> Bip32Path:
+    """
+    Parse a derivation path string to a Bip32Path object.
+
+    :param derivation_path_str: Derivation path string.
+    """
     if not derivation_path_str:
         raise ValueError("Empty derivation path")
 
@@ -34,52 +36,100 @@ def _parse_derivation_path_str(derivation_path_str) -> Bip32Path:
     return Bip32Path(path_elements)
 
 
-class LedgerStarknetApp:
-    def __init__(self, client: LedgerClient):
-        self.client = client
+def _derivation_path_to_bytes(derivation_path: Bip32Path) -> bytes:
+    """
+    Convert a derivation path to a bytes object.
 
-    def get_public_key(self, derivation_path: Bip32Path, display: bool = False) -> int:
-        data = b"".join(index.ToBytes() for index in derivation_path)
+    :param derivation_path: Derivation path.
+    """
+    return b"".join(index.ToBytes() for index in derivation_path)
+
+
+class LedgerStarknetApp:
+    def __init__(self):
+        self.client = LedgerClient(cla=STARKNET_CLA)
+
+    def get_public_key(self, derivation_path: Bip32Path, device_confirmation: bool = False) -> int:
+        """
+        Get the public key for the given derivation path.
+
+        :param derivation_path: Derivation path of the account.
+        :param device_confirmation: Whether to display confirmation on the device for extra security.
+        """
+
+        data = _derivation_path_to_bytes(derivation_path)
         response = self.client.apdu_exchange(
             ins=0x01,
             data=data,
-            p1=0x01 if display else 0x00,
+            p1=0x01 if device_confirmation else 0x00,
             p2=0x00,
         )
 
-        byte_slice = response[1:33]
         if len(response) != PUBLIC_KEY_SIZE:
             raise ValueError(
                 f"Unexpected response length (expected: {PUBLIC_KEY_SIZE}, actual: {len(data)}"
             )
 
-        public_key = int.from_bytes(byte_slice, byteorder="big")
+        public_key = int.from_bytes(response[1:33], byteorder="big")
         return public_key
+
+    def sign_hash(self, derivation_path: Bip32Path, hash_val: int) -> List[int]:
+        """
+        Request a signature for a raw hash with the given derivation path.
+        Currently, the Ledger app only supports blind signing raw hashes.
+
+        :param derivation_path: Derivation path of the account.
+        :param hash_val: Hash to sign.
+        """
+
+        # sign hash command 1
+        data = _derivation_path_to_bytes(derivation_path)
+        self.client.apdu_exchange(
+            ins=0x02,
+            data=data,
+            p1=0x00,
+            p2=0x00,
+        )
+
+        # for some reason ledger the Ledger app expects the data to be left shifted by 4 bits
+        shifted_int = hash_val << 4
+
+        shifted_bytes = shifted_int.to_bytes(32, byteorder="big")
+        response = self.client.apdu_exchange(
+            ins=0x02,
+            data=shifted_bytes,
+            p1=0x01,
+            p2=0x00,
+        )
+
+        if len(data) != SIGNATURE_SIZE + 1 or data[0] != SIGNATURE_SIZE:
+            raise ValueError(
+                f"Unexpected response length (expected: {SIGNATURE_SIZE}, actual: {len(data)}"
+            )
+
+        r, s = int.from_bytes(response[1:33], byteorder="big"), int.from_bytes(response[33:65], byteorder="big")
+        return [r, s]
 
 
 class LedgerSigner(BaseSigner):
-    def __init__(self, derivation_path_str: str):
+    def __init__(self, derivation_path_str: str, chain_id: ChainId):
         """
         :param derivation_path_str: Derivation path string of the account.
+        :param chain_id: ChainId of the chain.
         """
 
-        client = LedgerClient(cla=STARKNET_CLA)
-        self.app = LedgerStarknetApp(client)
+        self.app = LedgerStarknetApp()
         self.derivation_path = _parse_derivation_path_str(derivation_path_str)
+        self.chain_id = chain_id
 
     @property
     def public_key(self) -> int:
-        return self.app.get_public_key(self.derivation_path, display=False)
-
-    @property
-    def private_key(self) -> int:
-        return 0
+        return self.app.get_public_key(derivation_path=self.derivation_path)
 
     def sign_transaction(self, transaction: AccountTransaction) -> List[int]:
-        return [0]
+        tx_hash = transaction.calculate_hash(self.chain_id)
+        return self.app.sign_hash(derivation_path=self.derivation_path, hash_val=tx_hash)
 
     def sign_message(self, typed_data: TypedData, account_address: int) -> List[int]:
-        return [0]
-
-
-signer = LedgerSigner("m/2645'/1195502025'/1470455285'/0'/0'/0")
+        msg_hash = typed_data.message_hash(account_address)
+        return self.app.sign_hash(derivation_path=self.derivation_path, hash_val=msg_hash)
