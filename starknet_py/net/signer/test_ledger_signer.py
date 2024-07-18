ccf1d28fa3817ef15fda2312112edd0a991498b4
Fiiranek <jellybean@onet.eu>
2024-07-18 11:25:16 +0200

    Update ledger signer init tests

--- starknet_py/net/signer/test_ledger_signer.py
+++ starknet_py/net/signer/test_ledger_signer.py
@@ -1,18 +1,25 @@
 import pytest
 
+from starknet_py.net.models import StarknetChainId
 from starknet_py.net.signer.ledger_signer import EIP_2645_PATH_LENGTH, LedgerSigner
 
 
 def test_init_ledger_signer_with_invalid_derivation_path():
     with pytest.raises(ValueError, match="Empty derivation path"):
-        LedgerSigner("")
+        LedgerSigner(derivation_path_str="", chain_id=StarknetChainId.SEPOLIA)
 
     with pytest.raises(
-        ValueError, match=rf"Derivation path is not {EIP_2645_PATH_LENGTH}-level long"
+            ValueError, match=rf"Derivation path is not {EIP_2645_PATH_LENGTH}-level long"
     ):
-        LedgerSigner("m/2645'/1195502025'/1470455285'/0'/0'/0/0")
+        LedgerSigner(
+            derivation_path_str="m/2645'/1195502025'/1470455285'/0'/0'/0/0",
+            chain_id=StarknetChainId.SEPOLIA,
+        )
 
     with pytest.raises(
-        ValueError, match=r"Derivation path is not prefixed with m/2645."
+            ValueError, match=r"Derivation path is not prefixed with m/2645."
     ):
-        LedgerSigner("m/1234'/1195502025'/1470455285'/0'/0'/0")
+        LedgerSigner(
+            derivation_path_str="m/1234'/1195502025'/1470455285'/0'/0'/0",
+            chain_id=StarknetChainId.SEPOLIA,
+        )
