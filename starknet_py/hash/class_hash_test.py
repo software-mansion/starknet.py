447f70824a7cdd82e8725b6738ad50d855c5eba8
jumpman <57270771+kkawula@users.noreply.github.com>
2024-07-24 10:54:29 +0200

    Remove 'universal_deployer.cairo' code

--- starknet_py/hash/class_hash_test.py
+++ starknet_py/hash/class_hash_test.py
@@ -17,7 +17,6 @@ from starknet_py.tests.e2e.fixtures.misc import read_contract
         ("erc20_compiled.json", 0x2c709fc176283331897d0c5f113ba64b00e1530c3e91103dcf1b05a056b1aa1),
         ("oz_proxy_compiled.json", 0x382f95037fa7983ff69465b9d3f7394ce336870631066de682cf547dc1899dd),
         ("argent_proxy_compiled.json", 0x743aa3636b7c795931e9c4ed56dc57e7edda223a66c09df04fda40f9ba4cd53),
-        ("universal_deployer_compiled.json", 0x710bb1f5ef7f208249a370372a7586b72a759fbd2923013b14bd7f2e51bc4c),
         ("precompiled/oz_proxy_address_0.8.1_compiled.json", 0x413c36c287cb410d42f9e531563f68ac60a2913b5053608d640fb9b643acfe6),
     ]
 )
