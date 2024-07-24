1ace4873ca36cdf71b5d5b37572095dcc39f2afb
jumpman <57270771+kkawula@users.noreply.github.com>
2024-07-24 14:47:13 +0200

    Update 'test_compute_class_hash' test

--- starknet_py/hash/class_hash_test.py
+++ starknet_py/hash/class_hash_test.py
@@ -15,9 +15,6 @@ from starknet_py.tests.e2e.fixtures.misc import read_contract
         ("balance_compiled.json", 0x7a98eab69a2592ef5d3805990a43525d633ddc42b4d5b2524c7f38b7c59265f),
         ("map_compiled.json", 0x5eefff2c17c81fb81b1c34d2a9f324e7baf8c3099165b94d037a84b74b6900e),
         ("erc20_compiled.json", 0x2c709fc176283331897d0c5f113ba64b00e1530c3e91103dcf1b05a056b1aa1),
-        ("oz_proxy_compiled.json", 0x382f95037fa7983ff69465b9d3f7394ce336870631066de682cf547dc1899dd),
-        ("argent_proxy_compiled.json", 0x743aa3636b7c795931e9c4ed56dc57e7edda223a66c09df04fda40f9ba4cd53),
-        ("precompiled/oz_proxy_address_0.8.1_compiled.json", 0x413c36c287cb410d42f9e531563f68ac60a2913b5053608d640fb9b643acfe6),
     ]
 )
 def test_compute_class_hash(contract_source, expected_class_hash):
