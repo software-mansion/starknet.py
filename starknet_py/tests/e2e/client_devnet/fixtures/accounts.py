541deb6ce5fc13588389f9597307f3ec7822ed4f
jumpman <57270771+kkawula@users.noreply.github.com>
2024-07-25 15:21:52 +0200

    Add comment to address in 'account_to_impersonate' fixture

--- starknet_py/tests/e2e/client_devnet/fixtures/accounts.py
+++ starknet_py/tests/e2e/client_devnet/fixtures/accounts.py
@@ -25,6 +25,12 @@ async def account_forked_devnet(
 
 @pytest.fixture(scope="package")
 def account_to_impersonate(devnet_client_fork_mode: DevnetClient) -> BaseAccount:
+    """
+    Creates an account instance for impersonation.
+
+    :param address: address from Sepolia testnet that is not in the local state,
+                    so it can be impersonated.
+    """
     return Account(
         address="0x043abaa073c768ebf039c0c4f46db9acc39e9ec165690418060a652aab39e7d8",
         client=devnet_client_fork_mode,
