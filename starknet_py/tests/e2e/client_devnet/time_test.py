aba308b3dd80bf235fda3e7fd414eb7359e3e2ad
jumpman <57270771+kkawula@users.noreply.github.com>
2024-07-25 15:06:17 +0200

    Fix names in 'time_test.py'

--- starknet_py/tests/e2e/client_devnet/time_test.py
+++ starknet_py/tests/e2e/client_devnet/time_test.py
@@ -2,24 +2,25 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_time_advancing(devnet_client):
+async def test_set_and_increase_time(devnet_client):
     time = 3384617820
-    step, error = 100, 3
+    increase_value, error_margin = 100, 2
 
     await devnet_client.set_time(time, generate_block=True)
     block = await devnet_client.get_block(block_number="latest")
 
     assert block.timestamp == time
 
-    await devnet_client.increase_time(step)
+    await devnet_client.increase_time(increase_value)
     block = await devnet_client.get_block(block_number="latest")
 
-    assert block.timestamp <= time + step + error
-    assert block.timestamp >= time + step
+    assert (
+        time + increase_value <= block.timestamp <= time + increase_value + error_margin
+    )
 
 
 @pytest.mark.asyncio
-async def test_time_set_with_generate_block(devnet_client):
+async def test_set_time_with_generate_block(devnet_client):
     time = 3384617820
 
     block_number_before = await devnet_client.get_block_number()
@@ -30,7 +31,7 @@ async def test_time_set_with_generate_block(devnet_client):
 
 
 @pytest.mark.asyncio
-async def test_time_set_without_generate_block(devnet_client):
+async def test_set_time_without_generate_block(devnet_client):
     time = 3384617820
 
     block_number_before = await devnet_client.get_block_number()
