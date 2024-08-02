f480a7365ed0046dc899d3f885edc02decbf3d2e
Fiiranek <jellybean@onet.eu>
2024-08-02 13:12:11 +0200

    Update `test_generate`

--- starknet_py/net/signer/test_key_pair.py
+++ starknet_py/net/signer/test_key_pair.py
@@ -1,6 +1,9 @@
+from starknet_py.constants import FIELD_PRIME
 from starknet_py.net.signer.key_pair import KeyPair
 
 
 def test_generate():
     key_pair = KeyPair.generate()
     assert len(hex(key_pair.private_key)) == 65
+    assert key_pair.private_key < FIELD_PRIME
+    assert key_pair.public_key < FIELD_PRIME
