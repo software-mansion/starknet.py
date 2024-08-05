2dce96ffcee0139e111af1aa0556d27c69227a81
Fiiranek <jellybean@onet.eu>
2024-08-05 12:42:15 +0200

    Fix `test_generate`

--- starknet_py/net/signer/test_key_pair.py
+++ starknet_py/net/signer/test_key_pair.py
@@ -4,6 +4,8 @@ from starknet_py.net.signer.key_pair import KeyPair
 
 def test_generate():
     key_pair = KeyPair.generate()
-    assert len(hex(key_pair.private_key)) == 65
+    hex_private_key = hex(key_pair.private_key)[2:]
+    padded_hex_private_key = hex_private_key.zfill(64)
+    assert len(padded_hex_private_key) == 64
     assert key_pair.private_key < FIELD_PRIME
     assert key_pair.public_key < FIELD_PRIME
