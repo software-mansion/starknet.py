93554b3734dc95d34dd94b22b55c740256b4602a
jumpman <57270771+kkawula@users.noreply.github.com>
2024-08-20 13:46:28 +0200

    Fix 'test_deploy_account_v1'

--- starknet_py/tests/e2e/account/account_test.py
+++ starknet_py/tests/e2e/account/account_test.py
@@ -482,6 +482,7 @@ async def test_deploy_account_v1(client, deploy_account_details_factory, map_con
         key_pair=key_pair,
         client=client,
         max_fee=int(1e16),
+        constructor_calldata=[key_pair.public_key],
     )
     await deploy_result.wait_for_acceptance()
 
