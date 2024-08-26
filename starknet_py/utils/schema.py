3e51e779891b9550fa29d69f53b401c17806335d
kkawula <57270771+kkawula@users.noreply.github.com>
2024-08-26 12:02:58 +0200

    Refactor 'Meta' class for marshmallow schema

--- starknet_py/utils/schema.py
+++ starknet_py/utils/schema.py
@@ -2,12 +2,19 @@ import os
 
 from marshmallow import EXCLUDE, RAISE
 from marshmallow import Schema as MarshmallowSchema
+from marshmallow import SchemaOpts
 
 MARSHMALLOW_UNKNOWN_EXCLUDE = os.environ.get("STARKNET_PY_MARSHMALLOW_UNKNOWN_EXCLUDE")
 
 
-class Schema(MarshmallowSchema):
-    class Meta:
-        unknown = (
+class UnknownOpts(SchemaOpts):
+
+    def __init__(self, meta, **kwargs):
+        SchemaOpts.__init__(self, meta, **kwargs)
+        self.unknown = (
             EXCLUDE if (MARSHMALLOW_UNKNOWN_EXCLUDE or "").lower() == "true" else RAISE
         )
+
+
+class Schema(MarshmallowSchema):
+    OPTIONS_CLASS = UnknownOpts
