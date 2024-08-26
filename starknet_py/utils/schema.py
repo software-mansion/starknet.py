10cc3e2ccb656ddb257dc5e72a14bd181221b47d
kkawula <57270771+kkawula@users.noreply.github.com>
2024-08-26 11:46:52 +0200

    Fix typo in 'STARKNET_PY_MARSHMALLOW_UKNOWN_EXCLUDE'

--- starknet_py/utils/schema.py
+++ starknet_py/utils/schema.py
@@ -3,11 +3,11 @@ import os
 from marshmallow import EXCLUDE, RAISE
 from marshmallow import Schema as MarshmallowSchema
 
-MARSHMALLOW_UKNOWN_EXCLUDE = os.environ.get("STARKNET_PY_MARSHMALLOW_UKNOWN_EXCLUDE")
+MARSHMALLOW_UNKNOWN_EXCLUDE = os.environ.get("STARKNET_PY_MARSHMALLOW_UNKNOWN_EXCLUDE")
 
 
 class Schema(MarshmallowSchema):
     class Meta:
         unknown = (
-            EXCLUDE if (MARSHMALLOW_UKNOWN_EXCLUDE or "").lower() == "true" else RAISE
+            EXCLUDE if (MARSHMALLOW_UNKNOWN_EXCLUDE or "").lower() == "true" else RAISE
         )
