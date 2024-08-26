cdeb3177e2770f99e068c6b88992fd5e6ff6b481
Franciszek Job <54181625+franciszekjob@users.noreply.github.com>
2024-08-26 09:58:08 +0200

    Update starknet_py/net/signer/test_key_pair.py
    Co-authored-by: kkawula <57270771+kkawula@users.noreply.github.com>

--- starknet_py/net/signer/test_key_pair.py
+++ starknet_py/net/signer/test_key_pair.py
@@ -2,7 +2,7 @@ from starknet_py.constants import FIELD_PRIME
 from starknet_py.net.signer.key_pair import KeyPair
 
 
-def test_generate():
+def test_generate_key_pair():
     key_pair = KeyPair.generate()
     hex_private_key = hex(key_pair.private_key)[2:]
     padded_hex_private_key = hex_private_key.zfill(64)
