588028109e24cd246e1dd8d840d006617a1833dc
kkawula <57270771+kkawula@users.noreply.github.com>
2024-09-03 10:05:20 +0200

    Remove unused function

--- starknet_py/tests/e2e/fixtures/contracts.py
+++ starknet_py/tests/e2e/fixtures/contracts.py
@@ -9,17 +9,6 @@ from starknet_py.net.account.base_account import BaseAccount
 from starknet_py.tests.e2e.fixtures.constants import MAX_FEE, STRK_FEE_CONTRACT_ADDRESS
 
 
-async def deploy_contract(account: BaseAccount, class_hash: int, abi: List) -> Contract:
-    """
-    Deploys a contract and returns its instance.
-    """
-    deployment_result = await Contract.deploy_contract_v1(
-        account=account, class_hash=class_hash, abi=abi, max_fee=MAX_FEE
-    )
-    deployment_result = await deployment_result.wait_for_acceptance()
-    return deployment_result.deployed_contract
-
-
 @pytest.fixture(scope="package")
 def eth_fee_contract(account: BaseAccount, cairo_0_fee_contract_abi) -> Contract:
     """
