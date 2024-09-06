08059a3f56a9b4a4ad04587d9e50c64f5a0631d2
jumpman <57270771+kkawula@users.noreply.github.com>
2024-09-06 09:46:37 +0200

    Revert "Reapply "Skip ledger tests""
    This reverts commit c88cb429a2d93ab95229e6a683003c53c9b70cfe.

--- starknet_py/tests/unit/signer/test_ledger_signer.py
+++ starknet_py/tests/unit/signer/test_ledger_signer.py
@@ -1,3 +1,5 @@
+from sys import platform
+
 import pytest
 
 from starknet_py.common import create_sierra_compiled_contract
@@ -20,6 +22,7 @@ from starknet_py.tests.e2e.fixtures.misc import load_contract
 
 # TODO (#1425): Currently Ledger tests are skipped on Windows due to different Speculos setup.
 @pytest.mark.skipif(
+    platform == "win32",
     reason="Testing Ledger is skipped on Windows due to different Speculos setup.",
 )
 def test_init_with_invalid_derivation_path():
@@ -79,6 +82,7 @@ sierra_contract_class = create_sierra_compiled_contract(compiled_contract)
     ],
 )
 @pytest.mark.skipif(
+    platform == "win32",
     reason="Testing Ledger is skipped on Windows due to different Speculos setup.",
 )
 def test_sign_transaction(transaction):
@@ -101,6 +105,7 @@ def test_sign_transaction(transaction):
 
 
 @pytest.mark.skipif(
+    platform == "win32",
     reason="Testing Ledger is skipped on Windows due to different Speculos setup.",
 )
 def test_create_account_with_ledger_signer():
@@ -137,6 +142,7 @@ async def _get_account_balance_strk(client: FullNodeClient, address: int):
 
 @pytest.mark.asyncio
 @pytest.mark.skipif(
+    platform == "win32",
     reason="Testing Ledger is skipped on Windows due to different Speculos setup.",
 )
 async def test_deploy_account_and_transfer(client):
