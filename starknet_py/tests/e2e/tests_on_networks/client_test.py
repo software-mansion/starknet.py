a35bbad6384a8c2b2ee3836ccd19285fa27f370e
Fiiranek <jellybean@onet.eu>
2024-09-09 00:53:05 +0200

    Format

--- starknet_py/tests/e2e/tests_on_networks/client_test.py
+++ starknet_py/tests/e2e/tests_on_networks/client_test.py
@@ -100,7 +100,7 @@ async def test_transaction_not_received_max_fee_too_small(account_sepolia_testne
     with pytest.raises(
         ClientError,
         match=r"Client failed with code 55. "
-              r"Message: Account validation failed. Data: Max fee \(\d+\) is too low. Minimum fee: \d+.",
+        r"Message: Account validation failed. Data: Max fee \(\d+\) is too low. Minimum fee: \d+.",
     ):
         await account.client.send_transaction(sign_invoke)
 
@@ -118,7 +118,7 @@ async def test_transaction_not_received_max_fee_too_big(account_sepolia_testnet)
     with pytest.raises(
         ClientError,
         match=r"Client failed with code 55. "
-              r"Message: Account validation failed. Data: Max fee \(\d+\) exceeds balance \(\d+\).",
+        r"Message: Account validation failed. Data: Max fee \(\d+\) exceeds balance \(\d+\).",
     ):
         await account.client.send_transaction(sign_invoke)
 
