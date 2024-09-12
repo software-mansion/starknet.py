bf91319b1a254c136ef779939fee79a9c1b19451
Franciszek Job <54181625+franciszekjob@users.noreply.github.com>
2024-09-12 16:54:17 +0200

    Refactor signer tests (#1488)
    * Refactor ledger signer tests
    
    * Refactor start curve signer tests
    
    * Update mock calculated hash
    
    * Remove unused compiled contract

--- starknet_py/tests/unit/signer/test_stark_curve_signer.py
+++ starknet_py/tests/unit/signer/test_stark_curve_signer.py
@@ -1,45 +1,18 @@
+from unittest.mock import MagicMock, Mock
+
 import pytest
 
-from starknet_py.common import create_sierra_compiled_contract
 from starknet_py.net.models import StarknetChainId
-from starknet_py.net.models.transaction import DeclareV3, DeployAccountV1, InvokeV1
+from starknet_py.net.models.transaction import DeclareV3, DeployAccountV3, InvokeV3
 from starknet_py.net.signer.stark_curve_signer import KeyPair, StarkCurveSigner
-from starknet_py.tests.e2e.fixtures.constants import MAX_RESOURCE_BOUNDS
-from starknet_py.tests.e2e.fixtures.misc import load_contract
-
-compiled_contract = load_contract("HelloStarknet")["sierra"]
-sierra_contract_class = create_sierra_compiled_contract(compiled_contract)
 
 
 @pytest.mark.parametrize(
     "transaction",
     [
-        InvokeV1(
-            sender_address=0x1,
-            calldata=[1, 2, 3],
-            max_fee=10000,
-            signature=[],
-            nonce=23,
-            version=1,
-        ),
-        DeployAccountV1(
-            class_hash=0x1,
-            contract_address_salt=0x2,
-            constructor_calldata=[1, 2, 3, 4],
-            max_fee=10000,
-            signature=[],
-            nonce=23,
-            version=1,
-        ),
-        DeclareV3(
-            contract_class=sierra_contract_class.convert_to_sierra_contract_class(),
-            compiled_class_hash=0x1,
-            sender_address=0x123,
-            signature=[],
-            nonce=4,
-            version=3,
-            resource_bounds=MAX_RESOURCE_BOUNDS,
-        ),
+        Mock(spec=InvokeV3, calculate_hash=MagicMock(return_value=111)),
+        Mock(spec=DeployAccountV3, calculate_hash=MagicMock(return_value=222)),
+        Mock(spec=DeclareV3, calculate_hash=MagicMock(return_value=333)),
     ],
 )
 def test_sign_transaction(transaction):
