faa97ec48b41577772739710fbd600096425160c
Fiiranek <jellybean@onet.eu>
2024-11-08 13:55:47 +0100

    Add `ws_client` and `full_node_ws_client` fixtures

--- starknet_py/tests/e2e/fixtures/clients.py
+++ starknet_py/tests/e2e/fixtures/clients.py
@@ -1,9 +1,11 @@
 from typing import Generator
 
 import pytest
+import pytest_asyncio
 
 from starknet_py.net.full_node_client import FullNodeClient
 from starknet_py.net.full_node_ws_client import FullNodeWSClient
+from starknet_py.net.ws_client import WSClient
 
 
 @pytest.fixture(name="client", scope="package")
@@ -11,13 +13,25 @@ def create_full_node_client(devnet) -> FullNodeClient:
     return FullNodeClient(node_url=devnet + "/rpc")
 
 
-@pytest.fixture(scope="package")
-async def full_node_ws_client(devnet_ws) -> Generator[str, None, None]:
+@pytest_asyncio.fixture(scope="package")
+async def ws_client(devnet_ws) -> Generator[WSClient, None, None]:
     """
-    Connects WebSocket client to devnet, returns its instance and disconnects after the tests.
+    Connects `WSClient` to devnet, returns its instance and disconnects after the tests.
     """
-    ws_client = FullNodeWSClient(devnet_ws)
+    client = WSClient(devnet_ws)
 
-    await ws_client.connect()
-    yield ws_client
-    await ws_client.disconnect()
+    await client.connect()
+    yield client
+    await client.disconnect()
+
+
+@pytest_asyncio.fixture(scope="package")
+async def full_node_ws_client(devnet_ws) -> Generator[FullNodeWSClient, None, None]:
+    """
+    Connects `FullNodeWSClient` client to devnet, returns its instance and disconnects after the tests.
+    """
+    client = FullNodeWSClient(devnet_ws)
+
+    await client.connect()
+    yield client
+    await client.disconnect()
