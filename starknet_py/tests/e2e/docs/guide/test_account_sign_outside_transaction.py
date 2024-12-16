9776344696316550a11e6d889bf4074bcc398bd8
Ilia Batii <batiyiv@gmail.com>
2024-12-16 14:54:34 +0000

    Update starknet_py/tests/e2e/docs/guide/test_account_sign_outside_transaction.py
    Co-authored-by: Franciszek Job <54181625+franciszekjob@users.noreply.github.com>

--- starknet_py/tests/e2e/docs/guide/test_account_sign_outside_transaction.py
+++ starknet_py/tests/e2e/docs/guide/test_account_sign_outside_transaction.py
@@ -18,31 +18,31 @@ async def test_account_outside_execution_any_caller(
     from starknet_py.hash.selector import get_selector_from_name
     from starknet_py.net.client_models import Call, OutsideExecutionTimeBounds
 
-    # Create a call to put value 1 under key 100. That will be executed
-    # as part of outside execution.
-
+    # Create a call to put value 100 at key 1.
     put_call = Call(
         to_addr=map_contract.address,
         selector=get_selector_from_name("put"),
         calldata=[1, 100],
     )
 
-    # Create a special signed execution call. This call can now be executed by
-    # the specified caller. In this case, caller is ANY_CALLER, a special constant
-    # that allows anyone to execute the call.
-    call = await argent_account.sign_outside_execution_call(
+    # Create an outside execution call. This call can now be executed by
+    # the specified caller. In this case, anyone will be able to execute it.
+
+    # Note that signing account does not need to have any funds to sign the transaction.
+    call = await argent_account.sign_outside_transaction(
         calls=[
             put_call,
         ],
+        # The transaction can be executed in specified timeframe.
         execution_time_bounds=OutsideExecutionTimeBounds(
             execute_after=datetime.datetime.now() - datetime.timedelta(hours=1),
             execute_before=datetime.datetime.now() + datetime.timedelta(hours=1),
         ),
+        # Use ANY_CALLER, a special constant that allows anyone to execute the call.
         caller=ANY_CALLER,
     )
 
-    # Execute the call as a normal invoke transaction
-    # can be executed from any account specified in the caller field
+    # Now, if you're in specified timeframe, you can perform the outside execution by another account.
     tx = await account.execute_v1(calls=[call], max_fee=int(1e18))
     await account.client.wait_for_tx(tx.transaction_hash)
 
