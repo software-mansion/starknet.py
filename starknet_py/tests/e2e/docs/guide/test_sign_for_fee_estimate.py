c382a207c66dcc86ab60e7c32b11434252e77a87
Fiiranek <jellybean@onet.eu>
2025-01-08 17:13:59 +0100

    Fix `test_signing_fee_estimate`

--- starknet_py/tests/e2e/docs/guide/test_sign_for_fee_estimate.py
+++ starknet_py/tests/e2e/docs/guide/test_sign_for_fee_estimate.py
@@ -7,7 +7,7 @@ from starknet_py.net.client_models import ResourceBounds
 async def test_signing_fee_estimate(account, map_contract):
     # docs: start
     # Create a transaction
-    call = map_contract.functions["put"].prepare_invoke_3(key=10, value=20)
+    call = map_contract.functions["put"].prepare_invoke_v3(key=10, value=20)
     transaction = await account.sign_invoke_v3(
         calls=call,
         l1_resource_bounds=ResourceBounds(max_amount=0, max_price_per_unit=0),
@@ -26,7 +26,7 @@ async def test_signing_fee_estimate(account, map_contract):
 
     # Use a new fee in original transaction
     transaction = await account.sign_invoke_v3(
-        calls=call, l1_resource_bounds=estimate.to_resource_bounds()
+        calls=call, l1_resource_bounds=estimate.to_resource_bounds().l1_gas
     )
 
     # Send a transaction
