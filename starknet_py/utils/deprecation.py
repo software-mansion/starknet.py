144663e484926d9f0d67d29cfee4a5a68a5b1c97
Fiiranek <jellybean@onet.eu>
2025-01-09 23:49:26 +0100

    Set `stacklevel` to 3

--- starknet_py/utils/deprecation.py
+++ starknet_py/utils/deprecation.py
@@ -3,4 +3,4 @@ import warnings
 
 def _print_deprecation_warning(message: str):
     warnings.simplefilter("default", DeprecationWarning)
-    warnings.warn(message, category=DeprecationWarning, stacklevel=2)
+    warnings.warn(message, category=DeprecationWarning, stacklevel=3)
