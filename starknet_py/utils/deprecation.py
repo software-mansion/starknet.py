23a31e816765ada7e519d7344e05bd994e4cffe3
Fiiranek <jellybean@onet.eu>
2025-01-10 13:02:37 +0100

    Remove setting warnings filter

--- starknet_py/utils/deprecation.py
+++ starknet_py/utils/deprecation.py
@@ -2,5 +2,4 @@ import warnings
 
 
 def _print_deprecation_warning(message: str):
-    warnings.simplefilter("default", DeprecationWarning)
     warnings.warn(message, category=DeprecationWarning, stacklevel=3)
