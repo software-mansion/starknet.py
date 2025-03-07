software-mansion/starknet_python_sdk

V3 transactions in outside execution tests for argent
* Added new argent contract v0.4.0 that supports transactions v3 (casm, json and abi)
* Registered new factory for argent v040 injection in tests
* Updated tests logic
* Added new test that checks for new type of errors (Reentrancy introduced in argent v0.4.0)
* Refacttored account creation logic in tests.

I tried to keep changes to minimum, but could not help myself to introduce refactoring of a test account creation logic.
For some reason those accounts were populated through dependency injection factories, which is ok, but at some point one factory
got responsible for all possible types of accounts through same factory which over time got bloated with crotches.

I moved all the crotch logic into `AccountToBeDeployedDetailsFactory` and want to propose to get rid of that class completely (well
not in this PR) replacing it with obvious calls, like create_specific_account(), prepay_account(), this will make code straighforward
and easy to read. Currently to understand what is happening in a particular test one need to hop through 3-5 layers of constructor
injection calls.

Fixes: 1546

starknet_py/tests/e2e/account/outside_execution_test.py
@@ -5,7 +5,11 @@ import pytest
 from starknet_py.constants import ANY_CALLER, OutsideExecutionInterfaceID
 from starknet_py.hash.selector import get_selector_from_name
 from starknet_py.net.account.account import BaseAccount
-from starknet_py.net.client_models import Call, OutsideExecutionTimeBounds
+from starknet_py.net.client_models import (
+    Call,
+    OutsideExecutionTimeBounds,
+    ResourceBounds,
+)
 from starknet_py.tests.e2e.fixtures.constants import MAX_FEE
 from starknet_py.transaction_errors import TransactionRevertedError
 
@@ -13,25 +17,37 @@ from starknet_py.transaction_errors import TransactionRevertedError
 @pytest.mark.asyncio
 async def test_argent_account_outside_execution_compatibility(
     argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
 ):
     result = await argent_account.supports_interface(OutsideExecutionInterfaceID.V1)
     assert result is True
     result = await argent_account.supports_interface(OutsideExecutionInterfaceID.V2)
     assert result is False
 
+    result = await argent_account_v040.supports_interface(
+        OutsideExecutionInterfaceID.V1
+    )
+    assert result is True
+    result = await argent_account_v040.supports_interface(
+        OutsideExecutionInterfaceID.V2
+    )
+    assert result is True
 
-# TODO (#1546): Remove skip mark
-@pytest.mark.skip
+
 @pytest.mark.asyncio
 async def test_account_outside_execution_any_caller(
-    argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
+    account: BaseAccount,
     map_contract,
 ):
-
     assert any(
         [
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V1),
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V2),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
         ]
     )
 
@@ -41,7 +57,7 @@ async def test_account_outside_execution_any_caller(
         calldata=[20, 20],
     )
 
-    call = await argent_account.sign_outside_execution_call(
+    call = await argent_account_v040.sign_outside_execution_call(
         calls=[
             put_call,
         ],
@@ -52,22 +68,33 @@ async def test_account_outside_execution_any_caller(
         caller=ANY_CALLER,
     )
 
-    tx = await argent_account.execute_v1(calls=[call], max_fee=MAX_FEE)
-    await argent_account.client.wait_for_tx(tx.transaction_hash)
+    tx = await account.execute_v3(
+        calls=[call],
+        l1_resource_bounds=ResourceBounds(
+            max_amount=int(1e5), max_price_per_unit=int(1e13)
+        ),
+    )
+    await account.client.wait_for_tx(tx.transaction_hash)
 
 
 @pytest.mark.asyncio
 async def test_account_outside_execution_for_invalid_caller(
-    argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
     account: BaseAccount,
     map_contract,
 ):
     assert any(
         [
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V1),
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V2),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
         ]
     )
+
+    random_address = 0x1234567890123456789012345678901234567890
 
     put_call = Call(
         to_addr=map_contract.address,
@@ -75,7 +102,7 @@ async def test_account_outside_execution_for_invalid_caller(
         calldata=[20, 20],
     )
 
-    call = await argent_account.sign_outside_execution_call(
+    call = await argent_account_v040.sign_outside_execution_call(
         calls=[
             put_call,
         ],
@@ -83,29 +110,37 @@ async def test_account_outside_execution_for_invalid_caller(
             execute_after=datetime.datetime.now() - datetime.timedelta(hours=1),
             execute_before=datetime.datetime.now() + datetime.timedelta(hours=1),
         ),
-        caller=account.address,
+        caller=random_address,
     )
 
-    tx = await argent_account.execute_v1(calls=[call], max_fee=MAX_FEE)
+    tx = await account.execute_v3(
+        calls=[call],
+        l1_resource_bounds=ResourceBounds(
+            max_amount=int(1e5), max_price_per_unit=int(1e13)
+        ),
+    )
 
     with pytest.raises(TransactionRevertedError) as err:
-        await argent_account.client.wait_for_tx(tx.transaction_hash)
+        await argent_account_v040.client.wait_for_tx(tx.transaction_hash)
 
     assert "argent/invalid-caller" in err.value.message
 
 
-# TODO (#1546): Remove skip mark
-@pytest.mark.skip
 @pytest.mark.asyncio
 async def test_account_outside_execution_for_impossible_time_bounds(
-    argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
+    account: BaseAccount,
     map_contract,
 ):
 
     assert any(
         [
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V1),
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V2),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
         ]
     )
 
@@ -115,7 +150,7 @@ async def test_account_outside_execution_for_impossible_time_bounds(
         calldata=[20, 20],
     )
 
-    call = await argent_account.sign_outside_execution_call(
+    call = await argent_account_v040.sign_outside_execution_call(
         calls=[put_call],
         execution_time_bounds=OutsideExecutionTimeBounds(
             execute_after=datetime.datetime.now() - datetime.timedelta(days=10),
@@ -124,9 +159,49 @@ async def test_account_outside_execution_for_impossible_time_bounds(
         caller=ANY_CALLER,
     )
 
-    tx = await argent_account.execute_v1(calls=[call], max_fee=MAX_FEE)
+    tx = await account.execute_v1(calls=[call], max_fee=MAX_FEE)
 
     with pytest.raises(TransactionRevertedError) as err:
-        await argent_account.client.wait_for_tx(tx.transaction_hash)
+        await argent_account_v040.client.wait_for_tx(tx.transaction_hash)
 
     assert "argent/invalid-timestamp" in err.value.message
+
+
+@pytest.mark.asyncio
+async def test_account_outside_execution_by_itself_is_impossible(
+    argent_account_v040: BaseAccount,
+    map_contract,
+):
+
+    assert any(
+        [
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
+        ]
+    )
+
+    put_call = Call(
+        to_addr=map_contract.address,
+        selector=get_selector_from_name("put"),
+        calldata=[20, 20],
+    )
+
+    call = await argent_account_v040.sign_outside_execution_call(
+        calls=[put_call],
+        execution_time_bounds=OutsideExecutionTimeBounds(
+            execute_after=datetime.datetime.now() - datetime.timedelta(days=10),
+            execute_before=datetime.datetime.now() - datetime.timedelta(days=9),
+        ),
+        caller=ANY_CALLER,
+    )
+
+    tx = await argent_account_v040.execute_v1(calls=[call], max_fee=MAX_FEE)
+
+    with pytest.raises(TransactionRevertedError) as err:
+        await argent_account_v040.client.wait_for_tx(tx.transaction_hash)
+
+    assert "ReentrancyGuard: reentrant call" in err.value.message

software-mansion/starknet.py

starknet_py/tests/e2e/account/outside_execution_test.py
@@ -11,28 +11,39 @@ from starknet_py.transaction_errors import TransactionRevertedError
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1560)")
 async def test_argent_account_outside_execution_compatibility(
     argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
 ):
     result = await argent_account.supports_interface(OutsideExecutionInterfaceID.V1)
     assert result is True
     result = await argent_account.supports_interface(OutsideExecutionInterfaceID.V2)
     assert result is False
 
+    result = await argent_account_v040.supports_interface(
+        OutsideExecutionInterfaceID.V1
+    )
+    assert result is True
+    result = await argent_account_v040.supports_interface(
+        OutsideExecutionInterfaceID.V2
+    )
+    assert result is True
 
-# TODO (#1546): Remove skip mark
-@pytest.mark.skip
+
 @pytest.mark.asyncio
 async def test_account_outside_execution_any_caller(
-    argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
+    account: BaseAccount,
     map_contract,
 ):
-
     assert any(
         [
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V1),
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V2),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
         ]
     )
 
@@ -42,7 +53,7 @@ async def test_account_outside_execution_any_caller(
         calldata=[20, 20],
     )
 
-    call = await argent_account.sign_outside_execution_call(
+    call = await argent_account_v040.sign_outside_execution_call(
         calls=[
             put_call,
         ],
@@ -53,25 +64,31 @@ async def test_account_outside_execution_any_caller(
         caller=ANY_CALLER,
     )
 
-    tx = await argent_account.execute_v3(
-        calls=[call], resource_bounds=MAX_RESOURCE_BOUNDS
+    tx = await account.execute_v3(
+        calls=[call],
+        resource_bounds=MAX_RESOURCE_BOUNDS,
     )
-    await argent_account.client.wait_for_tx(tx.transaction_hash)
+    await account.client.wait_for_tx(tx.transaction_hash)
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1560)")
 async def test_account_outside_execution_for_invalid_caller(
-    argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
     account: BaseAccount,
     map_contract,
 ):
     assert any(
         [
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V1),
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V2),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
         ]
     )
+
+    random_address = 0x1234567890123456789012345678901234567890
 
     put_call = Call(
         to_addr=map_contract.address,
@@ -79,7 +96,7 @@ async def test_account_outside_execution_for_invalid_caller(
         calldata=[20, 20],
     )
 
-    call = await argent_account.sign_outside_execution_call(
+    call = await argent_account_v040.sign_outside_execution_call(
         calls=[
             put_call,
         ],
@@ -87,31 +104,35 @@ async def test_account_outside_execution_for_invalid_caller(
             execute_after=datetime.datetime.now() - datetime.timedelta(hours=1),
             execute_before=datetime.datetime.now() + datetime.timedelta(hours=1),
         ),
-        caller=account.address,
+        caller=random_address,
     )
 
-    tx = await argent_account.execute_v3(
-        calls=[call], resource_bounds=MAX_RESOURCE_BOUNDS
+    tx = await account.execute_v3(
+        calls=[call],
+        resource_bounds=MAX_RESOURCE_BOUNDS,
     )
 
     with pytest.raises(TransactionRevertedError) as err:
-        await argent_account.client.wait_for_tx(tx.transaction_hash)
+        await argent_account_v040.client.wait_for_tx(tx.transaction_hash)
 
     assert "argent/invalid-caller" in err.value.message
 
 
-# TODO (#1546): Remove skip mark
-@pytest.mark.skip
 @pytest.mark.asyncio
 async def test_account_outside_execution_for_impossible_time_bounds(
-    argent_account: BaseAccount,
+    argent_account_v040: BaseAccount,
+    account: BaseAccount,
     map_contract,
 ):
 
     assert any(
         [
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V1),
-            await argent_account.supports_interface(OutsideExecutionInterfaceID.V2),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
         ]
     )
 
@@ -121,7 +142,7 @@ async def test_account_outside_execution_for_impossible_time_bounds(
         calldata=[20, 20],
     )
 
-    call = await argent_account.sign_outside_execution_call(
+    call = await argent_account_v040.sign_outside_execution_call(
         calls=[put_call],
         execution_time_bounds=OutsideExecutionTimeBounds(
             execute_after=datetime.datetime.now() - datetime.timedelta(days=10),
@@ -130,11 +151,49 @@ async def test_account_outside_execution_for_impossible_time_bounds(
         caller=ANY_CALLER,
     )
 
-    tx = await argent_account.execute_v3(
-        calls=[call], resource_bounds=MAX_RESOURCE_BOUNDS
-    )
+    tx = await account.execute_v3(calls=[call], resource_bounds=MAX_RESOURCE_BOUNDS)
 
     with pytest.raises(TransactionRevertedError) as err:
-        await argent_account.client.wait_for_tx(tx.transaction_hash)
+        await argent_account_v040.client.wait_for_tx(tx.transaction_hash)
 
     assert "argent/invalid-timestamp" in err.value.message
+
+
+@pytest.mark.asyncio
+async def test_account_outside_execution_by_itself_is_impossible(
+    argent_account_v040: BaseAccount,
+    map_contract,
+):
+
+    assert any(
+        [
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V1
+            ),
+            await argent_account_v040.supports_interface(
+                OutsideExecutionInterfaceID.V2
+            ),
+        ]
+    )
+
+    put_call = Call(
+        to_addr=map_contract.address,
+        selector=get_selector_from_name("put"),
+        calldata=[20, 20],
+    )
+
+    call = await argent_account_v040.sign_outside_execution_call(
+        calls=[put_call],
+        execution_time_bounds=OutsideExecutionTimeBounds(
+            execute_after=datetime.datetime.now() - datetime.timedelta(days=10),
+            execute_before=datetime.datetime.now() - datetime.timedelta(days=9),
+        ),
+        caller=ANY_CALLER,
+    )
+
+    tx = await argent_account_v040.execute_v3(calls=[call], resource_bounds=MAX_RESOURCE_BOUNDS)
+
+    with pytest.raises(TransactionRevertedError) as err:
+        await argent_account_v040.client.wait_for_tx(tx.transaction_hash)
+
+    assert "ReentrancyGuard: reentrant call" in err.value.message