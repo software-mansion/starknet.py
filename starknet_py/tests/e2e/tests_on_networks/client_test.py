d359f78a7dd267b1cf6235f86cdd1426fcf03f86
Fiiranek <jellybean@onet.eu>
2025-04-14 13:13:19 +0200

    Fix formatting

--- starknet_py/tests/e2e/tests_on_networks/client_test.py
+++ starknet_py/tests/e2e/tests_on_networks/client_test.py
@@ -575,7 +575,9 @@ async def test_warning_on_incompatible_node_spec_version(client_sepolia_testnet)
     client_sepolia_testnet_copy = copy.deepcopy(client_sepolia_testnet)
     old_rpc_url = client_sepolia_testnet_copy.url.replace("v0_8", "v0_7")
     client_sepolia_testnet_copy.url = old_rpc_url
-    client_sepolia_testnet_copy._client.url = old_rpc_url # pylint: disable=protected-access
+    client_sepolia_testnet_copy._client.url = (
+        old_rpc_url  # pylint: disable=protected-access
+    )
 
     with warnings.catch_warnings(record=True) as warns:
         await client_sepolia_testnet_copy.get_chain_id()
