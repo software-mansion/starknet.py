9371da3a3efb1958b6e5ca4d10f386f93c1cf938
Fiiranek <jellybean@onet.eu>
2025-04-29 14:01:09 +0200

    Remove skipping tests

--- starknet_py/tests/e2e/tests_on_networks/trace_api_test.py
+++ starknet_py/tests/e2e/tests_on_networks/trace_api_test.py
@@ -14,7 +14,6 @@ from starknet_py.net.client_models import (
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1562)")
 async def test_trace_transaction_invoke_v3(client_sepolia_testnet):
     invoke_tx_hash = 0x26476DA48E56E5E7025543AD0BB9105DF00EE08571C6D17C4207462FF7717C4
     trace = await client_sepolia_testnet.trace_transaction(tx_hash=invoke_tx_hash)
@@ -27,7 +26,6 @@ async def test_trace_transaction_invoke_v3(client_sepolia_testnet):
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1562)")
 async def test_trace_transaction_declare_v3(client_sepolia_testnet):
     declare_tx_hash = 0x6054540622D534FFFFB162A0E80C21BC106581EAFEB3EFAD29385B78E04983D
     trace = await client_sepolia_testnet.trace_transaction(tx_hash=declare_tx_hash)
@@ -39,7 +37,6 @@ async def test_trace_transaction_declare_v3(client_sepolia_testnet):
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1562)")
 async def test_trace_transaction_deploy_account_v3(client_sepolia_testnet):
     deploy_account_tx_hash = (
         0x06718B783A0B888F5421C4EB76A532FEB9FD5167B2B09274298F79798C782B32
@@ -56,7 +53,6 @@ async def test_trace_transaction_deploy_account_v3(client_sepolia_testnet):
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1562)")
 async def test_trace_transaction_l1_handler(client_sepolia_testnet):
     l1_handler_tx_hash = (
         0x4C8C57B3AB646EF56AEF3DEF69A01BC86D049B98F25EBFE3699334D86C24D5
@@ -71,7 +67,6 @@ async def test_trace_transaction_l1_handler(client_sepolia_testnet):
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1562)")
 async def test_trace_transaction_reverted(client_sepolia_testnet):
     tx_hash = 0x00FECCA6A328DD11F40B79C30FE22D23BC6975D1A0923A95B90AFF4016A84333
     trace = await client_sepolia_testnet.trace_transaction(tx_hash=tx_hash)
@@ -80,7 +75,6 @@ async def test_trace_transaction_reverted(client_sepolia_testnet):
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip("TODO(#1562)")
 async def test_get_block_traces(client_sepolia_testnet):
     block_number = 80000
     block_transaction_traces = await client_sepolia_testnet.trace_block_transactions(
