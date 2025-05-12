0d254cc0ac3368eb5e65e81d35c9a0ae723bd146
Fiiranek <jellybean@onet.eu>
2025-05-12 15:02:44 +0200

    update automation rules

--- starknet_py/tests/unit/signer/test_ledger_signer.py
+++ starknet_py/tests/unit/signer/test_ledger_signer.py
@@ -141,11 +141,10 @@ async def _get_account_balance_strk(client: FullNodeClient, address: int):
 
 @pytest.mark.asyncio
 # TODO (#1425): Currently Ledger tests are skipped on Windows due to different Speculos setup.
-# @pytest.mark.skipif(
-#     platform == "win32",
-#     reason="Testing Ledger is skipped on Windows due to different Speculos setup.",
-# )
-@pytest.mark.skip
+@pytest.mark.skipif(
+    platform == "win32",
+    reason="Testing Ledger is skipped on Windows due to different Speculos setup.",
+)
 async def test_deploy_account_and_transfer(client_with_predeclared_argent):
     client = client_with_predeclared_argent
     signer = LedgerSigner(
