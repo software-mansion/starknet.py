776dd86d4ebe01381a7510f1f310781d513f19e9
Fiiranek <jellybean@onet.eu>
2025-05-16 01:33:25 +0200

    Change type of `l1_address` to `Felt`

--- starknet_py/net/schemas/rpc/transactions.py
+++ starknet_py/net/schemas/rpc/transactions.py
@@ -47,7 +47,7 @@ from starknet_py.utils.schema import Schema
 
 class L2toL1MessageSchema(Schema):
     l2_address = Felt(data_key="from_address", required=True)
-    l1_address = EthAddress(data_key="to_address", required=True)
+    l1_address = Felt(data_key="to_address", required=True)
     payload = fields.List(Felt(), data_key="payload", required=True)
 
     @post_load
