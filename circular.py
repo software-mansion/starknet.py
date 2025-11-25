4a2c90980787dcbca23e010ff56b6ce872d44562
Fiiranek <jellybean@onet.eu>
2025-11-25 12:59:57 +0100

    Remove print

--- circular.py
+++ circular.py
@@ -12,7 +12,6 @@ def _import_from_path(module_name, file_path):
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
     sys.modules[module_name] = module
-    print(f"module_name: {module_name}, file_path: {file_path}")
     spec.loader.exec_module(module)
 
 
