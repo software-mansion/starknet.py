1a1d558507ee4825e1952aa52d8a009b0a0ff5a8
Fiiranek <jellybean@onet.eu>
2025-12-08 11:17:57 +0100

    Remove unused import

--- starknet_py/net/schemas/rpc/executables_api.py
+++ starknet_py/net/schemas/rpc/executables_api.py
@@ -2,7 +2,6 @@ from typing import Any, Optional
 
 from marshmallow import ValidationError, fields, post_load, validate
 
-from starknet_py.constants import FIELD_PRIME
 from starknet_py.net.executable_models import (
     AllocConstantSize,
     AllocConstantSizeInner,
