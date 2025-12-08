de689fd54721051f8cce8450e5da4de9b6fa8a8b
Franciszek Job <54181625+franciszekjob@users.noreply.github.com>
2025-12-08 12:46:01 +0100

    Add `StorageKeySchema` (#1685)

--- starknet_py/net/schemas/contracts_storage_keys.py
+++ starknet_py/net/schemas/contracts_storage_keys.py
@@ -1,9 +1,28 @@
-from marshmallow import fields
+from marshmallow import fields, validate
 
 from starknet_py.net.schemas.common import Felt
 from starknet_py.utils.schema import Schema
 
+STORAGE_KEY_PATTERN = r"^0x(0|[0-7]{1}[a-fA-F0-9]{0,62})$"
+
+
+class StorageKeySchema(fields.Str):
+    """
+    Storage key schema, represented as a string of hex digits
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            validate=validate.Regexp(
+                regex=STORAGE_KEY_PATTERN,
+                error=f"Storage key must match the pattern: {STORAGE_KEY_PATTERN}",
+            ),
+            **kwargs,
+        )
+
 
 class ContractsStorageKeysSchema(Schema):
     contract_address = Felt(data_key="contract_address", required=True)
-    storage_keys = fields.List(Felt(), data_key="storage_keys", required=True)
+    storage_keys = fields.List(
+        StorageKeySchema(), data_key="storage_keys", required=True
+    )
