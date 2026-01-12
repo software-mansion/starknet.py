b1cdba66741d2fec97206d1e2a738b5bd8252305
Fiiranek <jellybean@onet.eu>
2026-01-12 01:40:03 +0100

    Fixes

--- starknet_py/tests/e2e/fixtures/misc.py
+++ starknet_py/tests/e2e/fixtures/misc.py
@@ -149,11 +149,8 @@ def load_contract(contract_name: str, package: Optional[str] = None):
         else:
             package = "contracts_v2"
 
-    artifacts_map_path = (
-        f"{MOCK_DIR}/{package}/target/dev/{package}.starknet_artifacts.json"
-    )
-    directory = f"{MOCK_DIR}/{package}/target/dev"
-
+    directory = MOCK_DIR / package / "target" / "dev"
+    artifacts_map_path = directory / f"{package}.starknet_artifacts.json"
     artifacts_map = json.loads(Path(artifacts_map_path).read_text("utf-8"))
 
     artifact_file_names = next(
