356ff9e15de440a713d6da491a113cbe65a07b2a
Fiiranek <jellybean@onet.eu>
2026-03-09 13:14:46 +0100

    Remove sleeps

--- starknet_py/tests/e2e/tests_on_networks/account_test.py
+++ starknet_py/tests/e2e/tests_on_networks/account_test.py
@@ -24,7 +24,6 @@ from starknet_py.tests.e2e.utils import _new_address
 @pytest.mark.extended_network_test
 @pytest.mark.asyncio
 async def test_execute_v3(account_sepolia_testnet):
-    sleep(10)
     sepolia_balance_contract_address = (
         0x0589A8B8BF819B7820CB699EA1F6C409BC012C9B9160106DDC3DACD6A89653CF
     )
@@ -52,7 +51,6 @@ async def test_deploy_account_v3(
     account_sepolia_testnet,
     client_sepolia_testnet,
 ):
-    sleep(10)
     key_pair = KeyPair.generate()
     constructor_calldata = [0, key_pair.public_key, 1]
     address, salt = _new_address(ARGENT_V040_CLASS_HASH, constructor_calldata)
@@ -112,7 +110,6 @@ async def test_deploy_account_v3(
 @pytest.mark.extended_network_test
 @pytest.mark.asyncio
 async def test_declare_v3(account_sepolia_testnet):
-    sleep(10)
     contract = load_contract(contract_name="SimpleContract", package="contracts_salted")
 
     compiled_contract = contract["sierra"]
@@ -134,7 +131,6 @@ async def test_declare_v3(account_sepolia_testnet):
 @pytest.mark.extended_network_test
 @pytest.mark.asyncio
 async def test_deploy_v3(account_sepolia_testnet):
-    sleep(10)
     calldata = []
     salt = _get_random_salt()
     deployer = Deployer()
