35c524f2441f1804147ab453685d95c010d28e14
ddoktorski <45050160+ddoktorski@users.noreply.github.com>
2026-05-07 13:43:50 +0200

    Use new contract in `test_declare_v3_tx` (#1720)

--- starknet_py/tests/e2e/declare/declare_test.py
+++ starknet_py/tests/e2e/declare/declare_test.py
@@ -1,15 +1,21 @@
 import pytest
 
+from starknet_py.common import create_casm_class
+from starknet_py.hash.casm_class_hash import compute_casm_class_hash
 from starknet_py.net.client_models import TransactionExecutionStatus
 from starknet_py.net.models.transaction import DeclareV3
 from starknet_py.tests.e2e.fixtures.constants import MAX_RESOURCE_BOUNDS
+from starknet_py.tests.e2e.fixtures.misc import load_contract
 
 
 @pytest.mark.asyncio
-async def test_declare_v3_tx(account, abi_types_compiled_contract_and_class_hash):
+async def test_declare_v3_tx(account):
+    contract = load_contract(contract_name="TestContract5", package="contracts_v2")
     declare_tx = await account.sign_declare_v3(
-        compiled_contract=abi_types_compiled_contract_and_class_hash[0],
-        compiled_class_hash=abi_types_compiled_contract_and_class_hash[1],
+        compiled_contract=contract["sierra"],
+        compiled_class_hash=compute_casm_class_hash(
+            create_casm_class(contract["casm"])
+        ),
         resource_bounds=MAX_RESOURCE_BOUNDS,
     )
     assert isinstance(declare_tx, DeclareV3)
